aaab96205dfed1e8c24d81bb7f893841ddc1b56b
pmontens <pieterjan@montens.net>
2021-05-07 18:00:44 +0200

    :wrench: adding api documentation screens (schema, openapi, redoc)

--- itou_c4_api/c4_directory/urls.py
+++ itou_c4_api/c4_directory/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from c4_directory import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('siaes/', views.siae_list),
     path('siae/<int:key>/', views.siae_detail),
+    # YOUR PATTERNS
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='OpenAPI'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- itou_c4_api/settings.py
+++ itou_c4_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'itou_c4_api',
     'c4_directory.apps.C4DirectoryConfig',
     'cocorico.apps.CocoricoConfig'
@@ -140,3 +141,15 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'API marché de l\'inclusion',
+    'DESCRIPTION': 'Une initiative de la Plateforme de l\'inclusion',
+    'VERSION': '0.1.0',
+    # OTHER SETTINGS
+}
