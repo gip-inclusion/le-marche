9a6c37e76af877b98021f4323ee9b072c94d654b
pmontens <pieterjan@montens.net>
2021-05-20 11:06:13 +0200

    :hammer_and_pick: adding sector endpoint

--- itou_c4_api/c4_directory/models.py
+++ itou_c4_api/c4_directory/models.py
@@ -54,11 +54,25 @@ class Siae(models.Model):
     class Meta:
         ordering = ['name']
 
-    # latitude
-    # longitude
-    # creation_date
-    # workers_count
-    # prestation_types
-    # sectors
-    # phone ?
-    # email ?
+
+class Sector(models.Model):
+    id = models.IntegerField(primary_key=True)
+    parent = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)
+    lft = models.IntegerField()
+    lvl = models.IntegerField()
+    rgt = models.IntegerField()
+    root = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class SectorString(models.Model):
+    id = models.IntegerField(primary_key=True)
+    translatable = models.ForeignKey(Sector, models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=100)
+    locale = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        ordering = ['id']
