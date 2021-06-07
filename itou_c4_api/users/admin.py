cf9d7d099b12b0f724906de7ebfccf79d5270743
pmontens <pieterjan@montens.net>
2021-06-07 14:35:30 +0200

    :wrench: updated user model, differentiated data access depending on user token (url)

--- itou_c4_api/users/admin.py
+++ itou_c4_api/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.forms import UserCreationForm, UserChangeForm
+from users.models import User
+
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
