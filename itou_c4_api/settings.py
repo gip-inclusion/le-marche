4eb0591d3bc40755fc2c22239cdcb6ab3c11ec20
pmontens <pieterjan@montens.net>
2021-06-08 17:55:03 +0200

    :wrench: clevercloud tuning

--- itou_c4_api/settings.py
+++ itou_c4_api/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = '[KEY]'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.environ['CURRENT_HOST'],
+]
 
 
 # Application definition
