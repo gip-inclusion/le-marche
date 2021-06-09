bf61f0743d9d9556cda8600eaee629c5c76be0cb
pmontens <pieterjan@montens.net>
2021-06-09 11:55:27 +0200

    :art: serving static files

--- itou_c4_api/urls.py
+++ itou_c4_api/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('c4_directory.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
