c2dc44e67b8ba9b4f0f37371ecacf81d5ea4d486
pmontens <pieterjan@montens.net>
2021-06-17 10:01:12 +0200

    :rotating_light: hotfix, set default debug to false

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False") in ("true", "True", "1", "t")
 
 ALLOWED_HOSTS = [
     "localhost",
