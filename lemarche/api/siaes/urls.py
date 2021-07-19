3d2bd60d0e51442bcb9730ff9fb0f714994b5af2
pmontens <pieterjan@montens.net>
2021-07-19 15:45:18 +0200

    :hammer: refactor of endpoint, using modelviewset & mixins

--- lemarche/api/siaes/urls.py
+++ lemarche/api/siaes/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     # path("api/", include(router.urls)),
     # path('siaes/', views.siae_list),
     # path('siae/<int:key>', views.siae_detail, name='siae-detail'),
-    path("secteurs/", views.SectorList.as_view()),
-    path("secteurs/<str:pk>", views.SectorDetail.as_view()),
+    path("secteurs/", views.Sectors.as_view({'get': 'list'})),
+    path("secteurs/<str:pk>", views.Sectors.as_view({'get': 'retrieve'})),
     # YOUR PATTERNS
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
--- lemarche/api/siaes/views.py
+++ lemarche/api/siaes/views.py
@@ -10,9 +10,10 @@ from lemarche.cocorico.models import Directory, DirectorySector, Sector, SectorS
 from lemarche.users.models import User
 from django.http import HttpResponse
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from hashids import Hashids
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -41,6 +42,47 @@ def decode_hashed_pk(func):
 
 #  FIXME:Given the peculiar user-token access, it's not possible
 # to use generic class-based views at this moment.
+class SiaeList(APIView):
+    """
+    Liste exhaustive des SIAE (structures de l'insertion par l'activité économique)
+    """
+
+    queryset = Directory.objects.all()
+    serializer_class = SiaeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['kind', 'department']
+
+    def get(self, request, format=None):
+        if request.method == "GET":
+            queryset = Directory.objects.all()
+
+            for backend in list(self.filter_backends):
+                queryset = backend().filter_queryset(self.request, queryset, self)
+
+            token = request.GET.get("token", None)
+            if not token:
+                # serializer = SiaeLightSerializer(queryset[:10], many=True)
+                serializer = SiaeLightSerializer(
+                    queryset[:10],
+                    many=True,
+                    context={"hashed_pk": True},
+                )
+            else:
+
+                try:
+                    user = User.objects.get(api_key=token)
+                    assert user.has_perm("api.access_api")
+                except User.DoesNotExist:
+                    return HttpResponse("503: Not Allowed", status=503)
+
+                serializer = SiaeSerializer(
+                    queryset,
+                    many=True,
+                    context={"hashed_pk": True},
+                )
+            # return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
+
 class SiaeList(APIView):
     """
     Liste exhaustive des SIAE (structures de l'insertion par l'activité économique)
@@ -125,19 +167,16 @@ class SiaeDetail(APIView):
         return Response(serializer.data)
 
 
-class SectorList(mixins.ListModelMixin, generics.GenericAPIView):
+class Sectors(mixins.ListModelMixin,
+              mixins.RetrieveModelMixin,
+              viewsets.GenericViewSet):
     queryset = SectorString.objects.get_all_active_sectors()
     serializer_class = SectorStringSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
     def get_serializer_context(self):
         context = {"hashed_pk": True}
         return context
 
-
-class SectorDetail(APIView):
     def get_object(self, pk):
         try:
             return SectorString.objects.get_sector(pk=pk)
@@ -145,7 +184,7 @@ class SectorDetail(APIView):
             raise Http404
 
     @decode_hashed_pk
-    def get(self, request, pk, format=None):
-        sector = self.get_object(pk)
-        serializer = SectorStringSerializer(sector, many=False, context={"hashed_pk": True})
+    def retrieve(self, request, pk=None, format=None):
+        queryset = self.get_object(pk=pk)
+        serializer = SectorStringSerializer(queryset, many=False, context={"hashed_pk": True})
         return Response(serializer.data)
