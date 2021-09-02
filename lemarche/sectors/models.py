ad0237add67362ff3b79af1db37609cc7da9fed5
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-02 15:00:02 +0200

    Create models Sector & SectorGroup

---
+++ lemarche/sectors/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+
+class SectorGroup(models.Model):
+    name = models.CharField(verbose_name="Nom", max_length=255)
+    slug = models.SlugField(verbose_name="Slug", max_length=255, unique=True)
+    created_at = models.DateTimeField("Date de création", default=timezone.now)
+    updated_at = models.DateTimeField("Date de modification", auto_now=True)
+
+    class Meta:
+        verbose_name = "Groupe de secteurs d'activité"
+        verbose_name_plural = "Groupes de secteurs d'activité"
+
+
+class Sector(models.Model):
+    name = models.CharField(verbose_name="Nom", max_length=255)
+    slug = models.SlugField(verbose_name="Slug", max_length=255, unique=True)
+    group = models.ForeignKey(
+        "SectorGroup",
+        verbose_name="Groupe parent",
+        related_name='sectors',
+        on_delete=models.SET_NULL,
+        null=True, blank=True)
+    created_at = models.DateTimeField("Date de création", default=timezone.now)
+    updated_at = models.DateTimeField("Date de modification", auto_now=True)
+
+    class Meta:
+        verbose_name = "Secteur d'activité"
+        verbose_name_plural = "Secteurs d'activité"
