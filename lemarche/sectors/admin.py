5881457a451380e242b065cfa16136818a68e96b
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-02 15:24:37 +0200

    Add simple link to SectorGroup items in admin

--- lemarche/sectors/admin.py
+++ lemarche/sectors/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.db.models import Count
+from django.utils.html import format_html
 
 from lemarche.sectors.models import SectorGroup, Sector
 
 
 @admin.register(SectorGroup)
 class SectorGroupAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "created_at"]
+    list_display = ["id", "name", "nb_sectors", "created_at"]
     search_fields = ["id", "name"]
 
     readonly_fields = ["created_at", "updated_at"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(sector_count=Count('sectors'))
+        return qs
+
+    def nb_sectors(self, sector_group):
+        url = reverse("admin:sectors_sector_changelist") + f"?group__id__exact={sector_group.id}"
+        return format_html(f"<a href=\"{url}\">{sector_group.sector_count}</a>")
+    nb_sectors.short_description = "Nombre de secteurs d'activité"
+    nb_sectors.admin_order_field = "sector_count"
 
 
 @admin.register(Sector)
