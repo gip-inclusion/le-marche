0f460bbd9fd2c95334f91441470c6fe51dc2e099
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-07 16:05:21 +0200

    :toolbox: Premier cleanup de l'API (#20)
    * Cleanup API urls. Use ViewSets
    
    * Move API utils to own folder. Move API Sectors to own folder

--- lemarche/api/siaes/serializers.py
+++ lemarche/api/siaes/serializers.py
@@ -1,83 +1,9 @@
-from hashids import Hashids
 from rest_framework import serializers
 from drf_spectacular.utils import extend_schema_field, OpenApiTypes
 
 from lemarche.siaes.models import Siae
-from lemarche.api.models import Sector, SectorString
-
-
-# TODO: implement hashid as shared object
-hasher = Hashids(alphabet="1234567890ABCDEF", min_length=5)
-
-
-class SectorSerializer(serializers.ModelSerializer):
-    id = serializers.SerializerMethodField()
-    parent = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Sector
-        fields = [
-            "id",
-            "parent",
-        ]
-
-    @extend_schema_field(OpenApiTypes.STR)
-    def get_id(self, obj):
-        must_hash_id = self.context.get("hashed_pk", False)
-        if must_hash_id:
-            return hasher.encode(obj.id)
-
-        return obj.id
-
-    @extend_schema_field(OpenApiTypes.STR)
-    def get_parent(self, obj):
-        if obj.parent is None:
-            return None
-
-        must_hash_id = self.context.get("hashed_pk", False)
-        return hasher.encode(obj.parent.id) if must_hash_id else obj.parent.id
-
-
-class SectorSimpleSerializer(SectorSerializer):
-    url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Sector
-        fields = [
-            "id",
-            "url",
-        ]
-
-    @extend_schema_field(OpenApiTypes.STR)
-    def get_url(self, obj):
-        # Writing URL by hand is a hack, use hyperlinkedmodelserializer for greater good
-        must_hash_id = self.context.get("hashed_pk", False)
-        key = hasher.encode(obj.id) if must_hash_id else obj.id
-        return f"/secteurs/{key}"
-
-
-class SectorStringSerializer(serializers.ModelSerializer):
-    # TODO : Use hyperlinkedmodelserializer
-    hierarchie = SectorSerializer(many=False, read_only=True, source="translatable")
-
-    nom = serializers.CharField(source="name")
-    url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = SectorString
-        fields = [
-            "nom",
-            "slug",
-            "url",
-            "hierarchie",
-        ]
-
-    @extend_schema_field(OpenApiTypes.STR)
-    def get_url(self, obj):
-        # Writing URL by hand is a hack, use hyperlinkedmodelserializer for greater good
-        must_hash_id = self.context.get("hashed_pk", False)
-        key = hasher.encode(obj.translatable.id) if must_hash_id else obj.translatable.id
-        return f"/secteurs/{key}"
+from lemarche.api.sectors.serializers import SectorSimpleSerializer
+from lemarche.api.utils import hasher
 
 
 class SiaeSerializer(serializers.ModelSerializer):
---
+++ lemarche/api/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from lemarche.api.siaes.views import SiaeViewSet
+from lemarche.api.sectors.views import SectorViewSet
+
+
+router = routers.DefaultRouter()
+
+router.register(r'siaes', SiaeViewSet, basename='siaes')
+router.register(r'sectors', SectorViewSet, basename='sectors')
+
+
+urlpatterns = [
+    # Additional API endpoints
+    path("siaes/siret/<str:siret>/", SiaeViewSet.as_view({"get": "retrieve_by_siret"})),
+
+    # Swagger / OpenAPI documentation
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+]
+
+urlpatterns += router.urls
