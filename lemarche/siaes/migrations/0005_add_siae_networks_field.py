d7c54275fee14bfa9dcdf4322aa835d03bf3dcab
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-08 11:04:38 +0200

    Fix siaes migration files

--- lemarche/siaes/migrations/0005_add_siae_networks_field.py
+++ lemarche/siaes/migrations/0005_add_siae_networks_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('siaes', '0005_siaenetwork'),
+        ('siaes', '0004_siaenetwork'),
     ]
 
     operations = [
