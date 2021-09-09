c6894833fadb04865d02a93134fcd70387cf01ee
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-09 14:25:58 +0200

    Add SiaeLabel model & admin

--- lemarche/siaes/admin.py
+++ lemarche/siaes/admin.py
@@ -3,12 +3,12 @@ from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html
 
-from lemarche.siaes.models import Siae, SiaeOffer
+from lemarche.siaes.models import Siae, SiaeOffer, SiaeLabel
 
 
 @admin.register(Siae)
 class SiaeAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "siret", "kind", "nb_offers", "created_at"]
+    list_display = ["id", "name", "siret", "kind", "nb_offers", "nb_labels", "created_at"]
     list_filter = ["kind", "networks"]
     search_fields = ["id", "name"]
 
@@ -54,7 +54,9 @@ class SiaeAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        qs = qs.annotate(offer_count=Count('offers'))
+        qs = qs \
+            .annotate(offer_count=Count('offers')) \
+            .annotate(label_count=Count('labels'))
         return qs
 
     def nb_offers(self, siae):
@@ -62,6 +64,12 @@ class SiaeAdmin(admin.ModelAdmin):
         return format_html(f"<a href=\"{url}\">{siae.offer_count}</a>")
     nb_offers.short_description = "Nombre de prestations"
     nb_offers.admin_order_field = "offer_count"
+
+    def nb_labels(self, siae):
+        url = reverse("admin:siaes_siaeoffer_changelist") + f"?siae__id__exact={siae.id}"
+        return format_html(f"<a href=\"{url}\">{siae.label_count}</a>")
+    nb_labels.short_description = "Nombre de labels"
+    nb_labels.admin_order_field = "label_count"
 
 
 @admin.register(SiaeOffer)
@@ -76,5 +84,20 @@ class SiaeOfferAdmin(admin.ModelAdmin):
     def siae_with_link(self, offer):
         url = reverse("admin:siaes_siae_change", args=[offer.siae_id])
         return format_html(f"<a href=\"{url}\">{offer.siae}</a>")
+    siae_with_link.short_description = "Structure"
+    siae_with_link.admin_order_field = "siae"
+
+
+@admin.register(SiaeLabel)
+class SiaeLabelAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "siae_with_link", "created_at"]
+    search_fields = ["id", "name"]
+
+    autocomplete_fields = ["siae"]
+    readonly_fields = ["created_at", "updated_at"]
+
+    def siae_with_link(self, label):
+        url = reverse("admin:siaes_siae_change", args=[label.siae_id])
+        return format_html(f"<a href=\"{url}\">{label.siae}</a>")
     siae_with_link.short_description = "Structure"
     siae_with_link.admin_order_field = "siae"
