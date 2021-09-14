a60583d9957d9944be1a64fd8588e31cbf213847
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-14 17:38:35 +0200

    Enrich Page model with additional fields

--- lemarche/pages/admin.py
+++ lemarche/pages/admin.py
@@ -5,11 +5,39 @@ from django.db import models
 
 from ckeditor.widgets import CKEditorWidget
 
+from lemarche.pages.models import Page
 
-class FlatPageCustom(FlatPageAdmin):
+
+class PageAdmin(FlatPageAdmin):
+    list_display = ["url", "title", "created_at", "updated_at"]
+
+    readonly_fields = ["created_at", "updated_at"]
     formfield_overrides = {
-        models.TextField: {'widget': CKEditorWidget}
+        models.TextField: {"widget": CKEditorWidget}
     }
 
+    fieldsets = (
+        (None, {
+            "fields": (
+                "url",
+                "title",
+                "content"
+            ),
+        }),
+        ("SEO", {
+            "fields": (
+                "meta_title",
+                "meta_description"
+            )
+        }),
+        ("Info", {
+            "fields": (
+                "created_at",
+                "updated_at"
+            )
+        })
+    )
+
+
 admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageCustom)
+admin.site.register(Page, PageAdmin)
