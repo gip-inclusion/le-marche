9e7c7ccfe8c34a4f8d974e9277a5b68745579072
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-16 13:39:43 +0200

    Add Siae gestionnaire filter in user admin

--- lemarche/users/admin.py
+++ lemarche/users/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from lemarche.users.models import User
 from lemarche.users.forms import UserChangeForm, UserCreationForm
+
+
+class SiaeAdminFilter(admin.SimpleListFilter):
+    """Custom admin filter to target users who are linked to a SIAE."""
+
+    title = 'Gestionnaire de structure ?'
+    parameter_name = 'is_siae_admin'
+
+    def lookups(self, request, model_admin):
+        return (('Yes', 'Oui'), ('No', 'Non'))
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == 'Yes':
+            return queryset.siae_admins()
+        elif value == 'No':
+            return queryset.filter(siaes__isnull=True)
+        return queryset
 
 
 class ApiKeyFilter(admin.SimpleListFilter):
@@ -23,24 +43,18 @@ class ApiKeyFilter(admin.SimpleListFilter):
         return queryset
 
 
-class SiaeInline(admin.TabularInline):
-    model = User.siaes.through
-    # autocomplete_fields = ["name"]
-
-
 class UserAdmin(UserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = User
 
     list_display = ["id", "first_name", "last_name", "kind", "last_login", "created_at"]
-    list_filter = ["kind", ApiKeyFilter, "is_staff"]
+    list_filter = ["kind", SiaeAdminFilter, ApiKeyFilter, "is_staff"]
     search_fields = ["id", "email"]
     ordering = ["-created_at"]
 
     readonly_fields = [field.name for field in User._meta.fields if field.name.startswith("c4_")] + \
-        ["last_login", "created_at", "updated_at"]
-    # inlines = [SiaeInline]
+        ["siae_admin_list", "last_login", "created_at", "updated_at"]
 
     fieldsets = (
         (None, {
@@ -57,6 +71,11 @@ class UserAdmin(UserAdmin):
                 "phone",
             )
         }),
+        ("Structures", {
+            "fields": (
+                "siae_admin_list",
+            )
+        }),
         ("API", {
             "fields": (
                 "api_key",
@@ -69,7 +88,7 @@ class UserAdmin(UserAdmin):
                 "groups"
             )
         }),
-        ("Donnée C4 Symphony", {
+        ("Données C4 Symphony", {
             "fields": (
                 "c4_id",
                 "c4_website",
@@ -123,6 +142,20 @@ class UserAdmin(UserAdmin):
             )
         })
     )
+
+    def siae_admin_list(self, user):
+        siaes = user.siaes.all()
+        if not siaes:
+            return 'Aucune'
+        else:
+            html = ''
+            for siae in siaes:
+                html += format_html(
+                    '<a href="{obj_url}">{obj_name}</a></br>',
+                    obj_url=reverse('admin:siaes_siae_change', args=[siae.id]),
+                    obj_name=siae)
+            return format_html(html)
+    siae_admin_list.short_description = 'Gestionnaire de'
 
 
 admin.site.register(User, UserAdmin)
