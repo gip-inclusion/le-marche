ed90721019e5364f3710b5cbe95490fa1f0b4101
Raphael Odini <raphodn@users.noreply.github.com>
2021-09-16 08:56:59 +0200

    Ajout d'outils de développement (django-debug-toolbar, django-extensions) (#38)
    * Add django-debug-toolbar to dev packages
    
    * Add django-extensions to dev packages

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -16,3 +17,8 @@ urlpatterns = [
     path("", include("lemarche.www.pages.urls")),
     path("", include("django.contrib.flatpages.urls")),
 ]
+
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
