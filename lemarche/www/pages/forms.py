897e17b8fc21533907fa3a61f65f8d5280a723c6
David Deloo <hello@hellodeloo.com>
2021-09-28 16:35:55 +0200

    Update static pages (#67)
    * update page contact
    
    * update signup page
    
    * update login page
    
    * update logout & required field
    
    * update password reset pages
    
    * fix before merge + extra stuff
    
    Co-authored-by: Raphael Odini <raphodn@users.noreply.github.com>

--- lemarche/www/pages/forms.py
+++ lemarche/www/pages/forms.py
@@ -3,13 +3,24 @@ from django import forms
 
 class ContactForm(forms.Form):
     first_name = forms.CharField(
-        label="Prénom", widget=forms.TextInput(attrs={"autofocus": "autofocus"}), required=False
+        label="Prénom",
+        widget=forms.TextInput(attrs={"autofocus": "autofocus", "class": "form-control"}),
+        required=False,
     )
-    last_name = forms.CharField(label="Nom", required=False)
-    email = forms.EmailField(label="Adresse e-mail", required=True)
-    phone = forms.CharField(label="Numéro de téléphone", max_length=16, required=False)
+    last_name = forms.CharField(label="Nom", widget=forms.TextInput(attrs={"class": "form-control"}), required=False)
+    email = forms.EmailField(
+        label="Adresse e-mail", widget=forms.TextInput(attrs={"class": "form-control"}), required=True
+    )
+    phone = forms.CharField(
+        label="Numéro de téléphone",
+        max_length=16,
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+        required=False,
+    )
 
     # ChoiceField + choices=SUBJECT_CHOICES ?
-    subject = forms.CharField(label="Sujet", max_length=150, required=True)
+    subject = forms.CharField(
+        label="Sujet", max_length=150, widget=forms.TextInput(attrs={"class": "form-control"}), required=True
+    )
 
-    message = forms.CharField(label="Message", widget=forms.Textarea, required=True)
+    message = forms.CharField(label="Message", widget=forms.Textarea(attrs={"class": "form-control"}), required=True)
