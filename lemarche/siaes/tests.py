b87739deec3f3632fc2fc581553a741f61485f35
Raphael Odini <raphodn@users.noreply.github.com>
2021-10-04 14:06:05 +0200

    Other fix

--- lemarche/siaes/tests.py
+++ lemarche/siaes/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
 
 from lemarche.siaes.factories import SiaeFactory
+from lemarche.siaes.models import Siae
 
 
 class SiaeModelTest(TestCase):
     def setUp(self):
-        SiaeFactory()
+        pass
 
     def test_str(self):
         siae = SiaeFactory(name="Ma boite")
         self.assertEqual(str(siae), "Ma boite")
+
+    def test_live_queryset(self):
+        SiaeFactory(is_active=True, is_delisted=True)
+        SiaeFactory(is_active=False, is_delisted=True)
+        SiaeFactory(is_active=True, is_delisted=False)  # live
+        SiaeFactory(is_active=False, is_delisted=False)
+        self.assertEqual(Siae.objects.count(), 4)
+        self.assertEqual(Siae.objects.live().count(), 1)
