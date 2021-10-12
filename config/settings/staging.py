191b2c7d11d67b64688244d9cd6c73dc60b2cddc
Raphael Odini <raphodn@users.noreply.github.com>
2021-10-12 12:12:43 +0200

    Cleanup env, create staging.py file (#93)

---
+++ config/settings/staging.py
@@ -0,0 +1,9 @@
+from .base import *  # noqa
+
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "staging.inclusion.beta.gouv.fr",
+    "staging.lemarche.inclusion.beta.gouv.fr",
+    "bitoubi-django-staging.cleverapps.io",
+]
