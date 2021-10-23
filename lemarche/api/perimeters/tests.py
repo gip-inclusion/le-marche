2006b52064087c2f97fd29e2a8d4595ad6884271
Raphael Odini <raphodn@users.noreply.github.com>
2021-10-23 11:15:05 +0200

    Tests on API filtering

--- lemarche/api/perimeters/tests.py
+++ lemarche/api/perimeters/tests.py
@@ -1,10 +1,63 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from lemarche.perimeters.factories import PerimeterFactory
+from lemarche.perimeters.models import Perimeter
+from lemarche.users.factories import UserFactory
+
+
+class PerimeterListFilterApiTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.perimeter_city = PerimeterFactory(
+            name="Grenoble", kind=Perimeter.KIND_CITY, insee_code="38185", department_code="38", region_code="84"
+        )
+        cls.perimeter_department = PerimeterFactory(
+            name="Isère", kind=Perimeter.KIND_DEPARTMENT, insee_code="38", region_code="84"
+        )
+        cls.perimeter_region = PerimeterFactory(
+            name="Auvergne-Rhône-Alpes", kind=Perimeter.KIND_REGION, insee_code="R84"
+        )
+        UserFactory(api_key="admin")
+
+    def test_should_return_perimeter_list(self):
+        url = reverse("api:perimeters-list")  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 3)
+        self.assertEqual(len(response.data["results"]), 3)
+
+    def test_should_filter_perimeter_list_by_kind(self):
+        url = f"{reverse('api:perimeters-list')}?kind={Perimeter.KIND_CITY}"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+        url = f"{reverse('api:perimeters-list')}?kind={Perimeter.KIND_CITY}&kind={Perimeter.KIND_DEPARTMENT}"  # anonymous user  # noqa
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1 + 1)
+        self.assertEqual(len(response.data["results"]), 1 + 1)
+
+    def test_should_filter_perimeter_list_by_name(self):
+        url = f"{reverse('api:perimeters-list')}?name=grenob"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+
+    def test_should_filter_perimeter_list_by_result_count(self):
+        url = f"{reverse('api:perimeters-list')}?results=1"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+
+    def test_perimeter_list_should_not_paginate(self):
+        url = f"{reverse('api:perimeters-list')}?results=1"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["previous"], None)
+        self.assertEqual(response.data["next"], None)
+
 
 class PerimeterChoicesApiTest(TestCase):
-    def test_should_return_perimeter_kinds(self):
-        url = reverse("api:perimeter-kinds-list")
+    def test_should_return_perimeter_kinds_list(self):
+        url = reverse("api:perimeter-kinds-list")  # anonymous user
         response = self.client.get(url)
         self.assertEqual(response.data["count"], 3)
         self.assertEqual(len(response.data["results"]), 3)
--- lemarche/api/siaes/tests.py
+++ lemarche/api/siaes/tests.py
@@ -1,8 +1,75 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from lemarche.sectors.factories import SectorFactory
 from lemarche.siaes.factories import SiaeFactory
+from lemarche.siaes.models import Siae
 from lemarche.users.factories import UserFactory
+
+
+class SiaeListFilterApiTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # default_siae = {"kind": Siae.KIND_EI, "presta_type": [Siae.PRESTA_DISP], "department": "01"}
+        SiaeFactory(kind=Siae.KIND_EI, presta_type=[Siae.PRESTA_DISP], department="01")
+        SiaeFactory(kind=Siae.KIND_ETTI, presta_type=[Siae.PRESTA_DISP], department="01")  # siae_with_kind
+        SiaeFactory(kind=Siae.KIND_ACI, presta_type=[Siae.PRESTA_BUILD], department="01")  # siae_with_presta_type
+        SiaeFactory(kind=Siae.KIND_EI, presta_type=[Siae.PRESTA_PREST], department="38")  # siae_with_department
+        siae_with_sector_1 = SiaeFactory(kind=Siae.KIND_EI, presta_type=[Siae.PRESTA_DISP], department="01")
+        cls.sector_1 = SectorFactory()
+        siae_with_sector_1.sectors.add(cls.sector_1)
+        siae_with_sector_2 = SiaeFactory(kind=Siae.KIND_EI, presta_type=[Siae.PRESTA_DISP], department="01")
+        cls.sector_2 = SectorFactory()
+        siae_with_sector_2.sectors.add(cls.sector_2)
+        UserFactory(api_key="admin")
+
+    def test_should_return_siae_list(self):
+        url = reverse("api:siae-list")  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 6)
+        self.assertEqual(len(response.data["results"]), 6)
+
+    def test_should_filter_siae_list_by_kind(self):
+        # single
+        url = f"{reverse('api:siae-list')}?kind={Siae.KIND_ETTI}"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+        # multiple
+        url = f"{reverse('api:siae-list')}?kind={Siae.KIND_ETTI}&kind={Siae.KIND_ACI}"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1 + 1)
+        self.assertEqual(len(response.data["results"]), 1 + 1)
+
+    def test_should_filter_siae_list_by_presta_type(self):
+        # single
+        url = f"{reverse('api:siae-list')}?presta_type={Siae.PRESTA_BUILD}"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+        # multiple
+        url = f"{reverse('api:siae-list')}?presta_type={Siae.PRESTA_BUILD}&presta_type={Siae.PRESTA_PREST}"  # anonymous user  # noqa
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1 + 1)
+        self.assertEqual(len(response.data["results"]), 1 + 1)
+
+    def test_should_filter_siae_list_by_department(self):
+        url = f"{reverse('api:siae-list')}?department=38"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+
+    def test_should_filter_siae_list_by_sector(self):
+        # single
+        url = f"{reverse('api:siae-list')}?sectors={self.sector_1.slug}"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(len(response.data["results"]), 1)
+        # multiple
+        url = f"{reverse('api:siae-list')}?sectors={self.sector_1.slug}&sectors={self.sector_2.slug}"  # anonymous user
+        response = self.client.get(url)
+        self.assertEqual(response.data["count"], 1 + 1)
+        self.assertEqual(len(response.data["results"]), 1 + 1)
 
 
 class SiaeRetrieveBySiretApiTest(TestCase):
@@ -12,17 +79,17 @@ class SiaeRetrieveBySiretApiTest(TestCase):
         UserFactory(api_key="admin")
 
     def test_should_return_404_if_siret_unknown(self):
-        url = reverse("api:siae-retrieve-by-siret", args=["123"])
+        url = reverse("api:siae-retrieve-by-siret", args=["123"])  # anonymous user
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_should_return_404_if_siret_known_but_with_spaces(self):
-        url = reverse("api:siae-retrieve-by-siret", args=["123 123 123 12345"])
+        url = reverse("api:siae-retrieve-by-siret", args=["123 123 123 12345"])  # anonymous user
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_should_return_siae_if_siret_known(self):
-        url = reverse("api:siae-retrieve-by-siret", args=["12312312312345"])
+        url = reverse("api:siae-retrieve-by-siret", args=["12312312312345"])  # anonymous user
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         # self.assertEqual(type(response.data), dict)
@@ -38,16 +105,16 @@ class SiaeRetrieveBySiretApiTest(TestCase):
 
 
 class SiaeChoicesApiTest(TestCase):
-    def test_should_return_siae_kinds(self):
-        url = reverse("api:siae-kinds-list")
+    def test_should_return_siae_kinds_list(self):
+        url = reverse("api:siae-kinds-list")  # anonymous user
         response = self.client.get(url)
         self.assertEqual(response.data["count"], 8)
         self.assertEqual(len(response.data["results"]), 8)
         self.assertTrue("id" in response.data["results"][0])
         self.assertTrue("name" in response.data["results"][0])
 
-    def test_should_return_siae_presta_types(self):
-        url = reverse("api:siae-presta-types-list")
+    def test_should_return_siae_presta_types_list(self):
+        url = reverse("api:siae-presta-types-list")  # anonymous user
         response = self.client.get(url)
         self.assertEqual(response.data["count"], 3)
         self.assertEqual(len(response.data["results"]), 3)
