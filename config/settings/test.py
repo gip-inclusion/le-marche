60ccb26a8da74ec5d9f632b0dfca403d5f7f812a
Raphael Odini <raphodn@users.noreply.github.com>
2021-10-25 08:26:13 +0200

    Fix des Github Actions (#107)
    * Tentative fix of Github Actions CI
    
    * Add missing migration

--- config/settings/test.py
+++ config/settings/test.py
@@ -7,4 +7,8 @@ from .base import *  # noqa
 # https://docs.python.org/3/library/logging.html#logging.disable
 logging.disable(logging.CRITICAL)
 
+# `ManifestStaticFilesStorage` (used in base settings) requires `collectstatic` to be run.
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+COMPRESS_OFFLINE = False
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
