0ed781d78b693bf8a0424fbbab708e252d7cb829
Raphael Odini <raphodn@users.noreply.github.com>
2021-10-27 17:17:20 +0200

    Fix again contact form from & reply_to

--- lemarche/www/pages/tasks.py
+++ lemarche/www/pages/tasks.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage  # send_mail
 from django.template.loader import render_to_string
 
 
@@ -9,10 +9,20 @@ def send_contact_form_email(contact_form_dict):
     email_subject = email_subject_prefix + "Marché de l'inclusion : demande d'information"
     email_body = render_to_string("pages/contact_form_email_body.txt", {"form_dict": contact_form_dict})
 
-    send_mail(
+    # send_mail(
+    #     subject=email_subject,
+    #     message=email_body,
+    #     from_email=settings.NOTIFY_EMAIL,
+    #     recipient_list=[settings.CONTACT_EMAIL],
+    #     reply_to=[contact_form_dict["email"]],
+    #     fail_silently=False,
+    # )
+
+    email = EmailMessage(
         subject=email_subject,
-        message=email_body,
-        from_email=contact_form_dict["email"],
-        recipient_list=[settings.CONTACT_EMAIL],
-        fail_silently=False,
+        body=email_body,
+        from_email=settings.NOTIFY_EMAIL,
+        to=[settings.CONTACT_EMAIL],
+        reply_to=[contact_form_dict["email"]],
     )
+    email.send(fail_silently=False)
