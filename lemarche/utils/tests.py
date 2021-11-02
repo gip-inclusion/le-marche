cf1a2e07ae099ac8e410b9e88578f677bdf429cf
Raphael Odini <raphodn@users.noreply.github.com>
2021-11-02 12:39:50 +0100

    Update signup welcome message. Add tests on email whitelisting

--- lemarche/utils/tests.py
+++ lemarche/utils/tests.py
@@ -3,6 +3,8 @@ from unittest import mock
 from django.test import TestCase
 from django.urls import reverse
 
+from lemarche.utils.emails import whitelist_recipient_list
+
 
 def mock_track(path, action, **kargs):
     return "track"
@@ -33,3 +35,11 @@ class TrackerTest(TestCase):
         url = reverse("pages:home")
         self.client.get(url, HTTP_USER_AGENT=bot_ua)
         self.assertEqual(mock_track.call_count, 0)
+
+
+class EmailTest(TestCase):
+    def should_filter_out_non_betagouv_emails_when_not_in_prod(self):
+        email_list = ["test@beta.gouv.fr", "test@example.com"]
+        email_list_filtered = whitelist_recipient_list(email_list)
+        self.assertTrue("test@beta.gouv.fr" in email_list_filtered)
+        self.assertTrue("test@example.com" not in email_list_filtered)
