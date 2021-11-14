32cce5fad28003c79ca3b03f2fbd7e137fd40f0f
Raphael Odini <raphodn@users.noreply.github.com>
2021-11-14 16:14:12 +0100

    Update readme. cleanup doc

--- env.default.sh
+++ env.default.sh
@@ -2,13 +2,6 @@
 # If you wish to change these, update lemarche/settings.py
 # to reflect the changes
 
-# Mysql Config
-export MYSQL_ADDON_HOST="localhost"
-export MYSQL_ADDON_PORT="3306"
-export MYSQL_ADDON_USER=""
-export MYSQL_ADDON_PASSWORD=""
-export MYSQL_ADDON_DB=""
-
 # PostGreSQL Config
 export POSTGRESQL_ADDON_HOST="localhost"
 export POSTGRESQL_ADDON_PORT="5432"
