105752eb1d512e80ba577451383bc485c1226d72
Raphael Odini <raphodn@users.noreply.github.com>
2021-11-21 21:30:17 +0100

    Missing migrations

--- lemarche/siaes/migrations/0034_siae_c1_sync_rename_fields.py
+++ lemarche/siaes/migrations/0034_siae_c1_sync_rename_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("siaes", "0032_avoid_null_on_charfields"),
+        ("siaes", "0033_alter_siae_contact_email"),
     ]
 
     operations = [
