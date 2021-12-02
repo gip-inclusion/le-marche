d8d587feadd53a7853feb543803d729cc6c61ec2
Madjid.A <madjid@udez.fr>
2021-12-02 15:46:20 +0100

    include feedback from the review

--- lemarche/utils/templatetags/theme_inclusion.py
+++ lemarche/utils/templatetags/theme_inclusion.py
@@ -1,5 +1,7 @@
 """
 https://docs.djangoproject.com/en/dev/howto/custom-template-tags/
+Exist also in the base code of C1 :
+    https://github.com/betagouv/itou/blob/master/itou/utils/templatetags/theme_inclusion.py
 """
 from django import template
 from django.templatetags.static import static
