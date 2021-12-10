9ca763cd7ade0d20d1aa427a9a65a5ea99eb14ea
David Deloo <hello@hellodeloo.com>
2021-12-10 09:55:13 +0100

    upd: add partners & remove slider on hp (#150)
    * upd: add partners & remove slider on hp
    
    * fix: put back again loading="lazy"

--- lemarche/static/js/home_partner_slider.js
+++ lemarche/static/js/home_partner_slider.js
@@ -1,33 +1,3 @@
-$('#slickPartnersHP').slick({
-  infinite: false,
-  adaptiveHeight: true,
-  speed: 300,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  dots: false,
-  arrows: true,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false
-      }
-    }
-  ]
-});
-
 $('#slickSolutionsHP').slick({
   infinite: false,
   adaptiveHeight: true,
