21bd3cedeef6b9f639cef3388ce413e66ba778d5
Raphael Odini <raphodn@users.noreply.github.com>
2021-12-13 12:31:35 +0100

    Add cron config for C1 Sync & API Entreprise data

---
+++ clevercloud/siaes_sync_c1_c4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+# Fetch new siaes from C1 + update existing
+
+# Do not run if this env var is not set:
+if [[ -z "$CRON_SYNC_C1_C4_ENABLED" ]]; then
+    exit 0
+fi
+
+# About clever cloud cronjobs:
+# https://www.clever-cloud.com/doc/tools/crons/
+
+if [[ "$INSTANCE_NUMBER" != "0" ]]; then
+    echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
+    exit 0
+fi
+
+# $APP_HOME is set by default by clever cloud.
+cd $APP_HOME
+
+django-admin sync_c1_c4
