7a5a68b5a1c13a3de4fd1d7f4f470e9008457f36
madjid.asa <madjid.asa@gmail.com>
2021-12-28 17:04:31 +0100

    ensure always https

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -21,3 +21,5 @@ ALLOWED_HOSTS = [
 # ------------------------------------------------------------------------------
 
 sentry_init(dsn=env.str("SENTRY_DSN_STAGING"))
+
+SECURE_SSL_REDIRECT = env.str("SECURE_SSL_REDIRECT", True)
