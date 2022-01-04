b2327b2d95f3b921a80b696485b9f6e246d4ffc8
Raphael Odini <raphodn@users.noreply.github.com>
2022-01-04 17:38:40 +0100

    Add user link in FavoriteList list

--- lemarche/favorites/admin.py
+++ lemarche/favorites/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html
 from fieldsets_with_inlines import FieldsetsInlineMixin
 
 from lemarche.favorites.models import FavoriteItem, FavoriteList
@@ -14,7 +16,7 @@ class FavoriteItemInline(admin.TabularInline):
 
 @admin.register(FavoriteList)
 class FavoriteListAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
-    list_display = ["id", "name", "user", "nb_siaes", "created_at", "updated_at"]
+    list_display = ["id", "name", "user_with_link", "nb_siaes", "created_at", "updated_at"]
     search_fields = ["id", "name", "slug", "user__id"]
 
     autocomplete_fields = ["user"]
@@ -40,6 +42,13 @@ class FavoriteListAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
         qs = super().get_queryset(request)
         qs = qs.annotate(siae_count=Count("siaes", distinct=True))
         return qs
+
+    def user_with_link(self, instance):
+        url = reverse("admin:users_user_change", args=[instance.user_id])
+        return format_html(f'<a href="{url}">{instance.user}</a>')
+
+    user_with_link.short_description = "Utilisateur"
+    user_with_link.admin_order_field = "user"
 
     def nb_siaes(self, favorite_list):
         # url = reverse("admin:siaes_siae_changelist") + f"?favorite_list__id__exact={favorite_list.id}"
