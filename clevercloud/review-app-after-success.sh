5fbdb304a3b42cb67304a20ac05adcace2dc433f
Raphael Odini <raphodn@users.noreply.github.com>
2022-01-04 11:23:57 +0100

    Add perimeters.sql to speed up review app creation

--- clevercloud/review-app-after-success.sh
+++ clevercloud/review-app-after-success.sh
@@ -13,9 +13,10 @@ if [ "$SKIP_FIXTURES" = true ] ; then
 fi
 
 echo "Loading perimeters"
-django-admin import_regions
-django-admin import_departements
-django-admin import_communes
+# django-admin import_regions
+# django-admin import_departements
+# django-admin import_communes
+PGPASSWORD=$POSTGRESQL_ADDON_PASSWORD pg_restore -d $POSTGRESQL_ADDON_DB -h $POSTGRESQL_ADDON_HOST -p $POSTGRESQL_ADDON_PORT -U $POSTGRESQL_ADDON_USER --if-exists --clean --no-owner --no-privileges $APP_HOME/lemarche/perimeters/management/commands/data/perimeters_20220104.sql
 
 # `ls $APP_HOME` does not work as the current user
 # does not have execution rights on the $APP_HOME directory.
