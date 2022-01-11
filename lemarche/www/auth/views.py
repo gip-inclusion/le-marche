c80dbe5bcbb6f74faf729a8a0e7fbb2c4b716c35
Raphael Odini <raphodn@users.noreply.github.com>
2022-01-11 10:37:44 +0100

    Pass next_url to signup when on login page

--- lemarche/www/auth/views.py
+++ lemarche/www/auth/views.py
@@ -31,20 +31,28 @@ class LoginView(auth_views.LoginView):
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        """Show post-migration message to existing users (they need to reset their password)."""
+        """
+        - Show post-migration message to existing users (they need to reset their password).
+        - Get next param to append to the signup url
+        """
         context = super().get_context_data(**kwargs)
         email = self.request.POST.get("username", "")
         if email:
             user = User.objects.filter(email=email).first()
             if user:
                 context["email_exists_password_empty"] = True if not getattr(user, "password", "") else False
+        next_url = self.request.GET.get("next", None)
+        if next_url:
+            context["next_param"] = f"?next={next_url}"
         return context
 
     def get_success_url(self):
-        """Redirect to next_url if there is a next param."""
+        """
+        Redirect to next_url if there is a next param.
+        We sanitize the url to avoid hacks.
+        """
         success_url = super().get_success_url()
         next_url = self.request.GET.get("next", None)
-        # sanitize next_url
         if next_url:
             safe_url = get_safe_url(self.request, param_name="next")
             if safe_url:
