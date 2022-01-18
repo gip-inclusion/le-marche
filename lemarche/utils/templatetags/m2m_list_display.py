9d41db59fdc4c458b6b5b548731067d42d5e5ecb
Raphael Odini <raphodn@users.noreply.github.com>
2022-01-18 15:01:39 +0100

    Refactor m2m_list_display

--- lemarche/utils/templatetags/m2m_list_display.py
+++ lemarche/utils/templatetags/m2m_list_display.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.encoding import force_str
+from django.utils.html import mark_safe
 
 from lemarche.siaes.models import Siae
 
@@ -8,16 +9,28 @@ register = template.Library()
 
 
 @register.simple_tag
-def m2m_list_display(obj, field, display_max=5):
+def m2m_list_display(obj, field, display_max=5, output_format="string"):
     """Pretty rendering of M2M fields."""
 
     if type(obj) == Siae:
         if field == "sectors":
             qs = obj.sectors.all()
 
-    values = [force_str(elem.name) for elem in list(qs) if elem.name != "Autre"]
-    values_to_string = ", ".join(values[:display_max])
-    if len(values) > display_max:
-        values_to_string += ", ..."
+    # get values
+    values = list(qs)
 
-    return values_to_string
+    # get list of names
+    values = [force_str(elem.name) for elem in values if elem.name != "Autre"]
+
+    # filter number of displayed values
+    if len(values) > display_max:
+        values = values[:display_max]
+        values.append("…")
+
+    # output format
+    if output_format == "list":
+        return values
+    elif output_format == "li":
+        return mark_safe("".join([f"<li>{elem}</li>" for elem in values]))
+    else:  # "string"
+        return ", ".join(values)
