e8ce63c4be19592d65dc412c8382c4e6981afcb3
Raphael Odini <raphodn@users.noreply.github.com>
2022-01-25 14:21:27 +0100

    Add siret validation

--- lemarche/siaes/management/commands/import_sep.py
+++ lemarche/siaes/management/commands/import_sep.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 from lemarche.sectors.models import Sector
 from lemarche.siaes.constants import DEPARTMENT_TO_REGION, department_from_postcode
 from lemarche.siaes.models import Siae
+from lemarche.siaes.validators import validate_siret
 from lemarche.utils.apis.geocoding import get_geocoding_data
 from lemarche.utils.data import rename_dict_key, reset_app_sql_sequences
 
@@ -105,7 +106,8 @@ class Command(BaseCommand):
         if "siret" in siae:
             siae["siret"].strip()
             siae["siret"] = siae["siret"].replace(" ", "").replace(" ", "")
-            siae["siret_is_valid"] = True
+            if validate_siret(siae["siret"]):
+                siae["siret_is_valid"] = True
 
         # presta_type
         siae["presta_type"] = list()
