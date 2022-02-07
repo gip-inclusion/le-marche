2cc3966f26ba0d995fa6a5e4ba4b461cba79554c
Raphael Odini <raphodn@users.noreply.github.com>
2022-02-07 14:40:11 +0100

    Ajouter un onglet collaborateurs au formulaire de modification de sa structure (#211)
    * First simple Siae collaborators tab
    
    * Order collaborators by last added
    
    * Redirect to collaborator view by default. Update tests

--- lemarche/www/dashboard/tests.py
+++ lemarche/www/dashboard/tests.py
@@ -118,16 +118,17 @@ class SiaeEditView(TestCase):
         url = reverse("dashboard:siae_edit", args=[self.siae_with_user.slug])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, f"/profil/prestataires/{self.siae_with_user.slug}/modifier/info-contact/")
+        self.assertEqual(response.url, f"/profil/prestataires/{self.siae_with_user.slug}/modifier/collaborateurs/")
 
         self.client.login(email=self.other_user_siae.email, password=DEFAULT_PASSWORD)
         url = reverse("dashboard:siae_edit", args=[self.siae_with_user.slug])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        # self.assertEqual(response.url, "/profil/")  # redirects first to siae_edit_info_contact
+        # self.assertEqual(response.url, "/profil/")  # redirects first to siae_edit_users
 
     def test_only_siae_user_can_edit_siae_tabs(self):
         SIAE_EDIT_URLS = [
+            "dashboard:siae_edit_users",
             "dashboard:siae_edit_info_contact",
             "dashboard:siae_edit_offer",
             "dashboard:siae_edit_presta",
