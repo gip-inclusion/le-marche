e09079dbfbad039a5012dd761fd10b584e49b7a7
Raphael Odini <raphodn@users.noreply.github.com>
2022-02-25 11:25:25 +0100

    Collaborateurs (5/6) : basculer tous les emails sur des templates Mailjet (#229)
    * Add method to send transactional emails with Mailjet
    
    * Replace first email with Mailjet template email
    
    * Replace assignee relance emails with Mailjet template
    
    * Replace initiator response emails with Mailjet templates
    
    * Replace initiator relance emails with Mailjet templates
    
    * Fix recipient_list index out of range
    
    * Don't send Mailjet template emails in dev. Add async to send method
    
    * Continue replacing user by initiator
    
    * Move Mailjet template_ids to env variables

--- lemarche/www/dashboard/tasks.py
+++ lemarche/www/dashboard/tasks.py
@@ -1,90 +1,87 @@
 from django.conf import settings
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.utils import timezone
 
+from lemarche.utils.apis import api_mailjet
 from lemarche.utils.emails import whitelist_recipient_list
 from lemarche.utils.urls import get_domain_url
 
 
-# TODO: make async (celery)
-def send_siae_user_request_email(siae_user_request):
+EMAIL_SUBJECT_PREFIX = f"[{settings.BITOUBI_ENV.upper()}] " if settings.BITOUBI_ENV != "prod" else ""
+
+
+def send_siae_user_request_email_to_assignee(siae_user_request):
     """
     Send request to the assignee
     """
-    email_subject_prefix = f"[{settings.BITOUBI_ENV.upper()}] " if settings.BITOUBI_ENV != "prod" else ""
-    email_subject = (
-        email_subject_prefix + f"{siae_user_request.initiator.full_name} souhaite se rattacher à votre structure"
-    )
-    email_body = render_to_string(
-        "dashboard/siae_user_request_email_body.txt",
-        {
-            "assignee_full_name": siae_user_request.assignee.full_name,
-            "user_full_name": siae_user_request.initiator.full_name,
-            "siae_name": siae_user_request.siae.name_display,
-            "siae_edit_users_url": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
-        },
-    )
+    email_subject = EMAIL_SUBJECT_PREFIX + "Nouveau collaborateur"
     recipient_list = whitelist_recipient_list([siae_user_request.assignee.email])
+    recipient_email = recipient_list[0] if recipient_list else ""
+    recipient_name = siae_user_request.assignee.full_name
 
-    send_mail(
+    variables = {
+        "ASSIGNEE_FULL_NAME": siae_user_request.assignee.full_name,
+        "INITIATOR_FULL_NAME": siae_user_request.initiator.full_name,
+        "SIAE_NAME": siae_user_request.siae.name_display,
+        "SIAE_EDIT_USERS_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
+    }
+
+    api_mailjet.send_transactional_email_with_template(
+        template_id=settings.MAILJET_SIAEUSERREQUEST_ASSIGNEE_TEMPLATE_ID,
         subject=email_subject,
-        message=email_body,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=recipient_list,
-        fail_silently=False,
+        recipient_email=recipient_email,
+        recipient_name=recipient_name,
+        variables=variables,
     )
 
     # log email
     log_item = {
         "action": "email_sent",
-        "email_to": recipient_list,
+        "email_to": recipient_email,
         "email_subject": email_subject,
-        "email_body": email_body,
+        # "email_body": email_body,
         "email_timestamp": timezone.now().isoformat(),
     }
     siae_user_request.logs.append(log_item)
     siae_user_request.save()
 
 
-def send_siae_user_request_response_email(siae_user_request, response=None):
+def send_siae_user_request_response_email_to_initiator(siae_user_request, response=None):
     """
     Send request response (True or False) to the initial user
     """
-    email_subject_prefix = f"[{settings.BITOUBI_ENV.upper()}] " if settings.BITOUBI_ENV != "prod" else ""
-    email_subject_text = "Nouveau collaborateur" if response else "Rattachement refusé"
-    email_subject = email_subject_prefix + email_subject_text
-    email_template = (
-        "dashboard/siae_user_request_response_true_email_body.txt"
+    email_subject_text = "Accéder à votre structure" if response else "Rattachement refusé"
+    email_subject = EMAIL_SUBJECT_PREFIX + email_subject_text
+    email_template_id = (
+        settings.MAILJET_SIAEUSERREQUEST_INITIATOR_RESPONSE_POSITIVE_TEMPLATE_ID
         if response
-        else "dashboard/siae_user_request_response_false_email_body.txt"
-    )
-    email_body = render_to_string(
-        email_template,
-        {
-            "assignee_full_name": siae_user_request.assignee.full_name,
-            "user_full_name": siae_user_request.initiator.full_name,
-            "siae_name": siae_user_request.siae.name_display,
-            "siae_edit_users_url": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
-        },
+        else settings.MAILJET_SIAEUSERREQUEST_INITIATOR_RESPONSE_NEGATIVE_TEMPLATE_ID
     )
     recipient_list = whitelist_recipient_list([siae_user_request.initiator.email])
+    recipient_email = recipient_list[0] if recipient_list else ""
+    recipient_name = siae_user_request.initiator.full_name
 
-    send_mail(
+    variables = {
+        "ASSIGNEE_FULL_NAME": siae_user_request.assignee.full_name,
+        "INITIATOR_FULL_NAME": siae_user_request.initiator.full_name,
+        "SIAE_NAME": siae_user_request.siae.name_display,
+        "SIAE_EDIT_USERS_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
+    }
+
+    api_mailjet.send_transactional_email_with_template(
+        template_id=email_template_id,
         subject=email_subject,
-        message=email_body,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=recipient_list,
-        fail_silently=False,
+        recipient_email=recipient_email,
+        recipient_name=recipient_name,
+        variables=variables,
     )
 
     # log email
     log_item = {
         "action": "email_sent",
-        "email_to": recipient_list,
+        "email_to": recipient_email,
         "email_subject": email_subject,
-        "email_body": email_body,
+        # "email_body": email_body,
         "email_timestamp": timezone.now().isoformat(),
     }
     siae_user_request.logs.append(log_item)
@@ -97,50 +94,70 @@ def send_siae_user_request_reminder_3_days_emails(siae_user_request):
     - the initial user
     - to the assignee
     """
-    send_siae_user_request_reminder_3_days_email(siae_user_request, email_to="assignee")
-    send_siae_user_request_reminder_3_days_email(siae_user_request, email_to="initiator")
+    send_siae_user_request_reminder_3_days_email_to_assignee(siae_user_request)
+    send_siae_user_request_reminder_3_days_email_to_initiator(siae_user_request)
 
 
-def send_siae_user_request_reminder_3_days_email(siae_user_request, email_to):
-    """
-    Send request reminder (after 3 days)
-    """
-    email_subject_prefix = f"[{settings.BITOUBI_ENV.upper()}] " if settings.BITOUBI_ENV != "prod" else ""
-    email_subject_text = "Nouveau collaborateur" if email_to == "assignee" else "Rattachement sans réponse"
-    email_subject = email_subject_prefix + email_subject_text
-    email_template = (
-        "dashboard/siae_user_request_reminder_3_days_assignee_email_body.txt"
-        if email_to == "assignee"
-        else "dashboard/siae_user_request_reminder_3_days_initiator_email_body.txt"
-    )
-    email_body = render_to_string(
-        email_template,
-        {
-            "assignee_full_name": siae_user_request.assignee.full_name,
-            "user_full_name": siae_user_request.initiator.full_name,
-            "siae_name": siae_user_request.siae.name_display,
-            "siae_edit_users_url": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
-        },
-    )
-    if email_to == "assignee":
-        recipient_list = whitelist_recipient_list([siae_user_request.assignee.email])
-    else:
-        recipient_list = whitelist_recipient_list([siae_user_request.initiator.email])
+def send_siae_user_request_reminder_3_days_email_to_assignee(siae_user_request):
+    email_subject = EMAIL_SUBJECT_PREFIX + "Nouveau collaborateur"
+    recipient_list = whitelist_recipient_list([siae_user_request.assignee.email])
+    recipient_email = recipient_list[0] if recipient_list else ""
+    recipient_name = siae_user_request.assignee.full_name
 
-    send_mail(
+    variables = {
+        "ASSIGNEE_FULL_NAME": siae_user_request.assignee.full_name,
+        "INITIATOR_FULL_NAME": siae_user_request.initiator.full_name,
+        "SIAE_NAME": siae_user_request.siae.name_display,
+        "SIAE_EDIT_USERS_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
+    }
+
+    api_mailjet.send_transactional_email_with_template(
+        template_id=settings.MAILJET_SIAEUSERREQUEST_REMINDER_1_ASSIGNEE_TEMPLATE_ID,
         subject=email_subject,
-        message=email_body,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=recipient_list,
-        fail_silently=False,
+        recipient_email=recipient_email,
+        recipient_name=recipient_name,
+        variables=variables,
     )
 
     # log email
     log_item = {
         "action": "email_sent",
-        "email_to": recipient_list,
+        "email_to": recipient_email,
         "email_subject": email_subject,
-        "email_body": email_body,
+        # "email_body": email_body,
+        "email_timestamp": timezone.now().isoformat(),
+    }
+    siae_user_request.logs.append(log_item)
+    siae_user_request.save()
+
+
+def send_siae_user_request_reminder_3_days_email_to_initiator(siae_user_request):
+    email_subject = EMAIL_SUBJECT_PREFIX + "Rattachement sans réponse"
+    recipient_list = whitelist_recipient_list([siae_user_request.initiator.email])
+    recipient_email = recipient_list[0] if recipient_list else ""
+    recipient_name = siae_user_request.initiator.full_name
+
+    variables = {
+        "ASSIGNEE_FULL_NAME": siae_user_request.assignee.full_name,
+        "INITIATOR_FULL_NAME": siae_user_request.initiator.full_name,
+        "SIAE_NAME": siae_user_request.siae.name_display,
+        "SIAE_EDIT_USERS_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
+    }
+
+    api_mailjet.send_transactional_email_with_template(
+        template_id=settings.MAILJET_SIAEUSERREQUEST_REMINDER_1_INITIATOR_TEMPLATE_ID,
+        subject=email_subject,
+        recipient_email=recipient_email,
+        recipient_name=recipient_name,
+        variables=variables,
+    )
+
+    # log email
+    log_item = {
+        "action": "email_sent",
+        "email_to": recipient_email,
+        "email_subject": email_subject,
+        # "email_body": email_body,
         "email_timestamp": timezone.now().isoformat(),
     }
     siae_user_request.logs.append(log_item)
@@ -153,51 +170,72 @@ def send_siae_user_request_reminder_8_days_emails(siae_user_request):
     - the initial user
     - to the assignee
     """
-    send_siae_user_request_reminder_8_days_email(siae_user_request, email_to="assignee")
-    send_siae_user_request_reminder_8_days_email(siae_user_request, email_to="initiator")
+    send_siae_user_request_reminder_8_days_email_to_assignee(siae_user_request)
+    send_siae_user_request_reminder_8_days_email_to_initiator(siae_user_request)
 
 
-def send_siae_user_request_reminder_8_days_email(siae_user_request, email_to):
-    """
-    Send request reminder (after 8 days)
-    """
-    email_subject_prefix = f"[{settings.BITOUBI_ENV.upper()}] " if settings.BITOUBI_ENV != "prod" else ""
-    email_subject_text = "Nouveau collaborateur" if email_to == "assignee" else "Rattachement sans réponse"
-    email_subject = email_subject_prefix + email_subject_text
-    email_template = (
-        "dashboard/siae_user_request_reminder_8_days_assignee_email_body.txt"
-        if email_to == "assignee"
-        else "dashboard/siae_user_request_reminder_8_days_initiator_email_body.txt"
-    )
-    email_body = render_to_string(
-        email_template,
-        {
-            "assignee_full_name": siae_user_request.assignee.full_name,
-            "user_full_name": siae_user_request.initiator.full_name,
-            "siae_name": siae_user_request.siae.name_display,
-            "siae_edit_users_url": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
-            "support_url": f"https://{get_domain_url()}{reverse_lazy('pages:contact')}?siret={siae_user_request.siae.siret}",  # noqa
-        },
-    )
-    if email_to == "assignee":
-        recipient_list = whitelist_recipient_list([siae_user_request.assignee.email])
-    else:
-        recipient_list = whitelist_recipient_list([siae_user_request.initiator.email])
+def send_siae_user_request_reminder_8_days_email_to_assignee(siae_user_request):
+    email_subject = EMAIL_SUBJECT_PREFIX + "Nouveau collaborateur"
+    recipient_list = whitelist_recipient_list([siae_user_request.assignee.email])
+    recipient_email = recipient_list[0] if recipient_list else ""
+    recipient_name = siae_user_request.assignee.full_name
 
-    send_mail(
+    variables = {
+        "ASSIGNEE_FULL_NAME": siae_user_request.assignee.full_name,
+        "INITIATOR_FULL_NAME": siae_user_request.initiator.full_name,
+        "SIAE_NAME": siae_user_request.siae.name_display,
+        "SIAE_EDIT_USERS_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
+        # "SUPPORT_URL": f"https://{get_domain_url()}{reverse_lazy('pages:contact')}?siret={siae_user_request.siae.siret}",  # noqa
+    }
+
+    api_mailjet.send_transactional_email_with_template(
+        template_id=settings.MAILJET_SIAEUSERREQUEST_REMINDER_2_ASSIGNEE_TEMPLATE_ID,
         subject=email_subject,
-        message=email_body,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=recipient_list,
-        fail_silently=False,
+        recipient_email=recipient_email,
+        recipient_name=recipient_name,
+        variables=variables,
     )
 
     # log email
     log_item = {
         "action": "email_sent",
-        "email_to": recipient_list,
+        "email_to": recipient_email,
         "email_subject": email_subject,
-        "email_body": email_body,
+        # "email_body": email_body,
+        "email_timestamp": timezone.now().isoformat(),
+    }
+    siae_user_request.logs.append(log_item)
+    siae_user_request.save()
+
+
+def send_siae_user_request_reminder_8_days_email_to_initiator(siae_user_request):
+    email_subject = EMAIL_SUBJECT_PREFIX + "Rattachement sans réponse"
+    recipient_list = whitelist_recipient_list([siae_user_request.initiator.email])
+    recipient_email = recipient_list[0] if recipient_list else ""
+    recipient_name = siae_user_request.initiator.full_name
+
+    variables = {
+        "ASSIGNEE_FULL_NAME": siae_user_request.assignee.full_name,
+        "INITIATOR_FULL_NAME": siae_user_request.initiator.full_name,
+        "SIAE_NAME": siae_user_request.siae.name_display,
+        "SIAE_EDIT_USERS_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_users', args=[siae_user_request.siae.slug])}",  # noqa
+        "SUPPORT_URL": f"https://{get_domain_url()}{reverse_lazy('pages:contact')}?siret={siae_user_request.siae.siret}",  # noqa
+    }
+
+    api_mailjet.send_transactional_email_with_template(
+        template_id=settings.MAILJET_SIAEUSERREQUEST_REMINDER_2_INITIATOR_TEMPLATE_ID,
+        subject=email_subject,
+        recipient_email=recipient_email,
+        recipient_name=recipient_name,
+        variables=variables,
+    )
+
+    # log email
+    log_item = {
+        "action": "email_sent",
+        "email_to": recipient_email,
+        "email_subject": email_subject,
+        # "email_body": email_body,
         "email_timestamp": timezone.now().isoformat(),
     }
     siae_user_request.logs.append(log_item)
