849e7ec3208ad3a6d47bd356b70706f1bfe72672
madjid.asa <madjid.asa@gmail.com>
2022-03-07 16:33:32 +0100

    update url

--- lemarche/www/tenders/urls.py
+++ lemarche/www/tenders/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from lemarche.www.tenders.views import AddTenderView
+from lemarche.www.tenders.views import TenderAddView
 
 
 # https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs
 app_name = "tenders"
 
 urlpatterns = [
-    path("ajouter", AddTenderView.as_view(), name="add"),
+    path("ajouter", TenderAddView.as_view(), name="add"),
 ]
