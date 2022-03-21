96eb037ee48db59dcb95e96abb5715ccc955a002
Raphael Odini <raphodn@users.noreply.github.com>
2022-03-21 17:22:19 +0100

    Add contact_social_website field to Siae & SiaeGroup

--- lemarche/siaes/models.py
+++ lemarche/siaes/models.py
@@ -62,6 +62,9 @@ class SiaeGroup(models.Model):
     )
     contact_email = models.EmailField(verbose_name="E-mail", blank=True)
     contact_phone = models.CharField(verbose_name="Téléphone", max_length=150, blank=True)
+    contact_social_website = models.URLField(
+        verbose_name="Réseau social", help_text="Doit commencer par http:// ou https://", blank=True
+    )
 
     logo_url = models.URLField(verbose_name="Lien vers le logo", max_length=500, blank=True)
 
@@ -462,6 +465,9 @@ class Siae(models.Model):
     )
     contact_email = models.EmailField(verbose_name="E-mail", blank=True)
     contact_phone = models.CharField(verbose_name="Téléphone", max_length=150, blank=True)
+    contact_social_website = models.URLField(
+        verbose_name="Réseau social", help_text="Doit commencer par http:// ou https://", blank=True
+    )
 
     image_name = models.CharField(verbose_name="Nom de l'image", max_length=255, blank=True)
     logo_url = models.URLField(verbose_name="Lien vers le logo", max_length=500, blank=True)
