a001b2e8e9692974d5223b7ae073c1c12431bdf3
madjid.asa <madjid.asa@gmail.com>
2022-03-23 17:40:59 +0100

    article list

--- lemarche/cms/models.py
+++ lemarche/cms/models.py
@@ -1,58 +1,54 @@
 from django.db import models
 from django.db.models import Q
-from modelcluster.fields import ParentalKey
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.fields import RichTextField
-from wagtail.core.models import Orderable, Page
+from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 
-class ArticlePage(Page):
-
-    # Database fields
-    intro = models.TextField()
-    body = RichTextField()
+class BaseArticle(Page):
+    intro = models.TextField(verbose_name="Introduction", null=True, blank=True)
     image = models.ForeignKey(
         "wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
     )
 
+    class Meta:
+        abstract = True
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+    ]
+
+    promote_panels = [
+        ImageChooserPanel("image"),
+        MultiFieldPanel(Page.promote_panels, "Configuration SEO"),
+    ]
+
+
+class ArticlePage(BaseArticle):
+
+    # Database fields
+    body = RichTextField()
+
     # Search index configuration
 
-    search_fields = Page.search_fields + [
+    search_fields = BaseArticle.search_fields + [
         index.FilterField("intro"),
         index.SearchField("body"),
     ]
 
     # Editor panels configuration
 
-    content_panels = Page.content_panels + [
-        FieldPanel("intro", classname="full"),
+    content_panels = BaseArticle.content_panels + [
         FieldPanel("body", classname="full"),
-        InlinePanel("related_links", label="Related links"),
-    ]
-
-    promote_panels = [
-        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
-        ImageChooserPanel("image"),
     ]
 
     # Parent page / subpage type rules
 
     parent_page_types = ["cms.ArticleList"]
     subpage_types = []
-
-
-class ArticlePageRelatedLink(Orderable):
-    page = ParentalKey(ArticlePage, on_delete=models.CASCADE, related_name="related_links")
-    name = models.CharField(max_length=255)
-    url = models.URLField()
-
-    panels = [
-        FieldPanel("name"),
-        FieldPanel("url"),
-    ]
 
 
 class ArticleList(RoutablePageMixin, Page):
@@ -64,7 +60,7 @@ class ArticleList(RoutablePageMixin, Page):
         return context
 
     def get_posts(self):
-        return ArticlePage.objects.descendant_of(self).live().order_by("-last_published_at")
+        return self.get_children().live().order_by("-last_published_at")
 
     @route(r"^$")
     def post_list(self, request, *args, **kwargs):
@@ -85,4 +81,20 @@ class ArticleList(RoutablePageMixin, Page):
         return Page.serve(self, request, *args, **kwargs)
 
     parent_page_types = ["wagtailcore.Page"]
-    subpage_types = ["cms.ArticlePage"]
+    subpage_types = ["cms.ArticlePage", "cms.StaticPage"]
+
+
+class StaticPage(BaseArticle):
+    template_name = models.CharField(
+        verbose_name="nom de la template",
+        help_text="Nom du fichier présent dans 'templates/cms/static', ex: valoriser-achats.html",
+        max_length=90,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.template = f"cms/static/{self.template_name}"
+
+    content_panels = BaseArticle.content_panels + [
+        FieldPanel("template_name", classname="full"),
+    ]
