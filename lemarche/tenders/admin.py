9f726c95b1cd3a3c1ca9b8a76138150a10760482
Raphael Odini <raphodn@users.noreply.github.com>
2022-03-24 19:00:40 +0100

    Improve tender display in admin

--- lemarche/tenders/admin.py
+++ lemarche/tenders/admin.py
@@ -9,13 +9,18 @@ class TenderAdmin(admin.ModelAdmin):
     list_display = [
         "id",
         "title",
+        "author",
         "kind",
         "deadline_date",
-        "response_kind",
         "start_working_date",
+        "response_kind",
+        "siae_found_count",
         "created_at",
     ]
-    list_filter = ["kind", "deadline_date", "response_kind", "start_working_date"]
+    list_filter = ["kind", "deadline_date", "start_working_date", "response_kind"]
     # filter on "perimeters"? (loads ALL the perimeters... Use django-admin-autocomplete-filter instead?)
     search_fields = ["id", "title"]
     search_help_text = "Cherche sur les champs : ID, Titre"
+    readonly_fields = ["siae_found_count"]
+
+    autocomplete_fields = ["perimeters", "sectors"]
