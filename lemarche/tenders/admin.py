9e446d1a3592829ffc05cd609bda40b4747326b1
Raphael Odini <raphodn@users.noreply.github.com>
2022-03-24 19:28:55 +0100

    Tender admin fieldset pour y voir plus clair

--- lemarche/tenders/admin.py
+++ lemarche/tenders/admin.py
@@ -21,6 +21,38 @@ class TenderAdmin(admin.ModelAdmin):
     # filter on "perimeters"? (loads ALL the perimeters... Use django-admin-autocomplete-filter instead?)
     search_fields = ["id", "title"]
     search_help_text = "Cherche sur les champs : ID, Titre"
-    readonly_fields = ["siae_found_count"]
+    readonly_fields = ["siae_found_count", "created_at", "updated_at"]
 
     autocomplete_fields = ["perimeters", "sectors"]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("title", "slug", "kind"),
+            },
+        ),
+        (
+            "Détails",
+            {
+                "fields": (
+                    "description",
+                    "constraints",
+                    "perimeters",
+                    "sectors",
+                    "external_link",
+                    "amount",
+                    "response_kind",
+                    "deadline_date",
+                    "start_working_date",
+                ),
+            },
+        ),
+        (
+            "Contact",
+            {
+                "fields": ("author", "contact_first_name", "contact_last_name", "contact_email", "contact_phone"),
+            },
+        ),
+        ("Info", {"fields": ("created_at", "updated_at")}),
+    )
