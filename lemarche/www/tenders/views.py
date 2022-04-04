6329c3a6819199b271c6b14a3ea199b1dad11eff
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-04 17:07:03 +0200

    Dépôt de besoins : stat sur le clic des coordonnées (#270)
    * Add tender contact_click stats in list display
    
    * Add button to show/hide tender contacts
    
    * Don't show collapse for tender author
    
    * Add ajax call on button click. Add new tender view
    
    * Hide contact button if user has already clicked
    
    * Add stats in admin
    
    * Add tests
    
    * Improve contact button look & feel

--- lemarche/www/tenders/views.py
+++ lemarche/www/tenders/views.py
@@ -2,11 +2,12 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseForbidden, HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.utils.safestring import mark_safe
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, View
 
 from lemarche.siaes.models import Siae
 from lemarche.tenders.models import Tender, TenderSiae
@@ -111,7 +112,7 @@ class TenderDetail(LoginRequiredMixin, DetailView):
         if self.request.user.kind == User.KIND_SIAE:
             tender = self.get_object()
             TenderSiae.objects.filter(
-                tender=tender, siae__in=self.request.user.siaes.all(), detail_display_date=None
+                tender=tender, siae__in=self.request.user.siaes.all(), detail_display_date__isnull=True
             ).update(detail_display_date=timezone.now())
         return super().get(request, *args, **kwargs)
 
@@ -119,4 +120,26 @@ class TenderDetail(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         user_kind = self.request.user.kind if self.request.user.is_authenticated else "anonymous"
         context["parent_title"] = TITLE_DETAIL_PAGE_SIAE if user_kind == User.KIND_SIAE else TITLE_DETAIL_PAGE_OTHERS
+        if self.request.user.kind == User.KIND_SIAE:
+            context["user_has_detail_display_date"] = TenderSiae.objects.filter(
+                tender=self.get_object(), siae__in=self.request.user.siaes.all(), detail_display_date__isnull=False
+            ).exists()
+            context["user_has_contact_click_date"] = TenderSiae.objects.filter(
+                tender=self.get_object(), siae__in=self.request.user.siaes.all(), contact_click_date__isnull=False
+            ).exists()
         return context
+
+
+class TenderDetailContactClickStat(LoginRequiredMixin, View):
+    def get_object(self):
+        return get_object_or_404(Tender, slug=self.kwargs.get("slug"))
+
+    def post(self, request, *args, **kwargs):
+        if self.request.user.kind == User.KIND_SIAE:
+            tender = self.get_object()
+            TenderSiae.objects.filter(
+                tender=tender, siae__in=self.request.user.siaes.all(), contact_click_date__isnull=True
+            ).update(contact_click_date=timezone.now())
+            return JsonResponse({"message": "success"})
+        else:
+            return HttpResponseForbidden()
