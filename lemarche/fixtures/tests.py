ba2ba4eef5c14ba629b32ede3d60b7973dd88853
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-08 12:09:01 +0200

    Recette jetable : enrichir les fixtures (#283)
    * Fix fixture tests
    
    * Add fixtures for SiaeGroup
    
    * Add fixtures for SiaeLabel
    
    * Add fixtures for SiaeOffer & SiaeClientReference
    
    * Add fixtures for Tenders

--- lemarche/fixtures/tests.py
+++ lemarche/fixtures/tests.py
@@ -1,24 +1,47 @@
-from django.core import management
 from django.test import TestCase
 
 from lemarche.networks.models import Network
 from lemarche.sectors.models import Sector, SectorGroup
-from lemarche.siaes.models import Siae, SiaeUser
+from lemarche.siaes.models import Siae, SiaeClientReference, SiaeGroup, SiaeLabel, SiaeOffer, SiaeUser
+from lemarche.tenders.models import Tender, TenderSiae
 from lemarche.users.models import User
 
 
 class FixturesTest(TestCase):
+    fixtures = [
+        "lemarche/fixtures/django/01_siaegroups.json",
+        "lemarche/fixtures/django/01_siaes.json",
+        "lemarche/fixtures/django/02_users.json",
+        "lemarche/fixtures/django/03_siae_users.json",
+        "lemarche/fixtures/django/04_sectorgroups.json",
+        "lemarche/fixtures/django/05_sectors.json",
+        "lemarche/fixtures/django/06_siae_sectors.json",
+        "lemarche/fixtures/django/06_siaegroup_sectors.json",
+        "lemarche/fixtures/django/07_networks.json",
+        "lemarche/fixtures/django/08_siae_networks.json",
+        "lemarche/fixtures/django/09_siaeclientreferences.json",
+        "lemarche/fixtures/django/09_siaelabels.json",
+        "lemarche/fixtures/django/09_siaeoffers.json",
+        "lemarche/fixtures/django/10_tenders.json",
+        "lemarche/fixtures/django/11_tender_sectors.json",
+        "lemarche/fixtures/django/11_tender_siaes.json",
+        "lemarche/fixtures/django/20_cms.json",
+    ]
+
     def test_flat_fixtures_load_successfully(self):
-        management.call_command("loaddata", "lemarche/fixtures/django/01_siaes.json")
-        management.call_command("loaddata", "lemarche/fixtures/django/02_users.json")
-        management.call_command("loaddata", "lemarche/fixtures/django/03_siae_users.json")
-        management.call_command("loaddata", "lemarche/fixtures/django/04_sectors.json")
-        management.call_command("loaddata", "lemarche/fixtures/django/05_siae_sectors.json")
-        management.call_command("loaddata", "lemarche/fixtures/django/06_networks.json")
-        management.call_command("loaddata", "lemarche/fixtures/django/07_siae_networks.json")
-        self.assertTrue(Siae.objects.count())
-        self.assertTrue(User.objects.count())
-        self.assertTrue(SiaeUser.objects.count())
-        self.assertTrue(Sector.objects.count())
-        self.assertTrue(SectorGroup.objects.count())
-        self.assertTrue(Network.objects.count())
+        # alternative?
+        # management.call_command("loaddata", "lemarche/fixtures/django/01_siaes.json")
+        # why not self.assertTrue(Siae.objects.all().count()) ?
+        # returns an error: django.db.utils.InternalError: variable not found in subplan target list
+        self.assertTrue(len(Siae.objects.all()) > 0)
+        self.assertTrue(len(SiaeGroup.objects.all()) > 0)
+        self.assertTrue(len(User.objects.all()) > 0)
+        self.assertTrue(len(SiaeUser.objects.all()) > 0)
+        self.assertTrue(len(Sector.objects.all()) > 0)
+        self.assertTrue(len(SectorGroup.objects.all()) > 0)
+        self.assertTrue(len(Network.objects.all()) > 0)
+        self.assertTrue(len(SiaeClientReference.objects.all()) > 0)
+        self.assertTrue(len(SiaeLabel.objects.all()) > 0)
+        self.assertTrue(len(SiaeOffer.objects.all()) > 0)
+        self.assertTrue(len(Tender.objects.all()) > 0)
+        self.assertTrue(len(TenderSiae.objects.all()) > 0)
