3a877f79504a273a28218592552fee4ae46cc60d
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-13 12:50:02 +0200

    Send email on first & +5 siae interested

--- lemarche/www/tenders/tasks.py
+++ lemarche/www/tenders/tasks.py
@@ -29,14 +29,14 @@ def send_tender_email_to_siae(tender: Tender, siae: Siae):
     recipient_list = whitelist_recipient_list([siae.contact_email])
     if recipient_list:
         recipient_email = recipient_list[0] if recipient_list else ""
-        recipient_name = tender.author.full_name
+        recipient_name = siae.contact_full_name
 
         variables = {
-            "FULL_NAME": siae.contact_first_name,
-            "BUYER_COMPANY": tender.author.company_name,
-            "RESPONSE_KIND": tender.get_kind_display(),
-            "SECTORS": tender.get_sectors_names,
-            "PERIMETERS": tender.get_perimeters_names,
+            "SIAE_CONTACT_FIRST_NAME": siae.contact_first_name,
+            "TENDER_AUTHOR_COMPANY": tender.author.company_name,
+            "TENDER_KIND": tender.get_kind_display(),
+            "TENDER_SECTORS": tender.get_sectors_names,
+            "TENDER_PERIMETERS": tender.get_perimeters_names,
             "TENDER_URL": f"https://{get_domain_url()}{tender.get_absolute_url()}",
         }
 
@@ -59,4 +59,30 @@ def send_siae_interested_email_to_author(tender: Tender):
     tender_siae_contact_click_count = TenderSiae.objects.filter(
         tender=tender, contact_click_date__isnull=False
     ).count()
-    print(tender_siae_contact_click_count)
+
+    if tender_siae_contact_click_count > 0:
+        if (tender_siae_contact_click_count == 1) or (tender_siae_contact_click_count % 5 == 0):
+            if tender_siae_contact_click_count == 1:
+                email_subject = EMAIL_SUBJECT_PREFIX + "Une première structure intéressée !"
+                template_id = settings.MAILJET_TENDERS_SIAE_INTERESTED_1_TEMPLATE_ID
+            else:
+                email_subject = EMAIL_SUBJECT_PREFIX + "5 nouvelles structures intéressées !"
+                template_id = settings.MAILJET_TENDERS_SIAE_INTERESTED_5_TEMPLATE_ID
+            recipient_list = whitelist_recipient_list([tender.author.email])  # tender.contact_email ?
+            if recipient_list:
+                recipient_email = recipient_list[0] if recipient_list else ""
+                recipient_name = tender.author.full_name
+
+                variables = {
+                    "TENDER_AUTHOR_FIRST_NAME": tender.author.first_name,
+                    "TENDER_TITLE": tender.title,
+                    "TENDER_SIAE_INTERESTED_LIST_URL": f"https://{get_domain_url()}{tender.get_absolute_url()}structures-interessees",  # noqa
+                }
+
+                api_mailjet.send_transactional_email_with_template(
+                    template_id=template_id,
+                    subject=email_subject,
+                    recipient_email=recipient_email,
+                    recipient_name=recipient_name,
+                    variables=variables,
+                )
