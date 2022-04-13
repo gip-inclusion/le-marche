231c3c4aefce56202d80ccd5a292ec2b3ba52ddf
Raphael Odini <raphodn@users.noreply.github.com>
2022-04-13 15:34:53 +0200

    Dépot de besoin : notifier l'acheteur de nouvelles structures intéressées (#293)
    * First step to send siae_interested notification emails
    
    * Send email on first & +5 siae interested

--- lemarche/www/tenders/tasks.py
+++ lemarche/www/tenders/tasks.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.utils import timezone
 
 from lemarche.siaes.models import Siae
-from lemarche.tenders.models import Tender
+from lemarche.tenders.models import Tender, TenderSiae
 from lemarche.utils.apis import api_mailjet
 from lemarche.utils.emails import whitelist_recipient_list
 from lemarche.utils.urls import get_domain_url
@@ -12,9 +12,9 @@ EMAIL_SUBJECT_PREFIX = f"[{settings.BITOUBI_ENV.upper()}] " if settings.BITOUBI_
 
 
 # @task()
-def send_tender_emails(tender: Tender):
+def send_tender_emails_to_siaes(tender: Tender):
     """
-    TODO: filter on source="EMAIL" ?
+    All corresponding Siae will be contacted
     """
     for siae in tender.siaes.all():
         send_tender_email_to_siae(tender, siae)
@@ -29,14 +29,14 @@ def send_tender_email_to_siae(tender: Tender, siae: Siae):
     recipient_list = whitelist_recipient_list([siae.contact_email])
     if recipient_list:
         recipient_email = recipient_list[0] if recipient_list else ""
-        recipient_name = tender.author.full_name
+        recipient_name = siae.contact_full_name
 
         variables = {
-            "FULL_NAME": siae.contact_first_name,
-            "BUYER_COMPANY": tender.author.company_name,
-            "RESPONSE_KIND": tender.get_kind_display(),
-            "SECTORS": tender.get_sectors_names,
-            "PERIMETERS": tender.get_perimeters_names,
+            "SIAE_CONTACT_FIRST_NAME": siae.contact_first_name,
+            "TENDER_AUTHOR_COMPANY": tender.author.company_name,
+            "TENDER_KIND": tender.get_kind_display(),
+            "TENDER_SECTORS": tender.get_sectors_names,
+            "TENDER_PERIMETERS": tender.get_perimeters_names,
             "TENDER_URL": f"https://{get_domain_url()}{tender.get_absolute_url()}",
         }
 
@@ -47,3 +47,42 @@ def send_tender_email_to_siae(tender: Tender, siae: Siae):
             recipient_name=recipient_name,
             variables=variables,
         )
+
+
+def send_siae_interested_email_to_author(tender: Tender):
+    """
+    The author is notified (by intervals) when new Siaes show interest (contact_click_date set)
+    Intervals:
+    - first Siae
+    - every 5 Siae
+    """
+    tender_siae_contact_click_count = TenderSiae.objects.filter(
+        tender=tender, contact_click_date__isnull=False
+    ).count()
+
+    if tender_siae_contact_click_count > 0:
+        if (tender_siae_contact_click_count == 1) or (tender_siae_contact_click_count % 5 == 0):
+            if tender_siae_contact_click_count == 1:
+                email_subject = EMAIL_SUBJECT_PREFIX + "Une première structure intéressée !"
+                template_id = settings.MAILJET_TENDERS_SIAE_INTERESTED_1_TEMPLATE_ID
+            else:
+                email_subject = EMAIL_SUBJECT_PREFIX + "5 nouvelles structures intéressées !"
+                template_id = settings.MAILJET_TENDERS_SIAE_INTERESTED_5_TEMPLATE_ID
+            recipient_list = whitelist_recipient_list([tender.author.email])  # tender.contact_email ?
+            if recipient_list:
+                recipient_email = recipient_list[0] if recipient_list else ""
+                recipient_name = tender.author.full_name
+
+                variables = {
+                    "TENDER_AUTHOR_FIRST_NAME": tender.author.first_name,
+                    "TENDER_TITLE": tender.title,
+                    "TENDER_SIAE_INTERESTED_LIST_URL": f"https://{get_domain_url()}{tender.get_absolute_url()}structures-interessees",  # noqa
+                }
+
+                api_mailjet.send_transactional_email_with_template(
+                    template_id=template_id,
+                    subject=email_subject,
+                    recipient_email=recipient_email,
+                    recipient_name=recipient_name,
+                    variables=variables,
+                )
