783316a993b4aa53eaa3d74a9f6a383ec9fe537a
Raphael Odini <raphodn@users.noreply.github.com>
2022-05-30 10:33:19 +0200

    Cache home page (every hour)

--- lemarche/www/pages/views.py
+++ lemarche/www/pages/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponsePermanentRedirect, JsonResponse
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import DetailView, FormView, ListView, TemplateView, View
 
 from lemarche.pages.models import Page
@@ -16,6 +18,7 @@ from lemarche.www.pages.forms import ContactForm
 from lemarche.www.pages.tasks import send_contact_form_email, send_contact_form_receipt
 
 
+@method_decorator(cache_page(60 * 60), name="dispatch")  # cache 1 hour
 class HomeView(TemplateView):
     template_name = "pages/home.html"
 
