6977496f7d16d7d81d25348fe6299e7006ddbd9c
madjid-asa <madjid-asa@users.noreply.github.com>
2022-07-08 15:41:32 +0200

    add apm (#372)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,3 +25,19 @@ SECURE_SSL_REDIRECT = env.str("SECURE_SSL_REDIRECT", True)
 
 MEDIA_URL = f"https://{S3_STORAGE_ENDPOINT_DOMAIN}/"  # noqa
 DEFAULT_FILE_STORAGE = "lemarche.utils.s3boto.S3BotoStorage"
+
+# Active Elastic APM metrics
+# See https://www.elastic.co/guide/en/apm/agent/python/current/configuration.html
+INSTALLED_APPS += ["elasticapm.contrib.django"]  # noqa F405
+MIDDLEWARE += ["elasticapm.contrib.django.middleware.TracingMiddleware"]  # noqa
+
+ELASTIC_APM = {
+    "ENABLED": env.bool("APM_ENABLED", True),
+    "SERVICE_NAME": "marche-django",
+    "SERVICE_VERSION": env.str("COMMIT_ID", None),
+    "SERVER_URL": env.str("APM_SERVER_URL", ""),
+    "SECRET_TOKEN": env.str("APM_AUTH_TOKEN", ""),
+    "ENVIRONMENT": BITOUBI_ENV,  # noqa
+    "DJANGO_TRANSACTION_NAME_FROM_ROUTE": True,
+    "TRANSACTION_SAMPLE_RATE": 0.1,
+}
