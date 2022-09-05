685e61b5fc7bc1e7c7bfacac975dff2fae86874b
madjid.asa <madjid.asa@gmail.com>
2022-09-05 15:18:33 +0200

    clean stat database router

--- config/settings/StatsRouter.py
+++ config/settings/StatsRouter.py
@@ -1,7 +1,7 @@
 class StatsRouter:
     """
     A router to control all database operations on models in the
-    auth and contenttypes applications.
+    stat application.
     """
 
     route_app_labels = {"stats"}
@@ -10,7 +10,7 @@ class StatsRouter:
 
     def db_for_read(self, model, **hints):
         """
-        Attempts to read auth and contenttypes models go to auth_db.
+        Attempts to read Tracker models go to stats db.
         """
         if model._meta.app_label in self.route_app_labels:
             return self.db_stats
@@ -18,7 +18,7 @@ class StatsRouter:
 
     def db_for_write(self, model, **hints):
         """
-        Attempts to write auth and contenttypes models go to auth_db.
+        Attempts to write Tracker models go to stats db.
         """
         if model._meta.app_label in self.route_app_labels:
             return self.db_stats
@@ -26,8 +26,7 @@ class StatsRouter:
 
     def allow_relation(self, obj1, obj2, **hints):
         """
-        Allow relations if a model in the auth or contenttypes apps is
-        involved.
+        Allow relations if a model in the  Tracker models go to stats db.
         """
         if obj1._meta.app_label in self.route_app_labels or obj2._meta.app_label in self.route_app_labels:
             return True
@@ -35,10 +34,8 @@ class StatsRouter:
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-        Make sure the auth and contenttypes apps only appear in the
-        self.db_stats database.
+        Make sure Tracker models go to stats db.
         """
         if app_label in self.route_app_labels:
-            print(app_label, db, db == self.db_stats)
             return db == self.db_stats
         return None
