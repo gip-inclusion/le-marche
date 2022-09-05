4ed9f67ec045bf0680ec5ab913e94a993215f732
madjid.asa <madjid.asa@gmail.com>
2022-09-05 12:22:49 +0200

    fix database model

--- lemarche/stats/models.py
+++ lemarche/stats/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 
 class Tracker(models.Model):
+    id_internal = models.AutoField(primary_key=True)
 
-    v = models.PositiveIntegerField(verbose_name="Version")
-    timestamp = models.DateTimeField(verbose_name="Timestamp (UNIX Epoch)")
-    order = models.PositiveIntegerField()
+    version = models.PositiveIntegerField(verbose_name="Version")
+    date_created = models.DateTimeField(verbose_name="Timestamp (UNIX Epoch)")
+    send_order = models.PositiveIntegerField(default=0)
     env = models.CharField(max_length=200)
+    source = models.CharField(max_length=200)
 
     session_id = models.UUIDField(verbose_name="browser session UUID")
     page = models.CharField(max_length=200)
     action = models.CharField(verbose_name="Type d'action", max_length=200)
-    meta = models.JSONField()
+    data = models.JSONField()
+    isadmin = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "trackers"
