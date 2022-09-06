e2164f116eca724f0f7a925775c5fa7a06f7abdd
madjid.asa <madjid.asa@gmail.com>
2022-09-06 12:03:03 +0200

    create stats user model

--- lemarche/stats/models.py
+++ lemarche/stats/models.py
@@ -18,3 +18,21 @@ class Tracker(models.Model):
 
     class Meta:
         db_table = "trackers"
+
+
+class StatsUser(models.Model):
+    id = models.PositiveIntegerField(
+        primary_key=True, auto_created=False, verbose_name="ID app leMarche", db_index=True
+    )
+    email = models.EmailField(verbose_name="Adresse e-mail", unique=True)
+    first_name = models.CharField(verbose_name="Prénom", max_length=150)
+    last_name = models.CharField(verbose_name="Nom", max_length=150)
+    kind = models.CharField(verbose_name="Type d'utilisateur", max_length=20, blank=True)
+    phone = models.CharField(verbose_name="Téléphone", max_length=20, blank=True)
+    company_name = models.CharField(verbose_name="Nom de l'entreprise", max_length=255, blank=True)
+    position = models.CharField(verbose_name="Poste", max_length=255, blank=True)
+    partner_kind = models.CharField(verbose_name="Type de partenaire", max_length=20, blank=True)
+
+    class Meta:
+        # avoid "stats_stats_user"
+        db_table = "stats_user"
