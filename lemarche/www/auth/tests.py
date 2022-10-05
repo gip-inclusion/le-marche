d2b2bddcd6c84313192b62e593998b848335e98b
Raphael Odini <raphodn@users.noreply.github.com>
2022-10-05 17:20:25 +0200

    Mise à jour de packets : selenium & coverage (#450)
    * Update selenium & coverage packages
    
    * Update tests

--- lemarche/www/auth/tests.py
+++ lemarche/www/auth/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 from lemarche.users.factories import DEFAULT_PASSWORD, UserFactory
@@ -78,36 +79,36 @@ class SignupFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}")
 
-        driver.find_element_by_css_selector("input#id_kind_0").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_0").click()
         for key in SIAE:
-            driver.find_element_by_css_selector(f"input#id_{key}").send_keys(SIAE[key])
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+            driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(SIAE[key])
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should create User
         self.assertEqual(User.objects.count(), self.user_count + 1)
         # user should be automatically logged in
-        header = driver.find_element_by_css_selector("header#header")
+        header = driver.find_element(By.CSS_SELECTOR, "header#header")
         self.assertTrue("Mon espace" in header.text)
         self.assertTrue("Connexion" not in header.text)
         # should redirect to user dashboard
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('dashboard:home')}")
         # message should be displayed
-        messages = driver.find_element_by_css_selector("div.messages")
+        messages = driver.find_element(By.CSS_SELECTOR, "div.messages")
         self.assertTrue("Vous pouvez maintenant ajouter votre structure" in messages.text)
 
     def test_siae_submits_signup_form_error(self):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}")
 
-        driver.find_element_by_css_selector("input#id_kind_0").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_0").click()
         for key in SIAE:
             if key not in ["last_name"]:
-                driver.find_element_by_css_selector(f"input#id_{key}").send_keys(SIAE[key])
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+                driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(SIAE[key])
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should not submit form (last_name field is required)
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('auth:signup')}")
@@ -116,36 +117,36 @@ class SignupFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}")
 
-        driver.find_element_by_css_selector("input#id_kind_1").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_1").click()
         for key in BUYER:
-            driver.find_element_by_css_selector(f"input#id_{key}").send_keys(BUYER[key])
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+            driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(BUYER[key])
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should create User
         self.assertEqual(User.objects.count(), self.user_count + 1)
         # user should be automatically logged in
-        header = driver.find_element_by_css_selector("header#header")
+        header = driver.find_element(By.CSS_SELECTOR, "header#header")
         self.assertTrue("Mon espace" in header.text)
         self.assertTrue("Connexion" not in header.text)
         # should redirect to home
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('pages:home')}")
         # message should be displayed
-        messages = driver.find_element_by_css_selector("div.messages")
+        messages = driver.find_element(By.CSS_SELECTOR, "div.messages")
         self.assertTrue("Inscription validée" in messages.text)
 
     def test_buyer_submits_signup_form_error(self):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}")
 
-        driver.find_element_by_css_selector("input#id_kind_1").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_1").click()
         for key in BUYER:
             if key not in ["position"]:
-                driver.find_element_by_css_selector(f"input#id_{key}").send_keys(BUYER[key])
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+                driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(BUYER[key])
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should not submit form (position field is required)
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('auth:signup')}")
@@ -154,37 +155,37 @@ class SignupFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}")
 
-        driver.find_element_by_css_selector("input#id_kind_2").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_2").click()
         for key in PARTNER:
-            driver.find_element_by_css_selector(f"input#id_{key}").send_keys(PARTNER[key])
-        driver.find_element_by_xpath("//select[@id='id_partner_kind']/option[text()='Réseaux IAE']").click()
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+            driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(PARTNER[key])
+        driver.find_element(By.XPATH, "//select[@id='id_partner_kind']/option[text()='Réseaux IAE']").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should create User
         self.assertEqual(User.objects.count(), self.user_count + 1)
         # user should be automatically logged in
-        header = driver.find_element_by_css_selector("header#header")
+        header = driver.find_element(By.CSS_SELECTOR, "header#header")
         self.assertTrue("Mon espace" in header.text)
         self.assertTrue("Connexion" not in header.text)
         # should redirect to home
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('pages:home')}")
         # message should be displayed
-        messages = driver.find_element_by_css_selector("div.messages")
+        messages = driver.find_element(By.CSS_SELECTOR, "div.messages")
         self.assertTrue("Inscription validée" in messages.text)
 
     def test_partner_submits_signup_form_error(self):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}")
 
-        driver.find_element_by_css_selector("input#id_kind_2").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_2").click()
         for key in PARTNER:
             if key not in ["company_name"]:
-                driver.find_element_by_css_selector(f"input#id_{key}").send_keys(PARTNER[key])
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+                driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(PARTNER[key])
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should not submit form (company_name field is required)
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('auth:signup')}")
@@ -193,24 +194,24 @@ class SignupFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:signup')}?next=/prestataires/?kind=ESAT")
 
-        driver.find_element_by_css_selector("input#id_kind_2").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_kind_2").click()
         for key in PARTNER:
-            driver.find_element_by_css_selector(f"input#id_{key}").send_keys(PARTNER[key])
-        driver.find_element_by_xpath("//select[@id='id_partner_kind']/option[text()='Réseaux IAE']").click()
-        driver.find_element_by_css_selector("input#id_accept_rgpd").click()
+            driver.find_element(By.CSS_SELECTOR, f"input#id_{key}").send_keys(PARTNER[key])
+        driver.find_element(By.XPATH, "//select[@id='id_partner_kind']/option[text()='Réseaux IAE']").click()
+        driver.find_element(By.CSS_SELECTOR, "input#id_accept_rgpd").click()
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should create User
         self.assertEqual(User.objects.count(), self.user_count + 1)
         # user should be automatically logged in
-        header = driver.find_element_by_css_selector("header#header")
+        header = driver.find_element(By.CSS_SELECTOR, "header#header")
         self.assertTrue("Mon espace" in header.text)
         self.assertTrue("Connexion" not in header.text)
         # should redirect to next url
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('siae:search_results')}?kind=ESAT")
         # message should be displayed
-        messages = driver.find_element_by_css_selector("div.messages")
+        messages = driver.find_element(By.CSS_SELECTOR, "div.messages")
         self.assertTrue("Inscription validée" in messages.text)
 
     @classmethod
@@ -233,10 +234,10 @@ class LoginFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:login')}")
 
-        driver.find_element_by_css_selector("input#id_username").send_keys(user_siae.email)
-        driver.find_element_by_css_selector("input#id_password").send_keys(DEFAULT_PASSWORD)
+        driver.find_element(By.CSS_SELECTOR, "input#id_username").send_keys(user_siae.email)
+        driver.find_element(By.CSS_SELECTOR, "input#id_password").send_keys(DEFAULT_PASSWORD)
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should redirect SIAE to profil
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('dashboard:home')}")
@@ -246,10 +247,10 @@ class LoginFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:login')}")
 
-        driver.find_element_by_css_selector("input#id_username").send_keys(user_buyer.email)
-        driver.find_element_by_css_selector("input#id_password").send_keys(DEFAULT_PASSWORD)
+        driver.find_element(By.CSS_SELECTOR, "input#id_username").send_keys(user_buyer.email)
+        driver.find_element(By.CSS_SELECTOR, "input#id_password").send_keys(DEFAULT_PASSWORD)
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should redirect BUYER to home
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('pages:home')}")
@@ -259,25 +260,25 @@ class LoginFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:login')}")
 
-        driver.find_element_by_css_selector("input#id_username").send_keys("SIAE5@example.com")
-        driver.find_element_by_css_selector("input#id_password").send_keys(DEFAULT_PASSWORD)
+        driver.find_element(By.CSS_SELECTOR, "input#id_username").send_keys("SIAE5@example.com")
+        driver.find_element(By.CSS_SELECTOR, "input#id_password").send_keys(DEFAULT_PASSWORD)
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
     def test_user_wrong_credentials_should_see_error_message(self):
         user_siae = UserFactory(email="siae5@example.com", kind=User.KIND_SIAE)
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:login')}")
 
-        driver.find_element_by_css_selector("input#id_username").send_keys(user_siae.email)
-        driver.find_element_by_css_selector("input#id_password").send_keys("password")
+        driver.find_element(By.CSS_SELECTOR, "input#id_username").send_keys(user_siae.email)
+        driver.find_element(By.CSS_SELECTOR, "input#id_password").send_keys("password")
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should not submit form
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('auth:login')}")
         # post-migration message should be displayed
-        messages = driver.find_element_by_css_selector("div.alert-danger")
+        messages = driver.find_element(By.CSS_SELECTOR, "div.alert-danger")
         self.assertTrue("aisissez un Adresse e-mail et un mot de passe valides" in messages.text)
 
     def test_user_empty_credentials_should_see_post_migration_message(self):
@@ -287,15 +288,15 @@ class LoginFormTest(StaticLiveServerTestCase):
         driver = self.driver
         driver.get(f"{self.live_server_url}{reverse('auth:login')}")
 
-        driver.find_element_by_css_selector("input#id_username").send_keys("existing-user@example.com")
-        driver.find_element_by_css_selector("input#id_password").send_keys("password")
+        driver.find_element(By.CSS_SELECTOR, "input#id_username").send_keys("existing-user@example.com")
+        driver.find_element(By.CSS_SELECTOR, "input#id_password").send_keys("password")
 
-        driver.find_element_by_css_selector("form button").click()
+        driver.find_element(By.CSS_SELECTOR, "form button").click()
 
         # should not submit form
         self.assertEqual(driver.current_url, f"{self.live_server_url}{reverse('auth:login')}")
         # # post-migration message should be displayed
-        messages = driver.find_element_by_css_selector("div#post-migration-login-message")
+        messages = driver.find_element(By.CSS_SELECTOR, "div#post-migration-login-message")
         self.assertTrue("Le marché de l'inclusion fait peau neuve" in messages.text)
 
     @classmethod
