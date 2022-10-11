554a8ec201a3f290785382b4e6e9df90a038339a
Raphael Odini <raphodn@users.noreply.github.com>
2022-10-11 14:41:10 +0200

    Add survey fields

--- lemarche/utils/constants.py
+++ lemarche/utils/constants.py
@@ -1,1 +1,14 @@
 EMPTY_CHOICE = (("", ""),)
+
+MARCHE_BENEFIT_TIME = "TIME"
+MARCHE_BENEFIT_DISCOVER = "DISCOVER"
+MARCHE_BENEFIT_MORE = "MORE"
+MARCHE_BENEFIT_ASSIGN = "ASSIGN"
+MARCHE_BENEFIT_NONE = "NONE"
+MARCHE_BENEFIT_CHOICES = (
+    (MARCHE_BENEFIT_TIME, "Gagner du temps"),
+    (MARCHE_BENEFIT_DISCOVER, "Découvrir de nouveaux prestataires inclusifs"),
+    (MARCHE_BENEFIT_MORE, "Faire plus d'achats inclusifs"),
+    (MARCHE_BENEFIT_ASSIGN, "Attribuer une clause sociale à un marché "),
+    (MARCHE_BENEFIT_NONE, "Aucun"),
+)
