ed83c9fe6255e5c2db9598eb36824480ac94fbb8
Raphael Odini <raphodn@users.noreply.github.com>
2022-10-12 17:05:09 +0200

    Edit tender survey benefit options

--- lemarche/utils/constants.py
+++ lemarche/utils/constants.py
@@ -3,12 +3,14 @@ EMPTY_CHOICE = (("", ""),)
 MARCHE_BENEFIT_TIME = "TIME"
 MARCHE_BENEFIT_DISCOVER = "DISCOVER"
 MARCHE_BENEFIT_MORE = "MORE"
-MARCHE_BENEFIT_ASSIGN = "ASSIGN"
+MARCHE_BENEFIT_CLAUSE = "CLAUSE"
+MARCHE_BENEFIT_SECURE = "SECURE"
 MARCHE_BENEFIT_NONE = "NONE"
 MARCHE_BENEFIT_CHOICES = (
     (MARCHE_BENEFIT_TIME, "Gagner du temps"),
     (MARCHE_BENEFIT_DISCOVER, "Découvrir de nouveaux prestataires inclusifs"),
     (MARCHE_BENEFIT_MORE, "Faire plus d'achats inclusifs"),
-    (MARCHE_BENEFIT_ASSIGN, "Attribuer une clause sociale à un marché "),
+    (MARCHE_BENEFIT_CLAUSE, "Intégrer une clause sociale à un marché"),
+    (MARCHE_BENEFIT_SECURE, "Sécuriser un marché réservé"),
     (MARCHE_BENEFIT_NONE, "Aucun"),
 )
