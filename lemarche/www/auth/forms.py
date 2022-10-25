d9446b597e438c50ee2f8e8064d4dc40aaa5be42
madjid.asa <madjid.asa@gmail.com>
2022-10-25 18:14:10 +0200

    add stats of inclusive providers on signup

--- lemarche/www/auth/forms.py
+++ lemarche/www/auth/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, UserCreationForm
 
 from lemarche.users.models import User
-from lemarche.utils.constants import EMPTY_CHOICE
+from lemarche.utils.constants import EMPTY_CHOICE, HOW_MANY_CHOICES
 from lemarche.utils.password_validation import CnilCompositionPasswordValidator
 
 
@@ -45,7 +45,20 @@ class SignupForm(UserCreationForm):
         required=True,
     )
     # help_text="Nous enverrons un e-mail de confirmation à cette adresse avant de valider le compte.")
-
+    nb_of_inclusive_provider_2022 = forms.ChoiceField(
+        # flake8: noqa E501
+        label="En 2022, avec combien de prestataires inclusifs relevant du secteur l’Insertion avez-vous déjà travaillé ?",
+        choices=HOW_MANY_CHOICES,
+        widget=forms.RadioSelect(),
+        required=False,
+    )
+    nb_of_handicap_provider_2022 = forms.ChoiceField(
+        # flake8: noqa E501
+        label="En 2022, avec combien de prestataires inclusifs relevant du secteur du Handicap avez-vous déjà travaillé ?",
+        choices=HOW_MANY_CHOICES,
+        widget=forms.RadioSelect(),
+        required=False,
+    )
     accept_rgpd = forms.BooleanField(label=User._meta.get_field("accept_rgpd").help_text, help_text="", required=True)
     # accept_survey is hidden by default in the frontend. Shown if the user choses kind BUYER or PARTNER
     accept_survey = forms.BooleanField(
@@ -82,6 +95,24 @@ class SignupForm(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data["email"]
         return email.lower()
+
+    def save(self, commit=True):
+
+        instance = super(SignupForm, self).save(commit=False)
+        extra_data = {}
+        if self.cleaned_data.get("nb_of_inclusive_provider_2022"):
+            extra_data["nb_of_inclusive_provider_2022"] = self.cleaned_data.get("nb_of_inclusive_provider_2022")
+
+        if self.cleaned_data.get("nb_of_handicap_provider_2022"):
+            extra_data["nb_of_handicap_provider_2022"] = self.cleaned_data.get("nb_of_handicap_provider_2022")
+
+        instance.extra_data = extra_data
+
+        if commit:
+            self.save_m2m()
+
+            instance.save()
+        return instance
 
 
 class LoginForm(AuthenticationForm):
