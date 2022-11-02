1acad3386621667aa1ff732fae764cc7295858a9
madjid.asa <madjid.asa@gmail.com>
2022-11-02 13:23:10 +0100

    install htmx and django-htmx

--- lemarche/utils/settings_context_processors.py
+++ lemarche/utils/settings_context_processors.py
@@ -6,8 +6,10 @@ def expose_settings(request):
     Put things into the context to make them available in templates.
     https://docs.djangoproject.com/en/2.1/ref/templates/api/#using-requestcontext
     """
+    base_template = "layouts/base_htmx.html" if request.htmx else "layouts/base.html"
 
     return {
+        "BASE_TEMPLATE": base_template,
         "BITOUBI_ENV": settings.BITOUBI_ENV,
         "BITOUBI_ENV_COLOR": settings.BITOUBI_ENV_COLOR,
         "HOTJAR_ID": settings.HOTJAR_ID,
