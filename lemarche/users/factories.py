31b756602c67792ca75f5d0441bfcdc66259d0b7
Raphael Odini <raphodn@users.noreply.github.com>
2022-11-16 16:19:37 +0100

    user factory: compute password once, then cache

--- lemarche/users/factories.py
+++ lemarche/users/factories.py
@@ -1,12 +1,19 @@
+import functools
 import string
 
 import factory.fuzzy
+from django.contrib.auth.hashers import make_password
 from factory.django import DjangoModelFactory
 
 from lemarche.users.models import User
 
 
 DEFAULT_PASSWORD = "P4ssw0rd!*"
+
+
+@functools.cache
+def default_password():
+    return make_password(DEFAULT_PASSWORD)
 
 
 class UserFactory(DjangoModelFactory):
@@ -16,7 +23,7 @@ class UserFactory(DjangoModelFactory):
     first_name = factory.Sequence("first_name{0}".format)
     last_name = factory.Sequence("last_name{0}".format)
     email = factory.Sequence("email{0}@example.com".format)
-    password = factory.PostGenerationMethodCall("set_password", DEFAULT_PASSWORD)
+    password = factory.LazyFunction(default_password)
     phone = factory.fuzzy.FuzzyText(length=10, chars=string.digits)
     kind = User.KIND_SIAE
 
