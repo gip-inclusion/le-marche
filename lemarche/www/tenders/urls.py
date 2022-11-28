7f9bc9c2cd5f386b1d87547c13789b8ccaf34d1a
madjid.asa <madjid.asa@gmail.com>
2022-11-28 14:49:57 +0100

    fix

--- lemarche/www/tenders/urls.py
+++ lemarche/www/tenders/urls.py
@@ -15,9 +15,9 @@ app_name = "tenders"
 urlpatterns = [
     path("ajouter/", TenderCreateMultiStepView.as_view(), name="create"),
     path("ajouter/<str:slug>", TenderCreateMultiStepView.as_view(), name="create"),
+    path("<str:slug>", TenderDetailView.as_view(), name="detail"),
     path("", TenderListView.as_view(), name="list"),
     path("<status>", TenderListView.as_view(), name="list"),
-    path("<str:slug>", TenderDetailView.as_view(), name="detail"),
     path("<str:slug>/structures-interessees", TenderSiaeInterestedListView.as_view(), name="detail-siae-interested"),
     path("<str:slug>/contact-click-stat", TenderDetailContactClickStat.as_view(), name="detail-contact-click-stat"),
 ]
