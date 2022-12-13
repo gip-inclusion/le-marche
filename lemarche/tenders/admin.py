ca59b06fc0d52546ec38e7908257623cbef24297
madjid.asa <madjid.asa@gmail.com>
2022-12-13 18:17:17 +0100

    add tenders.intervention_location in the admin panel

--- lemarche/tenders/admin.py
+++ lemarche/tenders/admin.py
@@ -120,6 +120,7 @@ class TenderAdmin(admin.ModelAdmin):
             "Lieux d'exécution 'spécifique'",
             {
                 "fields": (
+                    "intervention_location",
                     "perimeters",
                     "include_country_area",
                 ),
