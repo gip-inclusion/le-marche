f7343de037f25c3fddc97a08481dd39afcf9d48a
madjid.asa <madjid.asa@gmail.com>
2022-12-15 12:30:57 +0100

    fix perimeters-string-list for legacy perimeters

--- lemarche/www/tenders/views.py
+++ lemarche/www/tenders/views.py
@@ -113,7 +113,6 @@ class TenderCreateMultiStepView(SessionWizardView):
         if self.steps.current == self.STEP_CONFIRMATION:
             tender_dict = self.get_all_cleaned_data()
             tender_dict["sectors_list_string"] = ", ".join(tender_dict["sectors"].values_list("name", flat=True))
-            tender_dict["perimeters_list_string"] = tender_dict["location"].name
             tender_dict["get_kind_display"] = get_choice(Tender.TENDER_KIND_CHOICES, tender_dict["kind"])
             tender_dict["get_amount_display"] = get_choice(
                 tender_constants.AMOUNT_RANGE_CHOICES, tender_dict["amount"]
