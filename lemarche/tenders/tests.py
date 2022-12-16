c339adcbb14053ebf5380793c237853d46e1f5e3
Raphael Odini <raphodn@users.noreply.github.com>
2022-12-16 08:00:30 +0100

    Fix tests

--- lemarche/tenders/tests.py
+++ lemarche/tenders/tests.py
@@ -225,9 +225,10 @@ class TenderModelQuerysetStatsTest(TestCase):
     def test_with_siae_stats_queryset(self):
         self.assertEqual(Tender.objects.count(), 2 + 1)
         tender_with_siae_1 = Tender.objects.with_siae_stats().filter(id=self.tender_with_siae_1.id).first()
-        self.assertEqual(tender_with_siae_1.siaes.count(), 5)
-        self.assertEqual(tender_with_siae_1.siae_count, 5)
-        self.assertEqual(tender_with_siae_1.siae_email_send_count, 3)
+        self.assertEqual(tender_with_siae_1.siaes.count(), 6)
+        self.assertEqual(tender_with_siae_1.siae_count, 6)
+        self.assertEqual(tender_with_siae_1.siae_email_send_count, 4)
+        self.assertEqual(tender_with_siae_1.siae_email_link_click_count, 3)
         self.assertEqual(tender_with_siae_1.siae_detail_display_count, 2)
         self.assertEqual(tender_with_siae_1.siae_detail_contact_click_count, 1)
         self.assertEqual(tender_with_siae_1.siae_detail_contact_click_since_last_seen_date_count, 1)
@@ -247,7 +248,7 @@ class TenderModelQuerysetStatsTest(TestCase):
         self.assertEqual(tender_without_siae.siae_detail_contact_click_since_last_seen_date_count, 0)
 
     def test_siae_with_tender_stats_queryset(self):
-        self.assertEqual(Siae.objects.count(), 5 + 1)
+        self.assertEqual(Siae.objects.count(), 6 + 1)
         siae_with_tender_1 = Siae.objects.with_tender_stats().filter(id=self.siae_with_tender_1.id).first()
         # self.assertEqual(siae_with_tender_1.tenders.count(), 2)
         self.assertEqual(siae_with_tender_1.tender_count, 2)
