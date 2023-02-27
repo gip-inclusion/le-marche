509b26e3eb3db6255fd8ca7d8082ca9b86e4ec4d
Raphael Odini <raphodn@users.noreply.github.com>
2023-02-27 16:09:08 +0100

    Add log_item

--- lemarche/www/tenders/tasks.py
+++ lemarche/www/tenders/tasks.py
@@ -18,22 +18,31 @@ def send_tender_emails_to_siaes(tender: Tender):
     """
     email_subject = "Une opportunité commerciale pour vous sur le Marché de l'inclusion"
     siaes = tender.siaes.all()
+    siae_users_send_count = 0
 
     for siae in siaes:
         send_tender_email_to_siae(email_subject, tender, siae)
         for user in siae.users.all():
             send_tender_email_to_siae(email_subject, tender, siae, email_to_override=user.email)
+            siae_users_send_count += 1
 
     tender.tendersiae_set.update(email_send_date=timezone.now(), updated_at=timezone.now())
 
     # log email batch
-    log_item = {
+    siaes_log_item = {
         "action": "email_siaes_matched",
         "email_subject": email_subject,
         "email_count": siaes.count(),
         "email_timestamp": timezone.now().isoformat(),
     }
-    tender.logs.append(log_item)
+    tender.logs.append(siaes_log_item)
+    siae_users_log_item = {
+        "action": "email_siae_users_matched",
+        "email_subject": email_subject,
+        "email_count": siae_users_send_count,
+        "email_timestamp": timezone.now().isoformat(),
+    }
+    tender.logs.append(siae_users_log_item)
     tender.save()
 
 
