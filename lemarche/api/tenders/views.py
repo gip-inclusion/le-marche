046d0fd2ddbcc0bda35075358e440be25a877450
Raphael Odini <raphodn@users.noreply.github.com>
2023-03-14 16:04:11 +0100

    Add Authentication. Fill author

--- lemarche/api/tenders/views.py
+++ lemarche/api/tenders/views.py
@@ -1,13 +1,25 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import mixins, viewsets
 
 from lemarche.api.tenders.serializers import TenderSerializer
+from lemarche.api.utils import check_user_token
 from lemarche.tenders.models import Tender
 
 
 class TenderViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = TenderSerializer
 
-    @extend_schema(summary="Déposer un besoin d'achat", tags=[Tender._meta.verbose_name_plural])
+    @extend_schema(
+        summary="Déposer un besoin d'achat",
+        tags=[Tender._meta.verbose_name_plural],
+        parameters=[
+            OpenApiParameter(name="token", description="Token Utilisateur", required=True, type=str),
+        ],
+    )
     def create(self, request, *args, **kwargs):
+        token = request.GET.get("token", None)
+        self.user = check_user_token(token)
         return super().create(request, args, kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.user)
