52ad798b34564d01c15f27038fc4f2b8e215b909
Raphael Odini <raphodn@users.noreply.github.com>
2023-04-12 15:30:57 +0200

    Dépôt de besoin : répare l'envoi d'e-mails aux auteurs des besoins incrémentaux (#722)
    * Tender: fix email to incremental authors. ref #657
    
    * Fix queryset
    
    * Fix mixup

--- lemarche/tenders/management/commands/send_author_incremental_emails.py
+++ lemarche/tenders/management/commands/send_author_incremental_emails.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from lemarche.tenders.models import Tender
-from lemarche.www.tenders.tasks import send_author_incremental_2_days_emails
+from lemarche.www.tenders.tasks import send_author_incremental_2_days_email
 
 
 class Command(BaseCommand):
@@ -29,16 +29,16 @@ class Command(BaseCommand):
         two_days_ago = timezone.now() - timedelta(days=2)
         three_days_ago = timezone.now() - timedelta(days=3)
         tender_validated_incremental = Tender.objects.validated().is_incremental()
-        tender_validated_incremental_2_days = tender_validated_incremental.filter(created_at__gte=two_days_ago).filter(
-            created_at__lt=three_days_ago
-        )
+        tender_validated_incremental_2_days = tender_validated_incremental.filter(
+            created_at__gte=three_days_ago
+        ).filter(created_at__lt=two_days_ago)
         self.stdout.write(f"Found {tender_validated_incremental_2_days.count()} Tenders")
 
         if not dry_run:
             self.stdout.write("-" * 80)
             self.stdout.write("Step 2: Send emails")
             for tender in tender_validated_incremental_2_days:
-                send_author_incremental_2_days_emails(tender)
+                send_author_incremental_2_days_email(tender)
             self.stdout.write(f"Sent {tender_validated_incremental_2_days.count()} J+2 emails")
 
         self.stdout.write("-" * 80)
