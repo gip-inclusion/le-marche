81401ade2a371ff0cd3664d45fe11f726526bc36
Raphael Odini <raphodn@users.noreply.github.com>
2023-04-26 11:07:40 +0200

    Dépôt de besoin : envoyer un e-mail de rappel aux prestataires qui se sont montrés intéressés (#732)
    * Tender: siae interested reminder task
    
    * Add management command
    
    * Add cron config
    
    * Add tests

---
+++ clevercloud/tenders_send_siae_interested_reminder_emails.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+# Send TenderSiae interested reminder emails
+
+# Do not run if this env var is not set:
+if [[ -z "$CRON_TENDER_SEND_SIAE_INTERESTED_REMINDER_EMAILS_ENABLED" ]]; then
+    echo "CRON_TENDER_SEND_SIAE_INTERESTED_REMINDER_EMAILS_ENABLED not set. Exiting..."
+    exit 0
+fi
+
+# About clever cloud cronjobs:
+# https://www.clever-cloud.com/doc/tools/crons/
+
+if [[ "$INSTANCE_NUMBER" != "0" ]]; then
+    echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
+    exit 0
+fi
+
+# $APP_HOME is set by default by clever cloud.
+cd $APP_HOME
+
+django-admin send_siae_interested_reminder_emails --days-since-detail-contact-click-date 2
--- lemarche/tenders/management/commands/send_siae_interested_reminder_emails.py
+++ lemarche/tenders/management/commands/send_siae_interested_reminder_emails.py
@@ -4,28 +4,28 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 
 from lemarche.tenders.models import Tender, TenderSiae
-from lemarche.www.tenders.tasks import send_tender_contacted_reminder_email_to_siaes
+from lemarche.www.tenders.tasks import send_tender_interested_reminder_email_to_siaes
 
 
 class Command(BaseCommand):
     """
-    Daily script to check recently sent emails to Siae. If no email_link_click_date, then send reminder
+    Daily script to check recently contact click. send reminder
     When? J+2 (but doesn't run on weekends!)
 
     Usage:
-    python manage.py send_siae_contacted_reminder_emails --dry-run
-    python manage.py send_siae_contacted_reminder_emails --days-since-email-send-date 2
-    python manage.py send_siae_contacted_reminder_emails --tender-id 1
-    python manage.py send_siae_contacted_reminder_emails
+    python manage.py send_siae_interested_reminder_emails --dry-run
+    python manage.py send_siae_interested_reminder_emails --days-since-detail-contact-click-date 2
+    python manage.py send_siae_interested_reminder_emails --tender-id 1
+    python manage.py send_siae_interested_reminder_emails
     """
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--days-since-email-send-date",
-            dest="days_since_email_send_date",
+            "--days-since-detail-contact-click-date",
+            dest="days_since_detail_contact_click_date",
             type=int,
             default=2,
-            help="Laps de temps depuis 'email_send_date'",
+            help="Laps de temps depuis 'detail_contact_click_date'",
         )
         parser.add_argument(
             "--tender-id", dest="tender_id", type=int, default=None, help="Restreindre sur un besoin donné"
@@ -34,7 +34,7 @@ class Command(BaseCommand):
 
     def handle(self, dry_run=False, **options):
         self.stdout.write("-" * 80)
-        self.stdout.write("Script to send Siae contacted reminder emails...")
+        self.stdout.write("Script to send Siae interested reminder emails...")
 
         current_weekday = timezone.now().weekday()
         if current_weekday >= 5:
@@ -42,45 +42,44 @@ class Command(BaseCommand):
         else:
             self.stdout.write("-" * 80)
             self.stdout.write("Step 1: Find TenderSiae")
-            self.stdout.write(f"- where email_send_date J+{options['days_since_email_send_date']}")
-            self.stdout.write("- where email_link_click_date is None")
+            self.stdout.write(f"- where detail_contact_click_date J+{options['days_since_detail_contact_click_date']}")
 
-            lt_days_ago = timezone.now() - timedelta(days=options["days_since_email_send_date"])
-            gte_days_ago = timezone.now() - timedelta(days=options["days_since_email_send_date"] + 1)
+            lt_days_ago = timezone.now() - timedelta(days=options["days_since_detail_contact_click_date"])
+            gte_days_ago = timezone.now() - timedelta(days=options["days_since_detail_contact_click_date"] + 1)
             # Monday: special case (the script doesn't run during on weekends)
             # gte_days_ago = gte_days_ago+2 to account for Saturday & Sunday
             if current_weekday == 0:
-                gte_days_ago = timezone.now() - timedelta(days=options["days_since_email_send_date"] + 1 + 2)
-            tendersiae_contacted_reminder_list = TenderSiae.objects.email_click_reminder(
+                gte_days_ago = timezone.now() - timedelta(days=options["days_since_detail_contact_click_date"] + 1 + 2)
+            tendersiae_interested_reminder_list = TenderSiae.objects.detail_contact_click_post_reminder(
                 gte_days_ago=gte_days_ago, lt_days_ago=lt_days_ago
             )
 
             if options["tender_id"]:
-                tendersiae_contacted_reminder_list = tendersiae_contacted_reminder_list.filter(
+                tendersiae_interested_reminder_list = tendersiae_interested_reminder_list.filter(
                     tender_id=options["tender_id"]
                 )
-            self.stdout.write(f"Found {tendersiae_contacted_reminder_list.count()} TenderSiaes to remind")
+            self.stdout.write(f"Found {tendersiae_interested_reminder_list.count()} TenderSiaes to remind")
 
             if not dry_run:
                 self.stdout.write("-" * 80)
                 self.stdout.write("Step 2: Send emails for each tender")
                 tender_id_list = (
-                    tendersiae_contacted_reminder_list.values_list("tender_id", flat=True).distinct().order_by()
+                    tendersiae_interested_reminder_list.values_list("tender_id", flat=True).distinct().order_by()
                 )
                 self.stdout.write(f"{tender_id_list.count()} tenders concerned")
                 for tender_id in tender_id_list:
                     tender = Tender.objects.get(id=tender_id)
-                    tender_tendersiae_contacted_reminder_list = tendersiae_contacted_reminder_list.filter(
+                    tender_tendersiae_interested_reminder_list = tendersiae_interested_reminder_list.filter(
                         tender_id=tender_id
                     )
                     tender_tendersiae = TenderSiae.objects.filter(tender_id=tender_id).filter(
                         email_send_date__isnull=False
                     )
                     self.stdout.write(
-                        f"Tender {tender_id}: {tender_tendersiae_contacted_reminder_list.count()} TenderSiaes to remind (out of {tender_tendersiae.count()})"  # noqa
+                        f"Tender {tender_id}: {tender_tendersiae_interested_reminder_list.count()} TenderSiaes to remind (out of {tender_tendersiae.count()})"  # noqa
                     )
-                    send_tender_contacted_reminder_email_to_siaes(
-                        tender, days_since_email_send_date=options["days_since_email_send_date"]
+                    send_tender_interested_reminder_email_to_siaes(
+                        tender, days_since_detail_contact_click_date=options["days_since_detail_contact_click_date"]
                     )
                     self.stdout.write("Emails sent")
 
