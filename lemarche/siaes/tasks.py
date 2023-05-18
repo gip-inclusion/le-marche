7470307ca5226304fae9d8cfb398c263d28222f7
Raphael Odini <raphodn@users.noreply.github.com>
2023-05-18 23:35:37 +0200

    First basic send_completion_reminder_email_to_siae() task

--- lemarche/siaes/tasks.py
+++ lemarche/siaes/tasks.py
@@ -1,6 +1,12 @@
+from django.conf import settings
+from django.urls import reverse_lazy
 from huey.contrib.djhuey import task
 
+from lemarche.users.models import User
+from lemarche.utils.apis import api_mailjet
 from lemarche.utils.apis.geocoding import get_geocoding_data
+from lemarche.utils.emails import whitelist_recipient_list
+from lemarche.utils.urls import get_domain_url, get_share_url_object
 
 
 @task()
@@ -25,3 +31,41 @@ def set_siae_coords(model, siae):
             model.objects.filter(id=siae.id).update(coords=geocoding_data["coords"])
     else:
         print(f"Geocoding not found,{siae.name},{siae.post_code}")
+
+
+def send_completion_reminder_email_to_siaes():
+    from lemarche.siaes.models import Siae
+
+    siaes = Siae.objects.content_not_filled()
+    siaes = siaes.filter(user_count__gte=1)  # we only contact siaes with at least 1 user
+
+    for siae in siaes:
+        send_completion_reminder_email_to_siae(siae)
+
+    # log / print something somewhere ?
+
+
+def send_completion_reminder_email_to_siae(siae):
+    email_subject = "Vous avez raté des opportunités commerciales !"
+    siae_user_emails = siae.users.values_list("email", flat=True)
+    recipient_list = whitelist_recipient_list(siae_user_emails)
+    if recipient_list:
+        for siae_user_email in recipient_list:
+            siae_user = User.objects.get(email=siae_user_email).first_name
+            recipient_name = siae_user.full_name
+
+            variables = {
+                "SIAE_USER_FIRST_NAME": siae_user.first_name,
+                "SIAE_URL": get_share_url_object(siae),
+                "SIAE_EDIT_URL": f"https://{get_domain_url()}{reverse_lazy('dashboard:siae_edit_contact', args=[siae.slug])}",  # noqa
+            }
+
+            api_mailjet.send_transactional_email_with_template(
+                template_id=settings.MAILJET_SIAE_COMPLETION_REMINDER_TEMPLATE_ID,
+                subject=email_subject,
+                recipient_email=siae_user_email,
+                recipient_name=recipient_name,
+                variables=variables,
+            )
+
+            # log something
