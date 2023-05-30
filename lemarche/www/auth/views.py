8a2463bc089b4ca4d46facc20c259911e1ddf683
Raphael Odini <raphodn@users.noreply.github.com>
2023-05-30 08:30:13 +0200

    Connexion : rediriger les acheteurs vers le moteur de recherche (#759)

--- lemarche/www/auth/views.py
+++ lemarche/www/auth/views.py
@@ -61,6 +61,8 @@ class LoginView(auth_views.LoginView):
                 return safe_url
         elif self.request.user.kind == User.KIND_SIAE:
             return reverse_lazy("dashboard:home")
+        elif self.request.user.kind == User.KIND_BUYER:
+            return reverse_lazy("siae:search_results")
         return success_url
 
 
