abf1f921ecaa84c4467fd9a5a19b48437f53bd78
Raphael Odini <raphodn@users.noreply.github.com>
2023-06-05 15:43:38 +0200

    Delete existing links first

--- lemarche/labels/management/commands/import_esus.py
+++ lemarche/labels/management/commands/import_esus.py
@@ -39,16 +39,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout_info("-" * 80)
-        self.stdout_info("Import {LABEL_NAME}")
+        self.stdout_info(f"Import {LABEL_NAME}")
 
         label = Label.objects.get(slug=LABEL_SLUG)
         siaes = Siae.objects.all()
+        siae_label = SiaeLabel.objects.filter(label=label)
+        self.stdout_info("-" * 80)
         self.stdout_info(f"SIAE count: {siaes.count()}")
+        self.stdout_info(f"SIAE with {LABEL_NAME} label count: {siae_label.count()}")
+        if not options["dry_run"]:
+            siae_label.delete()
+            self.stdout_info("Deleted...")
 
         progress = 0
         results = {"success": 0, "error": 0}
 
         file_row_list = read_csv(options["file"])
+        self.stdout_info("-" * 80)
         self.stdout_info(f"{LABEL_NAME} file row count: {len(file_row_list)}")
 
         for row_item in file_row_list:
@@ -79,6 +86,7 @@ class Command(BaseCommand):
             label.data_last_sync_date = timezone.now()
             log_item = {
                 "action": "data_sync",
+                "timestamp": timezone.now().isoformat(),
                 "source": options["file"],
                 "results": {"success_count": results["success"], "error_count": results["error"]},
             }
