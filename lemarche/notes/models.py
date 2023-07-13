08896fd1ad1786649e95f7ebe201e280e71e2186
Raphael Odini <raphodn@users.noreply.github.com>
2023-07-13 12:51:55 +0200

    Add author field. Add to admin

--- lemarche/notes/models.py
+++ lemarche/notes/models.py
@@ -1,9 +1,19 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 
 class Note(models.Model):
     text = models.TextField(verbose_name="Contenu de la note", blank=False)
+
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        verbose_name="Auteur",
+        related_name="notes",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
 
     created_at = models.DateTimeField(verbose_name="Date de création", default=timezone.now)
     updated_at = models.DateTimeField(verbose_name="Date de modification", auto_now=True)
