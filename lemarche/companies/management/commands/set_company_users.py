8c009934dbe10271bba7c517e4de4a47927616dc
Raphael Odini <raphodn@users.noreply.github.com>
2023-07-18 15:29:58 +0200

    Fix stuff on company email_domain_list script. ref #830

--- lemarche/companies/management/commands/set_company_users.py
+++ lemarche/companies/management/commands/set_company_users.py
@@ -5,6 +5,9 @@ from lemarche.utils.commands import BaseCommand
 
 class Command(BaseCommand):
     """
+    Command to attach users to their company, depending on their e-mail address
+    Only for companies that have their 'email_domain_list' field set
+
     Usage:
     - poetry run python manage.py set_company_users --dry-run
     - poetry run python manage.py set_company_users --only-add
@@ -38,13 +41,15 @@ class Command(BaseCommand):
         progress = 0
 
         for company in companies_with_email_domain_list:
+            company_email_domain_list_users = list()
             for company_email_domain in company.email_domain_list:
                 company_email_domain_users = User.objects.filter(email__iendswith=company_email_domain)
-                if not options["dry_run"]:
-                    if options["only_add"]:
-                        company.users.add(*company_email_domain_users)
-                    else:
-                        company.users.set(company_email_domain_users)
+                company_email_domain_list_users += company_email_domain_users
+            if not options["dry_run"]:
+                if options["only_add"]:
+                    company.users.add(*company_email_domain_list_users)
+                else:
+                    company.users.set(company_email_domain_list_users)
             progress += 1
             if (progress % 50) == 0:
                 self.stdout_info(f"{progress}...")
@@ -58,4 +63,4 @@ class Command(BaseCommand):
         self.stdout_info("RECAP")
         self.stdout_info(f"Users with company count: {users_with_company.count()}")
         self.stdout_info(f"Companies with email_domain_list field count: {companies_with_email_domain_list.count()}")
-        self.stdout_info(f"Companies with users count: {companies_with_email_domain_list.count()}")
+        self.stdout_info(f"Companies with users count: {companies_with_users.count()}")
