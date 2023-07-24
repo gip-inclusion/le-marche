39ff16c2e53bc0fa0af44ae08cb17430936d9029
Raphael Odini <raphodn@users.noreply.github.com>
2023-07-24 16:45:12 +0200

    Nouveau modèle pour indiquer des notes (#836)
    * new model Note
    
    * Add author field. Add to admin
    
    * Add typing

---
+++ lemarche/notes/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from lemarche.notes.models import Note
+from lemarche.utils.admin.admin_site import admin_site
+
+
+@admin.register(Note, site=admin_site)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ["id", "author", "text", "created_at"]
+    search_fields = ["id", "text"]
+    search_help_text = "Cherche sur les champs : ID, Contenu"
+
+    readonly_fields = [
+        "author",
+        "created_at",
+        "updated_at",
+    ]
+
+    def save_model(self, request, obj: Note, form, change):
+        if not obj.id and not obj.author_id:
+            obj.author = request.user
+        obj.save()
