abe3a5f64afa82f59049ce85f1bf5a7e67b8c89a
Raphael Odini <raphodn@users.noreply.github.com>
2023-07-26 10:00:07 +0200

    Note : ajout du champ generic foreign key (#844)
    * Note: add generic foreign key fields. Add tests
    
    * Display GFK in admin

--- lemarche/notes/models.py
+++ lemarche/notes/models.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import timezone
 
@@ -16,6 +18,10 @@ class Note(models.Model):
         null=True,
         blank=True,
     )
+
+    content_type = models.ForeignKey(ContentType, blank=True, null=True, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField(blank=True, null=True)
+    content_object = GenericForeignKey("content_type", "object_id")
 
     created_at = models.DateTimeField(verbose_name="Date de création", default=timezone.now)
     updated_at = models.DateTimeField(verbose_name="Date de modification", auto_now=True)
