5b58c270c63692f81636dd318db9711e2ab6b74d
Raphael Odini <raphodn@users.noreply.github.com>
2023-07-27 13:03:04 +0200

    Structures : pouvoir indiquer des notes dans l'admin (#847)
    * Siae: add notes field (generic relation)
    
    * Add note form in Siae admin
    
    * add tests

--- lemarche/notes/tests.py
+++ lemarche/notes/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from lemarche.notes.factories import NoteFactory
 from lemarche.notes.models import Note
+from lemarche.siaes.factories import SiaeFactory
 from lemarche.tenders.factories import TenderFactory
 from lemarche.users.factories import UserFactory
 from lemarche.utils import constants
@@ -27,3 +28,14 @@ class NoteModelTest(TestCase):
         # reverse
         self.assertEqual(Note.objects.filter(tender__title=tender.title).count(), 2)
         self.assertEqual(tender.notes.count(), 2)
+
+    def test_create_siae_note_with_generic_relation(self):
+        siae = SiaeFactory()
+        NoteFactory(author=self.user, content_object=siae)
+        self.assertEqual(Note.objects.count(), 2 + 1)
+        # can create multiple notes for the same object
+        NoteFactory(author=self.user, content_object=siae)
+        self.assertEqual(Note.objects.count(), 2 + 1 + 1)
+        # reverse
+        self.assertEqual(Note.objects.filter(siae__name=siae.name).count(), 2)
+        self.assertEqual(siae.notes.count(), 2)
