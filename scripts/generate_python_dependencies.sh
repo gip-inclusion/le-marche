ec9d498fb88e52fc6612e5e55779a5204ab6002e
Raphael Odini <raphodn@users.noreply.github.com>
2023-08-02 14:07:06 +0200

    Update requirements

--- scripts/generate_python_dependencies.sh
+++ scripts/generate_python_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 poetry update
-poetry export --without-hashes --dev --output requirements/dev.txt
+poetry export --without-hashes --with dev --output requirements/dev.txt
 poetry export --without-hashes --output requirements/staging.txt
