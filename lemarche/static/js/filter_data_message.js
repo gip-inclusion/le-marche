27f6ceb39849d30786eb382aafe0564ce78c0fec
madjid-asa <madjid-asa@users.noreply.github.com>
2023-08-03 16:37:11 +0200

    display pretty messages of conversation (#855)

---
+++ lemarche/static/js/filter_data_message.js
@@ -0,0 +1,45 @@
+function filterData(data, filterText) {
+    return data.filter(item => {
+        const from = `${item.items[0].From.Name} || ${item.items[0].From.Address}`;
+        const to = item.items[0].To[0].Address;
+        const RawHtmlBody = item.items[0].RawHtmlBody;
+        // filter fields
+        return from.includes(filterText) || to.includes(filterText) || RawHtmlBody.includes(filterText);
+    });
+}
+
+// Fonction pour générer les lignes du tableau HTML
+function generateTableRows(filteredData) {
+    const rows = filteredData.map(item => {
+        const sendAt = new Date(item.items[0].SentAtDate).strftime('%d/%m/%Y %Hh%M');
+        const from = `${item.items[0].From.Name} - ${item.items[0].From.Address}`;
+        const RawHtmlBody = item.items[0].RawHtmlBody;
+
+        return `<tr>
+                    <td>${sendAt}</td>
+                    <td>${from}</td>
+                    <td>${RawHtmlBody}</td>
+                </tr>`;
+    });
+
+    return rows.join('');
+}
+
+function init() {
+    const dataTableBody = document.getElementById('table_filter_data_message');
+    const rawData = JSON.parse(dataTableBody.dataset.data);
+
+    // Filter data
+    // const filterInput = document.getElementsByName('id_data_filter');
+    // const filterText = filterInput.value.trim().toLowerCase();
+    // const filteredData = filterData(rawData, filterText);
+
+    // generate rows
+    const tableRows = generateTableRows(rawData);
+
+    // insert rows
+    dataTableBody.innerHTML = tableRows;
+}
+
+// Appeler la fonction init lorsque la page est chargée
+document.addEventListener('DOMContentLoaded', init);
