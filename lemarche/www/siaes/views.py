4365c33ed2bcc4bcabcbcfddc87a0116d38f6f94
madjid-asa <madjid-asa@users.noreply.github.com>
2023-08-10 16:12:21 +0200

    [Inbound Parsing] Mise à jour des infos pratique des structures  (#861)
    * adapt usefull infos siae for the inbound feature
    
    * activate the inbound feature
    
    * rename usefull to useful
    
    * fix tests
    
    * display phone for admin

--- lemarche/www/siaes/views.py
+++ lemarche/www/siaes/views.py
@@ -302,7 +302,8 @@ class SiaeDetailView(FormMixin, DetailView):
         """
         context = super().get_context_data(**kwargs)
         context["current_search_query"] = self.request.session.get(CURRENT_SEARCH_QUERY_COOKIE_NAME, "")
-        context["inbound_email_is_activated"] = self.request.user.is_authenticated and self.request.user.is_admin
+        context["inbound_email_is_activated"] = settings.INBOUND_EMAIL_IS_ACTIVATED
+        context["is_admin"] = self.request.user.is_authenticated and self.request.user.is_admin
         return context
 
 
