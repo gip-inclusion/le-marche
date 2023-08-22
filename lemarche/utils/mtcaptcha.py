9dc72ef6634c363ea95f625499da37340de317d8
SebastienReuiller <sebastien@reuiller.email>
2023-08-22 15:17:59 +0200

    log error only if the user is not responsible (#868)

--- lemarche/utils/mtcaptcha.py
+++ lemarche/utils/mtcaptcha.py
@@ -25,7 +25,9 @@ def check_captcha_token(form_data):
             if response.json().get("success", False):
                 return True
             else:
-                logger.exception("Token failed : %s", response.json()["fail_codes"])
+                # Log event only if the user is not responsible, see https://www.mtcaptcha.com/dev-guide
+                if response.json()["fail_codes"] not in ["token-expired", "invalid-token"]:
+                    logger.exception("Token failed : %s", response.json()["fail_codes"])
         else:
             logger.exception("Bad status code when calling Mtcaptcha API-> check-token: %s", response)
     except Exception as e:
