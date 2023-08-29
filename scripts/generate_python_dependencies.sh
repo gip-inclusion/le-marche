7b9fc37ed06df6354a2e5d6c161f4d8784c74fa9
Raphael Odini <raphodn@users.noreply.github.com>
2023-08-29 12:12:00 +0200

    Script de maj des dépendances : rendre poetry update optionnel (#879)

--- scripts/generate_python_dependencies.sh
+++ scripts/generate_python_dependencies.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-poetry update
+
+# ask if the use wants to run poetry update
+printf "Run 'poetry update' first ? (y/n)"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ]; then 
+    poetry update
+fi
+
+# update requirements .txt files
+echo "Updating 'requirements/dev.txt'"
 poetry export --without-hashes --with dev --output requirements/dev.txt
+echo "Updating 'requirements/staging.txt'"
 poetry export --without-hashes --output requirements/staging.txt
