be796dd5ce6ab9a72806611427e7a89b228e980d
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-02 14:36:54 +0200

    [Dépôt de besoin] Ajouter une stat du nombre de structures qui ont cliqué OU vu (#927)
    * Tender: add stat siae_email_link_click_or_detail_display_count
    
    * Update detail template
    
    * Fix re-add property

--- lemarche/tenders/admin.py
+++ lemarche/tenders/admin.py
@@ -133,6 +133,7 @@ class TenderAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
         # "siae_email_send_count_with_link",
         "siae_email_link_click_count_with_link",
         "siae_detail_display_count_with_link",
+        # "siae_email_link_click_or_detail_display_count_with_link",
         "siae_detail_contact_click_count_with_link",
         "siae_transactioned",
         "created_at",
@@ -176,6 +177,7 @@ class TenderAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
         "siae_email_send_count_with_link",
         "siae_email_link_click_count_with_link",
         "siae_detail_display_count_with_link",
+        "siae_email_link_click_or_detail_display_count_with_link",
         "siae_detail_contact_click_count_with_link",
         "logs_display",
         "extra_data_display",
@@ -281,6 +283,7 @@ class TenderAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
                     "siae_email_send_count_with_link",
                     "siae_email_link_click_count_with_link",
                     "siae_detail_display_count_with_link",
+                    "siae_email_link_click_or_detail_display_count_with_link",
                     "siae_detail_contact_click_count_with_link",
                 )
             },
@@ -445,6 +448,20 @@ class TenderAdmin(FieldsetsInlineMixin, admin.ModelAdmin):
 
     siae_detail_display_count_with_link.short_description = "S. vues"
     siae_detail_display_count_with_link.admin_order_field = "siae_detail_display_count"
+
+    def siae_email_link_click_or_detail_display_count_with_link(self, tender):
+        url = (
+            reverse("admin:siaes_siae_changelist")
+            + f"?tenders__in={tender.id}&tendersiae__detail_display_date__isnull=False"
+        )
+        return format_html(
+            f'<a href="{url}">{getattr(tender, "siae_email_link_click_or_detail_display_count", 0)}</a>'
+        )
+
+    siae_email_link_click_or_detail_display_count_with_link.short_description = "S. cliquées ou vues"
+    siae_email_link_click_or_detail_display_count_with_link.admin_order_field = (
+        "siae_email_link_click_or_detail_display_count"
+    )
 
     def siae_detail_contact_click_count_with_link(self, tender):
         url = (
