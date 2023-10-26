7dfe98589d0d02ee5ae652a44f97fdb759ead3c9
Raphael Odini <raphodn@users.noreply.github.com>
2023-10-26 14:22:45 +0200

    Label : pouvoir uploader un label dans l'admin (#946)
    * Label admin: add logo upload form (with S3 dropzone)
    
    * Add has_logo field in list

--- lemarche/labels/admin.py
+++ lemarche/labels/admin.py
@@ -5,16 +5,18 @@ from django.utils.html import format_html, mark_safe
 from lemarche.labels.models import Label
 from lemarche.utils.admin.admin_site import admin_site
 from lemarche.utils.fields import pretty_print_readonly_jsonfield
+from lemarche.utils.s3 import S3Upload
 
 
 @admin.register(Label, site=admin_site)
 class LabelAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "siae_count_annotated_with_link", "created_at"]
+    list_display = ["id", "name", "siae_count_annotated_with_link", "has_logo", "created_at"]
     search_fields = ["id", "name", "description"]
     search_help_text = "Cherche sur les champs : ID, Nom, Description"
 
     readonly_fields = [
         "siae_count_annotated_with_link",
+        "has_logo",
         "logo_url_display",
         "data_last_sync_date",
         "logs_display",
@@ -29,7 +31,7 @@ class LabelAdmin(admin.ModelAdmin):
                 "fields": ("name", "slug", "description", "website"),
             },
         ),
-        ("Logo", {"fields": ("logo_url", "logo_url_display")}),
+        ("Logo (voir en bas pour uploader)", {"fields": ("logo_url", "logo_url_display")}),
         ("Structures", {"fields": ("siae_count_annotated_with_link",)}),
         (
             "Source de données",
@@ -43,6 +45,22 @@ class LabelAdmin(admin.ModelAdmin):
         ("Stats", {"fields": ("logs_display",)}),
         ("Dates", {"fields": ("created_at", "updated_at")}),
     )
+
+    change_form_template = "labels/admin_change_form.html"
+
+    class Media:
+        js = (
+            "https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js",
+            "vendor/dropzone-5.9.3/dropzone.min.js",
+            "js/s3_upload.js",
+        )
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        s3_upload = S3Upload(kind="label_logo")
+        extra_context["s3_form_values_label_logo"] = s3_upload.form_values
+        extra_context["s3_upload_config_label_logo"] = s3_upload.config
+        return super(LabelAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -60,6 +78,12 @@ class LabelAdmin(admin.ModelAdmin):
         if not obj:
             return {"slug": ("name",)}
         return {}
+
+    def has_logo(self, instance):
+        return instance.has_logo
+
+    has_logo.boolean = True
+    has_logo.short_description = "Logo ?"
 
     def logo_url_display(self, instance):
         if instance.logo_url:
