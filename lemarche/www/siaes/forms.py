e0b3b4282ce52b923d2fb1313e0b680370f7ee8f
madjid-asa <madjid-asa@users.noreply.github.com>
2023-11-14 16:45:19 +0100

    Fix du bug de téléchargement des structures qui ont vu des dépôts de besoins (#969)
    * fix download siaes
    
    * clean some code

--- lemarche/www/siaes/forms.py
+++ lemarche/www/siaes/forms.py
@@ -306,12 +306,9 @@ class SiaeFilterForm(forms.Form):
 
         # a Tender author can export its Siae list
         tender = self.cleaned_data.get("tender", None)
-        tender_status = self.cleaned_data.get("tender_status", None)
         if tender:
-            if tender_status:  # status == "INTERESTED"
-                qs = qs.filter(tendersiae__tender=tender, tendersiae__detail_contact_click_date__isnull=False)
-            else:
-                qs = qs.filter(tendersiae__tender=tender, tendersiae__email_send_date__isnull=False)
+            tender_status = self.cleaned_data.get("tender_status", "ALL")
+            qs = qs.filter_with_tender(tender=tender, tender_status=tender_status)
 
         locations = self.cleaned_data.get("locations", None)
         if locations:
