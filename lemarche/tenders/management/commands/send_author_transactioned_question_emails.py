c1866fe1d7caad464ac172261ad482b3bf437ac6
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-23 13:10:51 +0100

    Move queryset to Tender model

--- lemarche/tenders/management/commands/send_author_transactioned_question_emails.py
+++ lemarche/tenders/management/commands/send_author_transactioned_question_emails.py
@@ -1,7 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 
 from lemarche.tenders.models import Tender
 from lemarche.www.tenders.tasks import send_tenders_author_feedback_or_survey
@@ -34,29 +33,18 @@ class Command(BaseCommand):
         self.stdout.write("Script to send email transactioned_question for tenders...")
 
         self.stdout.write("-" * 80)
-        seven_days_ago = datetime.today().date() - timedelta(days=7)
+        start_date_feature = datetime(2022, 6, 23).date()
         # we first filter on validated tenders
-        tender_qs = Tender.objects.validated()
-        # we also filter on tenders who haven't answered to the email yet
-        tender_qs = tender_qs.filter(survey_transactioned_answer=None)
-        # optional filter on kind
-        if kind:
-            tender_qs = tender_qs.filter(kind=kind)
-        # date filter
-        if is_all_tenders:
-            # all tenders with deadline_date 7 days or more
-            tender_qs = tender_qs.filter(deadline_date__lte=seven_days_ago)
-        else:
-            # only tenders with deadline_date 7 days ago and up to 23/06/2022
-            start_date_feature = datetime(2022, 6, 23).date()
-            tender_qs = tender_qs.filter(Q(deadline_date=seven_days_ago) & Q(deadline_date__gte=start_date_feature))
+        tender_qs = Tender.objects.transaction_survey_email(kind=kind, all=is_all_tenders).filter(
+            deadline_date__gte=start_date_feature
+        )
 
         self.stdout.write(f"Found {tender_qs.count()} tenders")
 
         if not dry_run:
             for tender in tender_qs:
                 send_tenders_author_feedback_or_survey(tender, kind="transactioned_question_7d")
-            self.stdout.write(f"Sent {tender_qs.count()} J+30 feedbacks")
+            self.stdout.write(f"Sent {tender_qs.count()} J+7 transactioned_question")
 
         self.stdout.write("-" * 80)
         self.stdout.write("Done!")
