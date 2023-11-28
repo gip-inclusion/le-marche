584dde308b6b62243aad38981096f2408480b411
Raphael Odini <raphodn@users.noreply.github.com>
2023-11-28 10:04:33 +0100

    [Dépôt de besoin] Modifications dans la tâche d'envoi du sondage transaction (#986)
    * Tender author survey email: update task to take deadline_date +7 days into account
    
    * Replace 30d by 7d
    
    * Move queryset to Tender model

--- lemarche/tenders/management/commands/send_author_transactioned_question_emails.py
+++ lemarche/tenders/management/commands/send_author_transactioned_question_emails.py
@@ -1,16 +1,15 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 
 from lemarche.tenders.models import Tender
-from lemarche.www.tenders.tasks import send_tenders_author_30_days
+from lemarche.www.tenders.tasks import send_tenders_author_feedback_or_survey
 
 
 class Command(BaseCommand):
     """
     Daily script to send an email to tender authors
-    When? J+30 after validation of tenders
+    When? J+7 after tender deadline_date
 
     Usage:
     python manage.py send_author_transactioned_question_emails --dry-run
@@ -26,34 +25,26 @@ class Command(BaseCommand):
             "--all",
             dest="is_all_tenders",
             action="store_true",
-            help="Send to all tenders validated 30 days ago or more",
+            help="Send to all tenders with deadline_date 7 days ago",
         )
 
     def handle(self, kind=None, dry_run=False, is_all_tenders=False, **options):
         self.stdout.write("-" * 80)
-        self.stdout.write("Script to send email feedback for tenders...")
+        self.stdout.write("Script to send email transactioned_question for tenders...")
 
         self.stdout.write("-" * 80)
-        thirty_days_ago = datetime.today().date() - timedelta(days=30)
-        tenders_validated = Tender.objects.validated()
-        if kind:
-            tenders_validated = tenders_validated.filter(kind=kind)
-        if is_all_tenders:
-            #  all tenders validated 30 days ago or more
-            tenders_for_feedbacks = tenders_validated.filter(validated_at__date__lte=thirty_days_ago)
-        else:
-            # only tenders validated 30 days ago and up to 23/06/2022
-            start_date_feature = datetime(2022, 6, 23).date()
-            tenders_for_feedbacks = tenders_validated.filter(
-                Q(validated_at__date=thirty_days_ago) & Q(validated_at__date__gte=start_date_feature)
-            )
+        start_date_feature = datetime(2022, 6, 23).date()
+        # we first filter on validated tenders
+        tender_qs = Tender.objects.transaction_survey_email(kind=kind, all=is_all_tenders).filter(
+            deadline_date__gte=start_date_feature
+        )
 
-        self.stdout.write(f"Found {tenders_for_feedbacks.count()} tenders")
+        self.stdout.write(f"Found {tender_qs.count()} tenders")
 
         if not dry_run:
-            for tender in tenders_for_feedbacks:
-                send_tenders_author_30_days(tender, kind="transactioned_question")
-            self.stdout.write(f"Sent {tenders_for_feedbacks.count()} J+30 feedbacks")
+            for tender in tender_qs:
+                send_tenders_author_feedback_or_survey(tender, kind="transactioned_question_7d")
+            self.stdout.write(f"Sent {tender_qs.count()} J+7 transactioned_question")
 
         self.stdout.write("-" * 80)
         self.stdout.write("Done!")
