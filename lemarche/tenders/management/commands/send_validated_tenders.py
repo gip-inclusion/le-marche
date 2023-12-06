3c2ed444e2c42a6c3349f7e5d322091a698806be
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-06 14:45:59 +0100

    [Dépôt de besoin] Envoyer en asynchrone les besoins validés (#998)
    * Stop sending validated Tender from admin
    
    * Add command to send validated tenders
    
    * Add cron
    
    * Improve stuff

---
+++ lemarche/tenders/management/commands/send_validated_tenders.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+
+from lemarche.tenders.models import Tender
+from lemarche.www.tenders.tasks import send_validated_tender
+
+
+class Command(BaseCommand):
+    """
+    Command to send validated tenders
+
+    Note: run via a CRON
+    "*/5 9-17 * * 1-5" = Every 5 minutes from 9am through 5pm, Monday through Friday
+    https://cron.help/#*/5_9-17_*_*_1-5
+
+    Usage: python manage.py send_validated_tenders
+    """
+
+    def handle(self, *args, **options):
+        validated_tenders_to_send = Tender.objects.validated_but_not_sent()
+        if validated_tenders_to_send.count():
+            self.stdout.write(f"Found {validated_tenders_to_send.count()} validated tender(s) to send")
+            for tender in validated_tenders_to_send:
+                send_validated_tender(tender)
