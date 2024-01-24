b341e9e732b37adf76506a007b07e33a862ac4d6
madjid.asa <madjid.asa@gmail.com>
2024-01-24 09:47:51 +0100

    split brevo api (emails and crm)

--- lemarche/utils/apis/api_brevo.py
+++ lemarche/utils/apis/api_brevo.py
@@ -1,12 +1,9 @@
-import json
 import logging
 
 import sib_api_v3_sdk
 from django.conf import settings
 from huey.contrib.djhuey import task
 from sib_api_v3_sdk.rest import ApiException
-
-from lemarche.users.models import User
 
 
 logger = logging.getLogger(__name__)
@@ -23,46 +20,6 @@ def get_config():
 def get_api_client():
     config = get_config()
     return sib_api_v3_sdk.ApiClient(config)
-
-
-def create_contact(user: User, list_id: int):
-    api_client = get_api_client()
-    api_instance = sib_api_v3_sdk.ContactsApi(api_client)
-    new_contact = sib_api_v3_sdk.CreateContact(
-        email=user.email,
-        list_ids=[list_id],
-        attributes={
-            "NOM": user.first_name,
-            "PRENOM": user.last_name,
-            "DATE_INSCRIPTION": user.created_at,
-            "TYPE_ORGANISATION": user.buyer_kind_detail,
-            "NOM_ENTREPRISE": user.company_name,
-        },
-        ext_id=str(user.id),
-        update_enabled=True,
-    )
-
-    try:
-        api_response = api_instance.create_contact(new_contact)
-        logger.info("Succes Brevo->ContactsApi->create_contact: %s\n" % api_response)
-    except ApiException as e:
-        logger.error("Exception when calling Brevo->ContactsApi->create_contact: %s\n" % e)
-
-
-def remove_contact_from_list(user: User, list_id: int):
-    api_client = get_api_client()
-    api_instance = sib_api_v3_sdk.ContactsApi(api_client)
-    contact_emails = sib_api_v3_sdk.RemoveContactFromList(emails=[user.email])
-
-    try:
-        api_response = api_instance.remove_contact_from_list(list_id=list_id, contact_emails=contact_emails)
-        logger.info("Succes Brevo->ContactsApi->remove_contact_from_list: %s\n" % api_response)
-    except ApiException as e:
-        error_body = json.loads(e.body)
-        if error_body.get("message") == "Contact already removed from list and/or does not exist":
-            logger.info("calling Brevo->ContactsApi->remove_contact_from_list: contact doesn't exist in this list")
-        else:
-            logger.error("Exception when calling Brevo->ContactsApi->remove_contact_from_list: %s\n" % e)
 
 
 @task()
