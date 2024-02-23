d19f152a469a84fd1277cd5faebbd72147de44a8
Raphael Odini <raphodn@users.noreply.github.com>
2024-02-23 12:27:43 +0100

    feat(besoins): Sondage aux prestataires intéressés : cron & task (#1085)
    * Setup cron & command
    
    * Add Tender task send_tenders_siaes_survey
    
    * Additional fixes (filter on tender without transaction info)

---
+++ lemarche/tenders/management/commands/send_siae_transactioned_question_emails.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+from django.core.management.base import BaseCommand
+
+from lemarche.tenders.models import Tender
+from lemarche.www.tenders.tasks import send_tenders_siaes_survey
+
+
+seven_days_ago = datetime.today().date() - timedelta(days=7)
+
+
+class Command(BaseCommand):
+    """
+    Daily script to send an email to tender siaes
+    Rules
+    - Tender must be "sent" + no info on "transactioned"
+    When?
+    - J+7 after tender start_working_date
+
+    Usage:
+    python manage.py send_siae_transactioned_question_emails --dry-run
+    python manage.py send_siae_transactioned_question_emails
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("--kind", type=str, dest="kind")
+        parser.add_argument("--dry-run", dest="dry_run", action="store_true", help="Dry run, no sends")
+
+    def handle(self, kind=None, reminder=False, dry_run=False, **options):
+        self.stdout.write("Script to send email tender transactioned_question to interested siaes...")
+
+        # tender must be sent & no info on transaction & start_working_date J+7
+        tender_qs = Tender.objects.sent().filter(siae_transactioned=None)
+        tender_qs = tender_qs.filter(start_working_date=seven_days_ago)
+
+        self.stdout.write(f"Found {tender_qs.count()} tenders")
+
+        if not dry_run:
+            email_kind = "transactioned_question_7d"
+            for tender in tender_qs:
+                send_tenders_siaes_survey(tender, kind=email_kind)
