9e47fe748f57802439332cd1043639f7485e4de0
Raphael Odini <raphodn@users.noreply.github.com>
2024-03-06 10:51:44 +0100

    Rebase

--- lemarche/tenders/migrations/0079_tender_siae_transactioned_last_updated.py
+++ lemarche/tenders/migrations/0079_tender_siae_transactioned_last_updated.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("tenders", "0077_alter_tender_siae_kind"),
+        ("tenders", "0078_tender_admins"),
     ]
 
     operations = [
