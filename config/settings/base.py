40507e74a1df18dfcb361283fa882f45a0ee513d
Raphael Odini <raphodn@users.noreply.github.com>
2024-04-05 11:40:01 +0200

    New env to change traces_sample_rate

--- config/settings/base.py
+++ config/settings/base.py
@@ -865,6 +865,7 @@ WAGTAIL_FRONTEND_LOGIN_URL = LOGIN_URL
 # https://docs.djangoproject.com/en/4.2/ref/settings/#data-upload-max-number-fields
 DATA_UPLOAD_MAX_NUMBER_FIELDS = os.getenv("DATA_UPLOAD_MAX_NUMBER_FIELDS", 10000)
 
+
 # Shell Plus (django-extensions)
 # ------------------------------------------------------------------------------
 
