b5fa3a59d98cf412f2744bb07c3a7eb4eeb1813c
Sébastien Reuiller <sebastien@reuiller.email>
2024-04-24 15:39:42 +0200

    makes docker env easier

--- docker/dev/django/entrypoint.sh
+++ docker/dev/django/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# Getting static files for Admin panel hosting!
 set -e
 echo ""
 echo "================================================"
-echo "= Bitoubi Api Dev docker-compose               ="
+echo "= Le marché de l'inclusion Dev docker-compose  ="
 echo "================================================"
 echo ""
 
@@ -12,11 +11,6 @@ while ! pg_isready -h $POSTGRESQL_ADDON_HOST -p $POSTGRESQL_ADDON_PORT; do
     sleep 1
 done
 
-
-# ./manage.py collectstatic --noinput
-# ./manage.py compress --force
-
 ./manage.py migrate
 
 ./manage.py runserver 0.0.0.0:8880
-# gunicorn config.wsgi:application -w 2 -b :8880 --reload
