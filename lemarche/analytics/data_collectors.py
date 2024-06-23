07d60bbe23d0ae741b38925de7e10be8a46639ed
madjid.asa <madjid.asa@gmail.com>
2024-06-23 18:15:39 +0200

    clean collectors

--- lemarche/analytics/data_collectors.py
+++ lemarche/analytics/data_collectors.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class DataCollector(ABC):
+    KIND_GLOBAL = "G"
+    KIND_PER_MODEL = "PM"
+
     @abstractmethod
     def collect_and_save_data(self, before=None, save=True):
         pass
