370c964e5f0435b47e31b095c923448dc8c14258
madjid.asa <madjid.asa@gmail.com>
2024-06-28 20:00:16 +0200

    add slug, created_at and updated_at to the model

--- lemarche/django_shepherd/admin.py
+++ lemarche/django_shepherd/admin.py
@@ -1,5 +1,6 @@
-# admin.py
+from ckeditor.widgets import CKEditorWidget
 from django.contrib import admin
+from django.db import models
 
 from lemarche.utils.admin.admin_site import admin_site
 
@@ -9,8 +10,22 @@ from .models import GuideStep, UserGuide
 class GuideStepInline(admin.TabularInline):
     model = GuideStep
     extra = 1
+    formfield_overrides = {
+        models.TextField: {"widget": CKEditorWidget(config_name="frontuser")},
+    }
 
 
 @admin.register(UserGuide, site=admin_site)
 class UserGuideAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "description",
+        "created_at",
+    ]
+
     inlines = [GuideStepInline]
+
+    formfield_overrides = {
+        models.TextField: {"widget": CKEditorWidget(config_name="frontuser")},
+    }
