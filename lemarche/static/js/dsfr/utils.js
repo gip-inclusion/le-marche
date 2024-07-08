6300adf41ea47b8143835e2ecfb80808f5693e2a
Sébastien Reuiller <sebastien@reuiller.email>
2024-07-08 15:37:53 +0200

    add asterix if input required

--- lemarche/static/js/dsfr/utils.js
+++ lemarche/static/js/dsfr/utils.js
@@ -1,29 +1,34 @@
 
-let toggleRequiredInFieldset = (toggle, element) => {
-    element.required = toggle;
-    // FIXME: find required class or add *
-    /*elementToToggle = element.parentNode.classList.contains("form-group") ? element.parentNode : element.parentNode.parentNode;
-    if (toggle) {
-        elementToToggle.classList.add('form-group-required');
-    } else {
-        elementToToggle.classList.remove('form-group-required');
-    }*/
-};
+let toggleRequiredInFieldset = (required, element) => {
+    console.log('toggleRequiredInFieldset', required, element);
+    element.required = required;
+    
+    // Find the sibling label of the element
+    let label = element.labels.length > 0 ? element.labels[0] : null;
+    if (label) {
+        // Check if the label already contains a "*" at the end
+        let isAsteriskPresent = label.textContent.trim().endsWith("*");
+        if (required && !isAsteriskPresent) {
+            label.textContent += "*";
+        } else if (!required && isAsteriskPresent) {
+            label.textContent = label.textContent.trim().slice(0, -1);
+        }
+    }
+}
 
 let toggleFieldsetElement = (toggle, element, required = undefined) => {
     if (toggle) {
         element.classList.remove('fr-hidden');
     } else {
         element.classList.add('fr-hidden');
-    }
-    
+    }    
     let inputElements = element.querySelectorAll('input');
     if (inputElements.length > 0) {
-        toggleRequiredInFieldset(toggle, inputElements[0]);
+        toggleRequiredInFieldset(required, inputElements[0]);
     } else {
         let selectElements = element.querySelectorAll('select');
         if (selectElements.length > 0) {
-            toggleRequiredInFieldset(toggle, selectElements[0]);
+            toggleRequiredInFieldset(required, selectElements[0]);
         }
     }
 }
