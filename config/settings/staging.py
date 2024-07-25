00639c7750285a387172fb2e92a8b38f578650ab
madjid.asa <madjid.asa@gmail.com>
2024-07-25 16:28:55 +0200

    try to add config for s3

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -7,6 +7,26 @@ from .sentry import sentry_init
 env = environ.Env()
 
 
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        # "BACKEND": "lemarche.utils.s3boto.S3BotoStorage",
+        "OPTIONS": {
+            "bucket_name": S3_STORAGE_BUCKET_NAME,  # noqa
+            "access_key": S3_STORAGE_ACCESS_KEY_ID,  # noqa
+            "secret_key": S3_STORAGE_SECRET_ACCESS_KEY,  # noqa
+            "endpoint_url": f"https://{S3_STORAGE_ENDPOINT_DOMAIN}",  # noqa
+            "region_name": S3_STORAGE_BUCKET_REGION,  # noqa
+            "file_overwrite": False,
+            "location": env.str("S3_LOCATION", ""),
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "staging.inclusion.beta.gouv.fr",
@@ -24,7 +44,6 @@ CSRF_TRUSTED_ORIGINS = [
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", True)
 
 MEDIA_URL = f"https://{S3_STORAGE_ENDPOINT_DOMAIN}/"  # noqa
-DEFAULT_FILE_STORAGE = "lemarche.utils.s3boto.S3BotoStorage"
 
 
 # Sentry
