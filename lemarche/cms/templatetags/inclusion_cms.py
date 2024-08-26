0670f7e3fa5cb27595697bdb2206d6954737a367
Sébastien Reuiller <sebastien@reuiller.email>
2024-08-26 15:11:11 +0200

    adapt categories tag

--- lemarche/cms/templatetags/inclusion_cms.py
+++ lemarche/cms/templatetags/inclusion_cms.py
@@ -7,5 +7,9 @@ register = template.Library()
 @register.inclusion_tag("cms/show_article_categories_badges.html")
 def show_article_categories_badges(article):
     """display list of article categories"""
-    categories = article.categories.all()
+
+    categories = []
+    if hasattr(article, "articlepage"):
+        categories = article.articlepage.categories.all()
+
     return {"categories": categories}
