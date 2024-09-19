04fc088bc231d7b297864c6c7b3628047241f8be
madjid.asa <madjid.asa@gmail.com>
2024-09-19 20:12:07 +0200

    add wagtail transfer

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail_transfer import urls as wagtailtransfer_urls
 
 from lemarche.utils.admin.admin_site import admin_site
 
@@ -24,6 +25,7 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     # url docuemnts of glog
     path("documents/", include(wagtaildocs_urls)),
+    path("wagtail-transfer/", include(wagtailtransfer_urls)),
     # urls pages blog
     path("", include("lemarche.www.pages.urls")),
     path("", include(wagtail_urls)),
