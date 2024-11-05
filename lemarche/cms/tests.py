a2c5914fd637fb28674c2d6d32cfd36634b2209a
Chlorophyllia <chloerndt@gmail.com>
2024-11-05 18:19:25 +0100

    Commande et tests pour charger des pages à partir d'un JSON

---
+++ lemarche/cms/tests.py
@@ -0,0 +1,35 @@
+import json
+
+from django.core.management import call_command
+from django.test import TestCase
+from wagtail.models import Site
+
+from content_manager.models import ContentPage
+
+
+class CreateContentPagesCommandTests(TestCase):
+    def test_create_content_pages_with_json(self):
+        with open("lemarche/fixtures/cms_content_pages.json") as f:
+            pages_data = json.load(f)
+
+        call_command("create_content_pages")
+
+        # Check that the pages were created
+        for page_data in pages_data:
+            slug = page_data["slug"]
+            self.assertTrue(
+                ContentPage.objects.filter(slug=slug).exists(), msg=f"La page avec le slug '{slug}' n'a pas été créée."
+            )
+
+    def test_prevent_duplicate_page_creation(self):
+        """Ensure the command does not create duplicate pages"""
+        home_page = Site.objects.get(is_default_site=True).root_page
+        home_page.add_child(instance=ContentPage(slug="mentions-legales", title="Mentions légales"))
+
+        call_command("create_content_pages")
+
+        self.assertEqual(
+            ContentPage.objects.filter(slug="mentions-legales").count(),
+            1,
+            msg="Une page en double a été créée pour le slug 'mentions-legales'.",
+        )
