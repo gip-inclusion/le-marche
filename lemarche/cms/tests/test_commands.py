d2a453304071f91452bd359ef9b836635b02cf81
Chloé <chloerndt@gmail.com>
2024-11-20 11:19:45 +0100

    Test : assertion sur la réponse http d'une page nouvellement créée

--- lemarche/cms/tests/test_commands.py
+++ lemarche/cms/tests/test_commands.py
@@ -1,7 +1,7 @@
 import json
 
 from django.core.management import call_command
-from django.test import TestCase
+from django.test import Client, TestCase
 from wagtail.models import Site
 
 from content_manager.models import ContentPage
@@ -9,6 +9,7 @@ from content_manager.models import ContentPage
 
 class CreateContentPagesCommandTests(TestCase):
     def test_create_content_pages_with_json(self):
+        client = Client()
         with open("lemarche/fixtures/cms_content_pages.json") as f:
             pages_data = json.load(f)
 
@@ -19,6 +20,13 @@ class CreateContentPagesCommandTests(TestCase):
             slug = page_data["slug"]
             self.assertTrue(
                 ContentPage.objects.filter(slug=slug).exists(), msg=f"La page avec le slug '{slug}' n'a pas été créée."
+            )
+
+            response = client.get(f"/{slug}/")
+            self.assertEqual(
+                response.status_code,
+                200,
+                msg=f"La page avec le slug '{slug}' n'est pas accessible (status: {response.status_code}).",
             )
 
     def test_prevent_duplicate_page_creation(self):
