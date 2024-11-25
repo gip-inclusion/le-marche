71f6053a683d2511bd320713e45fdfce92b2dbb1
Chloend <chloerndt@gmail.com>
2024-11-25 12:52:57 +0100

    Queryset home_page remplacé par un get + traduction des messages en français

--- lemarche/cms/management/commands/create_content_pages.py
+++ lemarche/cms/management/commands/create_content_pages.py
@@ -1,17 +1,23 @@
 import json
 
 from django.core.management.base import BaseCommand
-from wagtail.models import Site
+from wagtail.models import Page
 
 from content_manager.models import ContentPage
 
 
 class Command(BaseCommand):
     help = """
-    Creates a series of content pages.
+    Créé une série de pages de type ContentPage.
     """
 
     def handle(self, *args, **kwargs):
+        try:
+            home_page = Page.objects.get(title="Accueil")
+        except Page.DoesNotExist:
+            self.stdout.write(self.style.ERROR("La page 'Accueil' n'existe pas."))
+            return
+
         try:
             with open("lemarche/fixtures/cms_content_pages.json") as f:
                 pages_data = json.load(f)
@@ -28,8 +34,6 @@ class Command(BaseCommand):
             )
             return
 
-        home_page = Site.objects.filter(is_default_site=True).first().root_page
-
         for page_data in pages_data:
             slug = page_data["slug"]
             title = page_data["title"]
@@ -41,15 +45,14 @@ class Command(BaseCommand):
         """
         Creates a page for the site.
         """
-
         # Don't replace or duplicate an already existing page
         already_exists = ContentPage.objects.filter(slug=slug).first()
         if already_exists:
-            self.stdout.write(f"The {slug} page seem to already exist with id {already_exists.id}")
+            self.stdout.write(f"La page /{slug}/ semble déjà exister avec l'id {already_exists.id}")
             return already_exists
 
         new_page = parent_page.add_child(instance=ContentPage(title=title, body=body, slug=slug, show_in_menus=True))
 
-        self.stdout.write(self.style.SUCCESS(f"Page {slug} created with id {new_page.id}"))
+        self.stdout.write(self.style.SUCCESS(f"Page /{slug}/ créée avec l'id {new_page.id}"))
 
         return new_page
