61ddbc51407dcd1360b7fc3bd86aa6b062db47ed
madjid.asa <madjid.asa@gmail.com>
2024-11-27 11:18:56 +0100

    remove unused functions

--- lemarche/api/utils.py
+++ lemarche/api/utils.py
@@ -2,28 +2,6 @@ import random
 import string
 
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
-
-from lemarche.users.models import User
-
-
-# Custom Service Exceptions
-class Unauthorized(APIException):
-    status_code = 401
-    default_detail = "Unauthorized"
-    default_code = "unauthorized"
-
-
-def check_user_token(token):
-    """
-    User token functionnality is temporary, and only used
-    to trace API usage and support : once a proper
-    auth protocol is implemented it will be replaced
-    """
-    try:
-        return User.objects.has_api_key().get(api_key=token)
-    except (User.DoesNotExist, AssertionError):
-        raise Unauthorized
 
 
 def custom_preprocessing_hook(endpoints):
