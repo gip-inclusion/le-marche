be8f01bdb460258b1e23c3e38394bf8448e39dbe
Chloend <chloerndt@gmail.com>
2024-12-13 01:19:23 +0100

    Test 'reset_modification_request' Tender method

--- lemarche/www/tenders/tests.py
+++ lemarche/www/tenders/tests.py
@@ -316,6 +316,17 @@ class TenderCreateViewTest(TestCase):
 
         self.assertEqual(len(tender.logs), 2)
 
+    def test_reset_modification_request(self):
+        """Test 'reset_modification_request' method to check tender fields updates"""
+        tender, _ = self.setup_mock_user_and_tender_creation(user=self.user_buyer)
+        fields_set = set()
+        tender.reset_modification_request(fields_set)
+        tender.save()
+
+        self.assertEqual(tender.status, tender_constants.STATUS_PUBLISHED)
+        self.assertEqual(tender.email_sent_for_modification, False)
+        self.assertEqual(tender.changes_information, "")
+
 
 class TenderListViewTest(TestCase):
     @classmethod
