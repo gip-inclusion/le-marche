6c9d3ad3485baebca4e585de4477727938eaa2ed
Chloend <chloerndt@gmail.com>
2024-12-24 11:04:27 +0100

    Create 'update_tender_status_to_rejected' command with its tests

---
+++ lemarche/tenders/management/commands/update_tender_status_to_rejected.py
@@ -0,0 +1,43 @@
+from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from lemarche.tenders import constants as tender_constants
+from lemarche.tenders.models import Tender
+
+
+class Command(BaseCommand):
+    help = "Si aucune modification n'est apportée dans les 10 jours suivant la demande, le Besoin est rejeté."
+
+    def handle(self, *args, **options):
+        threshold_date = timezone.now() - timedelta(days=10)
+        tenders_to_update = []
+
+        tenders_draft = Tender.objects.filter(status=tender_constants.STATUS_DRAFT)
+        tenders_draft_count = tenders_draft.count()
+
+        self.stdout.write(f"Besoin(s) à traiter : {tenders_draft_count}")
+
+        for tender in tenders_draft:
+            email_sent_at = None
+            for log_entry in tender.logs:
+                if log_entry.get("action") == "send_tender_author_modification_request":
+                    email_sent_at = log_entry.get("email_sent_at")
+                    break
+
+            if email_sent_at:
+                email_sent_at_date = timezone.now().strptime(email_sent_at, "%Y-%m-%dT%H:%M:%S.%f%z")
+                if email_sent_at_date <= threshold_date:
+                    tenders_to_update.append(tender)
+
+        for tender in tenders_to_update:
+            tender.status = tender_constants.STATUS_REJECTED
+            tender.save(update_fields=["status"])
+
+        if not tenders_to_update:
+            self.stdout.write("Aucun besoin rejeté")
+        elif len(tenders_to_update) == 1:
+            self.stdout.write("1 besoin rejeté")
+        else:
+            self.stdout.write(f"{len(tenders_to_update)} besoins rejetés")
