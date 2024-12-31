987b04484948484015c564b9d795ce60383829ae
Guilouf <Guilouf@users.noreply.github.com>
2024-12-31 10:33:20 +0100

    fix: Erreurs de serialisation des inbound email (#1619)

--- lemarche/api/emails/serializers.py
+++ lemarche/api/emails/serializers.py
@@ -7,24 +7,26 @@ class UserInboundParsingSerializer(serializers.DictField):
 
 
 class EmailItemSerializer(serializers.Serializer):
-    """Email ItemSerializer for data comes from Brevo"""
+    """Email ItemSerializer for data comes from Brevo
+    https://developers.brevo.com/docs/inbound-parse-webhooks#parsed-email-payload
+    """
 
     Uuid = serializers.ListField(child=serializers.UUIDField())
     MessageId = serializers.CharField()
-    InReplyTo = serializers.CharField(required=False, allow_null=True)
+    InReplyTo = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     From = UserInboundParsingSerializer()
     To = serializers.ListField(child=UserInboundParsingSerializer())
     Cc = serializers.ListField(child=UserInboundParsingSerializer(), required=False)
-    ReplyTo = serializers.CharField(required=False, allow_null=True)
+    ReplyTo = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     SentAtDate = serializers.CharField()
     Subject = serializers.CharField(required=False, allow_null=True)
-    RawHtmlBody = serializers.CharField(required=False, allow_null=True)
-    RawTextBody = serializers.CharField(required=False, allow_null=True)
+    RawHtmlBody = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    RawTextBody = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     ExtractedMarkdownMessage = serializers.CharField(required=False, allow_null=True)
-    ExtractedMarkdownSignature = serializers.CharField(required=False, allow_null=True)
-    SpamScore = serializers.FloatField()
+    ExtractedMarkdownSignature = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    SpamScore = serializers.FloatField(required=False)
     Attachments = serializers.ListField(child=serializers.DictField(), required=False)
-    Headers = serializers.DictField()
+    Headers = serializers.DictField(required=False)
 
 
 class EmailsSerializer(serializers.Serializer):
