d08a0f50d25851aae1c9f6fcdeb545e83b3273c5
Guilouf <Guilouf@users.noreply.github.com>
2025-01-23 17:16:37 +0100

    Remove sectors, geo_range and geo_range_custom_distance from siae model

--- lemarche/api/sectors/serializers.py
+++ lemarche/api/sectors/serializers.py
@@ -22,12 +22,3 @@ class SectorSerializer(serializers.ModelSerializer):
             "slug",
             "group",
         ]
-
-
-class SectorSimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sector
-        fields = [
-            "name",
-            "slug",
-        ]
