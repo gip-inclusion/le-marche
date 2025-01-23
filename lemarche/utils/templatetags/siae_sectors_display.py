39e702b45a630fd2c04467e002bc651f87612047
Guilouf <Guilouf@users.noreply.github.com>
2025-01-23 18:22:48 +0100

    Fix test for template and and remove "multisectoriel" as it was not displayed before

--- lemarche/utils/templatetags/siae_sectors_display.py
+++ lemarche/utils/templatetags/siae_sectors_display.py
@@ -20,9 +20,6 @@ def siae_sectors_display(object, display_max=5, current_search_query="", output_
     # if the search query contains sectors, filter values on these sectors
     if "sectors=" in current_search_query:
         values = [elem for elem in values if elem["slug"] in current_search_query]
-    # if SIAE ETTI with many sectors, then display simply "Multisectoriel"
-    elif hasattr(object, "kind") and object.kind == "ETTI" and len(values) > 3:
-        values = [{"name": "Multisectoriel"}]
 
     # get list of names
     values = [force_str(elem["name"]) for elem in values if elem["name"] != "Autre"]
