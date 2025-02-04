66ea90a412c260a3ad8455dc9297040e4b6c7e0b
Guilouf <Guilouf@users.noreply.github.com>
2025-02-04 14:34:34 +0100

    Display only when user has not seen it

--- lemarche/django_shepherd/context_processor.py
+++ lemarche/django_shepherd/context_processor.py
@@ -1,3 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from lemarche.django_shepherd.models import UserGuide
+
+
 def expose_guide_context(request):
-    display_guide = True
+    """
+    DISPLAY_GUIDE is set to True if for the current url a display guide is found and the current user
+    have not completed it yet
+    """
+    url = request.path.strip("/")
+    try:
+        user_guide = UserGuide.objects.get(url__contains=url)
+    except ObjectDoesNotExist:
+        display_guide = False
+    else:
+        display_guide = not user_guide.guided_users.filter(id=request.user.id).exists()
     return {"DISPLAY_GUIDE": display_guide}
