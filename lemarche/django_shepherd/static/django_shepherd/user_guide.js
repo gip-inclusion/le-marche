50f6861579c66e45ccab865dbc21dbe3691e29ad
Guilouf <Guilouf@users.noreply.github.com>
2025-02-04 18:47:57 +0100

    Local module for shepherd.js

--- lemarche/django_shepherd/static/django_shepherd/user_guide.js
+++ lemarche/django_shepherd/static/django_shepherd/user_guide.js
@@ -1,73 +1,61 @@
-class UserGuide {
-    constructor() {
-        this.tour = new Shepherd.Tour({
-            useModalOverlay: true,
-            confirmCancel: true,
-            confirmCancelMessage: "Ëtes-vous sûr d'annuler ce guide des foncctionnalités ? Il ne vous sera plus proposé",
-            defaultStepOptions: {
-                classes: 'shepherd-theme-arrows',
-                scrollTo: {
-                    behavior: 'smooth',
-                    block: 'center'
-                },
-            }
-        });
+import Shepherd from "shepherd_map";
 
-        // Once cancelled or completed
-        this.tour.on("cancel", this.onTourEnd);
-        this.tour.on("complete", this.onTourEnd);
+const tour = new Shepherd.Tour({
+    useModalOverlay: true,
+    confirmCancel: true,
+    confirmCancelMessage: "Ëtes-vous sûr d'annuler ce guide des foncctionnalités ? Il ne vous sera plus proposé",
+    defaultStepOptions: {
+        classes: 'shepherd-theme-arrows',
+        scrollTo: {
+            behavior: 'smooth',
+            block: 'center'
+        },
     }
+});
 
-    init() {
-        document.addEventListener('startUserGuide', (event) => {
-            const guideName = event.detail.guideName;
-            this.startGuide(guideName);
-        });
+// Once cancelled or completed
+tour.on("cancel", onTourEnd);
+tour.on("complete", onTourEnd);
 
-        // Listen to htmx events
-        document.body.addEventListener('htmx:afterSwap', (event) => {
-            if (event.detail.target.id === 'guideContainer') {
-                const guideName = event.detail.target.getAttribute('data-guide-name');
-                this.startGuide(guideName);
-            }
-        });
-    }
-    /*Called on ending tour events. Call an url to add the user to already viewed guides*/
-    onTourEnd() {
-        fetch(`/django_shepherd/viewed_guide/${displayGuidePk}/`)
-    }
-
-    startGuide() {
-        this.tour.steps = []; // Clear previous steps
-        displayGuidePayload.steps.forEach((step, index) => {
-            const isFirstStep = index === 0;
-            const isLastStep = index === displayGuidePayload.steps.length - 1;
-            this.tour.addStep({
-                title: step.title,
-                text: step.text,
-                attachTo: {
-                    element: step.element,
-                    on: step.position
-                },
-                buttons: [
-                    !isLastStep && {
-                        text: 'Ignorer',
-                        action: this.tour.cancel,
-                        classes: 'btn btn-secondary'
-                    },
-                    !isFirstStep && {
-                        text: 'Précédent',
-                        action: this.tour.back,
-                        classes: 'btn btn-primary'
-                    },
-                    {
-                        text: isLastStep ? 'Finir' : 'Suivant',
-                        action: this.tour.next,
-                        classes: 'btn ' + (isLastStep ? 'btn-success' : 'btn-primary')
-                    }
-                ].filter(Boolean)
-            });
-        });
-        this.tour.start();
-    }
+/*Called on ending tour events. Call an url to add the user to already viewed guides*/
+function onTourEnd() {
+    // "this" refers to a "tour" instance
+    fetch(`/django_shepherd/viewed_guide/${this.displayGuidePk}/`)
 }
+
+function startGuide(displayGuidePayload, displayGuidePk) {
+    tour.displayGuidePk = displayGuidePk
+    tour.steps = []; // Clear previous steps
+    displayGuidePayload.steps.forEach((step, index) => {
+        const isFirstStep = index === 0;
+        const isLastStep = index === displayGuidePayload.steps.length - 1;
+        tour.addStep({
+            title: step.title,
+            text: step.text,
+            attachTo: {
+                element: step.element,
+                on: step.position
+            },
+            buttons: [
+                !isLastStep && {
+                    text: 'Ignorer',
+                    action: tour.cancel,
+                    classes: 'btn btn-secondary'
+                },
+                !isFirstStep && {
+                    text: 'Précédent',
+                    action: tour.back,
+                    classes: 'btn btn-primary'
+                },
+                {
+                    text: isLastStep ? 'Finir' : 'Suivant',
+                    action: tour.next,
+                    classes: 'btn ' + (isLastStep ? 'btn-success' : 'btn-primary')
+                }
+            ].filter(Boolean)
+        });
+    });
+    tour.start();
+}
+
+export {startGuide as default}
