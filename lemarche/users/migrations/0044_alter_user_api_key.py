9e13078534f917a5030b6ae948eddbe64e3c7c74
Sébastien Reuiller <sebastien@reuiller.email>
2025-02-07 09:32:07 +0100

    fix migration conflict

--- lemarche/users/migrations/0044_alter_user_api_key.py
+++ lemarche/users/migrations/0044_alter_user_api_key.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("users", "0039_remove_user_user_email_ci_uniqueness_and_more"),
+        ("users", "0043_user_is_anonymized"),
     ]
 
     operations = [
