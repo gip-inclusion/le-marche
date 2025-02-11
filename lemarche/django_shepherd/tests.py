3b3dfd6bb366ecfa32194c0d6ec9d6f983ce5067
Guilouf <Guilouf@users.noreply.github.com>
2025-02-11 11:22:22 +0100

    guide context test case

--- lemarche/django_shepherd/tests.py
+++ lemarche/django_shepherd/tests.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory, TestCase
 
 from lemarche.django_shepherd.context_processor import expose_guide_context
@@ -10,7 +11,9 @@ class GuideContextTestCase(TestCase):
     def setUp(self):
         self.request_factory = RequestFactory()
         self.url_with_param = "http://localhost:8000/prestataires/truc/?blalb=something"
+        self.already_seen_user = get_user_model().objects.create_user(email="fake@email.com", password="1234")
         self.guide = UserGuide.objects.create(name="guide_1", url=self.url_with_param)
+        self.guide.guided_users.add(self.already_seen_user)
 
     def test_anonymous_user(self):
         request = self.request_factory.get(self.url_with_param)
@@ -26,7 +29,7 @@ class GuideContextTestCase(TestCase):
         )
 
     def test_logged_user(self):
-        user = User()
+        user = get_user_model()()
         request = self.request_factory.get(self.url_with_param)
         request.user = user
 
@@ -37,5 +40,19 @@ class GuideContextTestCase(TestCase):
                 "DISPLAY_GUIDE_FLAG": True,
                 "DISPLAY_GUIDE_PK": self.guide.pk,
                 "DISPLAY_GUIDE_PAYLOAD": {"steps": []},
+            },
+        )
+
+    def test_already_seen_user(self):
+        request = self.request_factory.get(self.url_with_param)
+        request.user = self.already_seen_user
+
+        context = expose_guide_context(request)
+        self.assertEqual(
+            context,
+            {
+                "DISPLAY_GUIDE_FLAG": False,
+                "DISPLAY_GUIDE_PK": None,
+                "DISPLAY_GUIDE_PAYLOAD": None,
             },
         )
