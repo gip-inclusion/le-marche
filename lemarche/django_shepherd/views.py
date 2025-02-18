45e2bf136f571f6770c73da4d6f725cd79c0230d
Guilouf <Guilouf@users.noreply.github.com>
2025-02-18 17:29:46 +0100

    Restrict to logged users only

--- lemarche/django_shepherd/views.py
+++ lemarche/django_shepherd/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views.generic.detail import BaseDetailView
 
 from lemarche.django_shepherd.models import UserGuide
 
 
-class StepViewedView(BaseDetailView):
+class StepViewedView(LoginRequiredMixin, BaseDetailView):
     queryset = UserGuide.objects.all()
 
     def get(self, request, *args, **kwargs):
