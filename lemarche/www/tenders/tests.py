50e35b6a32764d7da96aa89fe369bcfacb8fdf8d
Guilouf <Guilouf@users.noreply.github.com>
2025-02-24 17:19:28 +0100

    Fix missing template

--- lemarche/www/tenders/tests.py
+++ lemarche/www/tenders/tests.py
@@ -1810,6 +1810,7 @@ class TenderQuestionAnswerTestCase(TestCase):
         TenderSiaeFactory(tender=self.tender, siae=self.siae_2)
 
         TemplateTransactionalFactory(name="gfdg", code="TENDERS_AUTHOR_SIAE_INTERESTED_1")
+        TemplateTransactionalFactory(name="gfdg", code="TENDERS_AUTHOR_SIAE_INTERESTED_2")
 
     def test_with_authenticated_user_single_siae(self):
         url = reverse("tenders:detail-contact-click-stat", kwargs={"slug": self.tender.slug})
