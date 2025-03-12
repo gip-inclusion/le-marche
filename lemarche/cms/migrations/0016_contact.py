a6bcaabf85d5e548628fea4d53eac248a7065478
Guilouf <Guilouf@users.noreply.github.com>
2025-03-12 11:36:42 +0100

    fix missing clsing

--- lemarche/cms/migrations/0016_contact.py
+++ lemarche/cms/migrations/0016_contact.py
@@ -6,7 +6,7 @@ def paragraph_migration(apps, schema_editor):
     Paragraph.objects.create(
         title="Vous avez des question ?",
         slug="rdv-contact",
-        content='<h4 data-block-key="2tu4c"><b>Vous avez des questions ?</h4><p data-block-key="3vo75">Contactez nous au<br/><b>06 66 66 66 66</b></p>',
+        content='<h4 data-block-key="2tu4c"><b>Vous avez des questions ?</b></h4><p data-block-key="3vo75">Contactez nous au<br/><b>06 66 66 66 66</b></p>',
     )
 
 
