5949c8ef70c92a7d5fd1c764616d66c7d5e5e14f
Sébastien Reuiller <sebastien@reuiller.email>
2025-04-01 15:49:41 +0200

    chore: disable authentication for InboundParsingEmailView

--- lemarche/api/emails/views.py
+++ lemarche/api/emails/views.py
@@ -48,6 +48,7 @@ class BrevoWhitelistPermission(BasePermission):
 
 class InboundParsingEmailView(APIView):
     permission_classes = [BrevoWhitelistPermission]  # override the default class that requires Authentication
+    authentication_classes = []  # override the default class that also requires Authentication
 
     @extend_schema(exclude=True)
     def post(self, request):
