0f257a9bcdd0aea6a9df47b8ed4bf4d8291e5be2
Guilouf <gui.lebreton@gmail.com>
2025-04-03 15:22:44 +0200

    Remove advert

--- lemarche/cms/snippets.py
+++ lemarche/cms/snippets.py
@@ -31,41 +31,6 @@ class ArticleCategory(models.Model):
 
 
 @register_snippet
-class Advert(models.Model):
-    title = models.CharField(max_length=255, verbose_name="Titre de l'encart")
-    image = models.ForeignKey(
-        "wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
-    )
-
-    # call to action
-    cta_url = models.URLField(null=True, blank=True)
-    cta_id = models.SlugField(
-        verbose_name="slug",
-        allow_unicode=True,
-        max_length=255,
-        help_text="id du call to action (pour le suivi)",
-    )
-    cta_text = models.CharField(max_length=255, verbose_name="Titre du call to action")
-    cta_open_in_new_tab = models.BooleanField(default=True, verbose_name="Ouvrir dans une nouvelle fenêtre")
-
-    panels = [
-        FieldPanel("title"),
-        FieldPanel("image"),
-        FieldPanel("cta_url"),
-        FieldPanel("cta_id"),
-        FieldPanel("cta_text"),
-        FieldPanel("cta_open_in_new_tab"),
-    ]
-
-    class Meta:
-        verbose_name = "Panneau publicitaire"
-        verbose_name_plural = "Panneaux publicitaires"
-
-    def __str__(self):
-        return self.title
-
-
-@register_snippet
 class InfoCard(models.Model):
     title = models.CharField(max_length=255, verbose_name="Titre de l'encart")
     slug = models.SlugField(
