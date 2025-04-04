b1328032149c45465ed1f985847445426ad821c3
Guilouf <gui.lebreton@gmail.com>
2025-04-04 15:38:56 +0200

    Remove HomePage and FaqPage

--- lemarche/cms/models.py
+++ lemarche/cms/models.py
@@ -1,15 +1,9 @@
-from django.db import models
 from django.db.models import Count, Q
 from django.shortcuts import get_object_or_404
-from wagtail import blocks as wagtail_blocks
-from wagtail.admin.panels import FieldPanel
-from wagtail.blocks import RichTextBlock
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
-from wagtail.fields import StreamField
 from wagtail.models import Page
 
 from content_manager.models import ContentPage, Tag
-from lemarche.cms import blocks
 
 
 class ArticleList(RoutablePageMixin, Page):
@@ -64,117 +58,3 @@ class ArticleList(RoutablePageMixin, Page):
         )
 
     subpage_types = ["content_manager.ContentPage"]
-
-
-class HomePage(Page):
-    banner_title = models.CharField(
-        default="Votre recherche de prestataires inclusifs est chronophage ?", max_length=120
-    )
-    banner_subtitle = models.CharField(
-        blank=True, max_length=120, default="Confiez votre sourcing au marché de l'inclusion !"
-    )
-    banner_arguments_list = StreamField(
-        [
-            ("item", wagtail_blocks.CharBlock()),
-        ],
-        min_num=3,
-        max_num=3,
-        use_json_field=True,
-    )
-    banner_cta_section = StreamField(
-        [
-            ("cta_primary", blocks.CallToAction(label="CTA primaire")),
-            ("cta_secondary", blocks.CallToAction(label="CTA secondaire")),
-            ("cta_primary_auth", blocks.CallToAction(label="CTA primaire connecté")),
-            ("cta_secondary_auth", blocks.CallToAction(label="CTA secondaire connecté")),
-        ],
-        min_num=1,
-        max_num=4,
-        use_json_field=True,
-        null=True,
-    )
-
-    content = StreamField(
-        [
-            ("website_stats", blocks.StatsWebsite()),
-            ("section_they_publish_tenders", blocks.TendersTestimonialsSection()),
-            ("section_studies_cases_tenders", blocks.TendersStudiesCasesSection()),
-            ("section_our_siaes", blocks.OurSiaesSection()),  # TODO: to be remove after deploy
-            ("section_ecosystem", blocks.EcosystemSection()),
-            ("section_our_ressources", blocks.OurRessourcesSection()),
-            ("section_what_find_here", blocks.WhatFindHereSection()),
-            ("section_our_partners", blocks.OurPartnersSection()),
-            ("section_our_features", blocks.OurFeaturesSection()),
-            ("section_why_call_siaes", blocks.WhyCallSiaes()),
-        ],
-        null=True,
-        block_counts={
-            "website_stats": {"max_num": 1},
-            "section_they_publish_tenders": {"max_num": 1},
-            "section_studies_cases_tenders": {"max_num": 1},
-            "section_our_siaes": {"max_num": 1},
-            "section_our_ressources": {"max_num": 1},
-            "section_what_find_here": {"max_num": 1},
-            "section_our_partners": {"max_num": 1},
-            "section_our_features": {"max_num": 1},
-            "section_why_call_siaes": {"max_num": 1},
-        },
-        use_json_field=True,
-    )
-
-    sub_header_custom_message = StreamField(
-        [
-            ("message", RichTextBlock(label="Message personnalisé du bandeau")),
-        ],
-        blank=True,
-        null=True,
-        verbose_name="Message personnalisé du bandeau",
-        help_text="Contenu affiché dans le bandeau sous l'en-tête.",
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel("sub_header_custom_message"),
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        FieldPanel("banner_arguments_list"),
-        FieldPanel("banner_cta_section"),
-        FieldPanel("content"),
-    ]
-
-    page_description = "Use this page type like a landing page"
-    parent_page_types = ["wagtailcore.Page", "cms.HomePage"]
-
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request, *args, **kwargs)
-        context["sub_header_custom_message"] = self.sub_header_custom_message
-        return context
-
-
-class FAQPage(Page):
-    intro = models.TextField(verbose_name="Introduction de la page", null=True, blank=True)
-    image = models.ForeignKey(
-        "wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel("intro", classname="full"),
-        FieldPanel(
-            "image",
-            classname="collapsible",
-        ),
-        FieldPanel("faqs"),
-    ]
-
-    faqs = StreamField(
-        [
-            ("faq_group", blocks.FAQGroupBlock()),
-        ],
-        blank=True,
-        use_json_field=True,
-    )
-
-    parent_page_types = ["wagtailcore.Page", "cms.HomePage", "cms.ArticleList"]
-
-    class Meta:
-        verbose_name = "FAQ Page"
-        verbose_name_plural = "FAQ Pages"
