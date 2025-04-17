e6a9b38d9d4e5310490241fd2dd381058d0972aa
Guilouf <Guilouf@users.noreply.github.com>
2025-04-17 16:24:11 +0200

    Fix rebase migrations

--- lemarche/tenders/migrations/0099_alter_tender_external_link.py
+++ lemarche/tenders/migrations/0099_alter_tender_external_link.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("tenders", "0097_questionanswer"),
+        ("tenders", "0098_update_rejected"),
     ]
 
     operations = [
