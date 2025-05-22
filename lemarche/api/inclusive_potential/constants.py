1d3593ac7ac301ecc6e663c26e3a01e50fd24aa8
Sébastien Reuiller <sebastien@reuiller.email>
2025-05-22 18:38:13 +0200

    add interpretation

---
+++ lemarche/api/inclusive_potential/constants.py
@@ -0,0 +1,66 @@
+RECOMMENDATIONS = {
+    "reservation": {
+        "title": "Réservation totale",
+        "response": "Vous pouvez réserver l’ensemble de votre projet d’achat à des fournisseurs inclusifs !",
+        "explanation": (
+            "Après analyse, nous identifions {siaes_count} fournisseurs potentiels avec un taux de"
+            "dépendance économique moyen de {eco_dependency}% et un chiffre d'affaires moyen de {ca_average}€.\n\n"
+            "Votre projet d'achat présente donc un fort potentiel inclusif. L'analyse croisée du nombre de "
+            "fournisseurs inclusifs et le niveau de dépendance économique démontre que l'offre locale est suffisante "
+            "et bien dimensionnée pour répondre à l'ensemble de votre projet d'achat.\n\n"
+            "Vous pouvez donc envisager de réserver l'ensemble du marché à des fournisseurs inclusifs (art. L.2113-12"
+            " et 13, L.3113-2-1), si le besoin est homogène."
+        ),
+    },
+    "lot": {
+        "title": "Lot réservé",
+        "response": "Vous pouvez allotir votre projet d'achat et en réserver certains lots !",
+        "explanation": (
+            "Après analyse, nous identifions {siaes_count} fournisseurs potentiels avec un taux de"
+            "dépendance économique moyen de {eco_dependency}% et un chiffre d'affaires moyen de {ca_average}€.\n\n"
+            "Votre projet d'achat a donc un très bon potentiel inclusif, mais il ne peut pas être totalement couvert "
+            "par un fournisseur inclusif seul car l'offre n'est pas suffisamment dimensionné pour répondre à "
+            "l'ensemble de votre projet.\n\n"
+            "Cependant, nous vous recommandons d'allotir votre projet d'achat et d'en réserver un ou plusieurs lots "
+            "pour n'en réserver qu'une partie à des fournisseurs inclusifs."
+        ),
+    },
+    "clause": {
+        "title": "Clause sociale d'exécution",
+        "response": "Vous pouvez intégrer une clause sociale d'exécution dans votre projet d'achat !",
+        "explanation": (
+            "Après analyse, nous identifions {siaes_count} fournisseurs potentiels avec un taux de"
+            "dépendance économique moyen de {eco_dependency}% et un chiffre d'affaires moyen de {ca_average}€.\n\n"
+            "Votre projet d'achat a donc un bon potentiel inclusif, mais il ne peut pas être totalement couvert par un"
+            "fournisseur inclusif seul car l'analyse économique de l'offre inclusive montre des risques de "
+            "dépendances économiques. Cependant, nous vous recommandons d'opter pour l'intégration d'une clause "
+            "sociale d'insertion (art. L.2112-2), c'est-à-dire une obligation pour le titulaire du marché de "
+            "réaliser un volume d'heures d'insertion (ex : embaucher une personne éloignée de l'emploi ou "
+            "sous-traiter à un fournisseur inclusif)."
+            "Cette clause permet d'agir sans modifier profondément la procédure."
+        ),
+    },
+    "critere": {
+        "title": "Critère social d'attribution",
+        "response": "Vous pouvez introduire un critère social dans l'analyse des offres !",
+        "explanation": (
+            "Après analyse, nous identifions {siaes_count} fournisseurs potentiels avec un taux de"
+            "dépendance économique moyen de {eco_dependency}% et un chiffre d'affaires moyen de {ca_average}€.\n\n"
+            "L'analyse montre un potentiel inclusif faible : l'offre inclusive locale est limitée pour justifier une "
+            "clause ou une réservation. Mais vous pouvez toujours introduire un critère social dans l'analyse des "
+            "offres (art. R.2152-7). Ce critère permet de valoriser les soumissionnaires qui font des efforts pour "
+            "embaucher localement, travailler avec le secteur de l'inclusion, former du public, etc. Ce levier est "
+            "pertinent pour encourager des pratiques responsables sans contraindre le déroulement de votre marché."
+        ),
+    },
+    "aucun": {
+        "title": "Aucun potentiel inclusif identifié",
+        "response": "Malheureusement, le potentiel inclusif de votre projet d'achat est trop faible !",
+        "explanation": (
+            "Votre projet d'achat ne permet pas, à ce stade, d'intégrer un dispositif inclusif : il génère trop de "
+            "dépendance économique ou il n'existe aucun fournisseur inclusif en capacité d'y répondre dans votre zone "
+            "géographique. Cependant, pour vérifier la faisabilité de votre projet d'achat, nous vous "
+            "recommandons de lancer un sourcing inversé sur le Marché de l'inclusion."
+        ),
+    },
+}
--- lemarche/api/inclusive_potential/views.py
+++ lemarche/api/inclusive_potential/views.py
@@ -2,6 +2,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from lemarche.api.inclusive_potential.constants import RECOMMENDATIONS
 from lemarche.api.inclusive_potential.serializers import InclusivePotentialQuerySerializer
 from lemarche.siaes.constants import KIND_HANDICAP_LIST, KIND_INSERTION_LIST
 from lemarche.siaes.models import Siae
@@ -35,26 +36,50 @@ class InclusivePotentialView(APIView):
             if siae.super_badge:
                 siaes_with_super_badge += 1
 
-        eco_dependency = None
-        ca_average = None
+        siaes_count = siaes.count()
+        analysis_data = {}
         if budget:
-            # Get all valid CA values from siaes
+            # Get all valid CA values from siaes and calculate the average manually to avoid to many fat queries
             ca_values = [siae.ca or siae.api_entreprise_ca for siae in siaes if siae.ca or siae.api_entreprise_ca]
-
             if ca_values:
-                ca_average = round(sum(ca_values) / len(ca_values))
-                eco_dependency = round(budget / ca_average * 100)
+                analysis_data["ca_average"] = round(sum(ca_values) / len(ca_values))
+                analysis_data["eco_dependency"] = round(budget / analysis_data["ca_average"] * 100)
 
-        return Response(
-            {
-                "sector_name": sector.name,
-                "perimeter_name": perimeter.name if perimeter else None,
-                "perimeter_kind": perimeter.kind if perimeter else None,
-                "potential_siaes": siaes.count(),
-                "insertion_siaes": insertion_siaes,
-                "handicap_siaes": handicap_siaes,
-                "siaes_with_super_badge": siaes_with_super_badge,
-                "eco_dependency": eco_dependency,
-                "ca_average": ca_average,
-            }
-        )
+            recommendation = None
+            if siaes_count > 30:
+                if "eco_dependency" in analysis_data and analysis_data["eco_dependency"] < 30:
+                    recommendation = RECOMMENDATIONS["reservation"]
+                else:
+                    recommendation = RECOMMENDATIONS["lot"]
+            elif siaes_count > 10:
+                recommendation = RECOMMENDATIONS["lot"]
+
+            elif siaes_count > 1:
+                recommendation = RECOMMENDATIONS["clause"]
+            else:
+                recommendation = RECOMMENDATIONS["aucun"]
+
+            recommendation["explanation"] = (
+                recommendation["explanation"].format(
+                    siaes_count=siaes_count,
+                    eco_dependency=analysis_data["eco_dependency"],
+                    ca_average=analysis_data["ca_average"],
+                )
+                if "eco_dependency" in analysis_data and "ca_average" in analysis_data
+                else None
+            )
+            analysis_data["recommendation"] = recommendation
+
+        data = {
+            "sector_name": sector.name,
+            "perimeter_name": perimeter.name if perimeter else None,
+            "perimeter_kind": perimeter.kind if perimeter else None,
+            "potential_siaes": siaes_count,
+            "insertion_siaes": insertion_siaes,
+            "handicap_siaes": handicap_siaes,
+            "siaes_with_super_badge": siaes_with_super_badge,
+        }
+        if budget:
+            data.update(analysis_data)
+
+        return Response(data)
