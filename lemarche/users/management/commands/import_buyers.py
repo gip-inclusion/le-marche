1fd8699ba679a8c9da5031a9364ffe13dd319748
Guilouf <Guilouf@users.noreply.github.com>
2025-06-05 12:11:35 +0200

    Create users from csv file

--- lemarche/users/management/commands/import_buyers.py
+++ lemarche/users/management/commands/import_buyers.py
@@ -1,10 +1,19 @@
+import csv
+
 from django.core.management.base import BaseCommand
+
+from lemarche.users.models import User
 
 
 class Command(BaseCommand):
     """Import new buyers from a csv file."""
 
     def add_arguments(self, parser):
+        parser.add_argument(
+            "filename",
+            type=str,
+            help="Fichier csv contenant les acheteurs à importer.",
+        )
         parser.add_argument(
             "--brevo-template_id",
             type=int,
@@ -17,4 +26,20 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        pass
+        with open(options["filename"], "r", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            imported_list = list(reader)
+
+        for imported_user in imported_list:
+            User.objects.create_user(
+                email=imported_user["EMAIL"],
+                first_name=imported_user["FIRST_NAME"],
+                last_name=imported_user["LAST_NAME"],
+                phone=imported_user["PHONE"],
+                kind=User.KIND_BUYER,
+                # company_name=imported_user["Nom de la société"],
+                position=imported_user["POSITION"],
+                accept_rgpd=True,
+                accept_survey=True,
+                password=None,
+            )
