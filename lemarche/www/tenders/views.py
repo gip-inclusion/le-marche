4d2237ffa272db2e52e44ee187e53223a9f17614
Guilouf <gui.lebreton@gmail.com>
2025-07-28 13:05:49 +0200

    Do not display optionnal questions for KIND_TENDERS

--- lemarche/www/tenders/views.py
+++ lemarche/www/tenders/views.py
@@ -132,7 +132,13 @@ class TenderCreateMultiStepView(SessionWizardView):
             if self.instance.id:  # for draft
                 kwargs["questions_list"] = list(self.instance.questions_list())
             else:  # for new Tender, display suggested questions first
-                kwargs["questions_list"] = [{"text": question.text} for question in SuggestedQuestion.objects.all()]
+                # Do not display suggested questions for KIND_TENDER
+                if kwargs["kind"] == tender_constants.KIND_TENDER:
+                    kwargs["questions_list"] = []
+                else:
+                    kwargs["questions_list"] = [
+                        {"text": question.text} for question in SuggestedQuestion.objects.all()
+                    ]
         if step == self.STEP_CONTACT:
             kwargs["kind"] = self.get_cleaned_data_for_step(self.STEP_GENERAL).get("kind")
             detail_data = self.get_cleaned_data_for_step(self.STEP_DETAIL)
