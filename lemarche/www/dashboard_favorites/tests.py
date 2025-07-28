5dc9fb4ecdca2b3ced9eac41cc7b4821364384d3
Guilouf <gui.lebreton@gmail.com>
2025-07-28 12:38:28 +0200

    Fix tests no strong enough

--- lemarche/www/dashboard_favorites/tests.py
+++ lemarche/www/dashboard_favorites/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from lemarche.favorites.factories import FavoriteListFactory
+from lemarche.siaes.factories import SiaeActivityFactory, SiaeFactory
 from lemarche.users.factories import UserFactory
 from lemarche.users.models import User
 
@@ -11,8 +12,16 @@ class DashboardFavoriteListViewTest(TestCase):
     def setUpTestData(cls):
         cls.user_favorite_list = UserFactory(kind=User.KIND_BUYER)
         cls.other_user_favorite_list = UserFactory(kind=User.KIND_PARTNER)
-        cls.favorite_list_1 = FavoriteListFactory(name="Liste 1", user=cls.user_favorite_list)
-        cls.favorite_list_2 = FavoriteListFactory(name="Liste 2", user=cls.other_user_favorite_list)
+        # Additional data to check the correct prefetch
+        siae_1 = SiaeFactory()
+        siae_2 = SiaeFactory()
+        SiaeActivityFactory(
+            siae=siae_1,
+        )
+        cls.favorite_list_1 = FavoriteListFactory(name="Liste 1", user=cls.user_favorite_list, siaes=[siae_1, siae_2])
+        cls.favorite_list_2 = FavoriteListFactory(
+            name="Liste 2", user=cls.other_user_favorite_list, siaes=[siae_1, siae_2]
+        )
 
     def test_anonymous_user_cannot_view_favorite_list(self):
         url = reverse("dashboard_favorites:list")
@@ -36,6 +45,6 @@ class DashboardFavoriteListViewTest(TestCase):
         url = reverse("dashboard_favorites:list_detail", args=[self.favorite_list_1.slug])
 
         # check number of queries
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(10):
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
