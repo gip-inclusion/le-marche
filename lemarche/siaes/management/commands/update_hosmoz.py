a5a2be593ce43b358cc3b67eeadd9c4d74885311
Guilouf <gui.lebreton@gmail.com>
2025-08-20 17:20:01 +0200

    fix cases

--- lemarche/siaes/management/commands/update_hosmoz.py
+++ lemarche/siaes/management/commands/update_hosmoz.py
@@ -2,8 +2,9 @@ import csv
 
 from django import forms
 from django.core.management.base import BaseCommand
-from django.db.models import Case, Value, When
+from django.db.models import Case, EmailField, F, PositiveIntegerField, Value, When
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 from lemarche.siaes.models import Siae
 
@@ -55,19 +56,28 @@ class Command(BaseCommand):
         siret = row["Siret"].replace(" ", "")
 
         Siae.objects.filter(siret=siret).update(
-            contact_email=Case(When(contact_email="", then=Value(cleaned_data.get("contact_email", "")))),
+            contact_email=Case(
+                When(contact_email="", then=Value(cleaned_data.get("contact_email", ""))),
+                default=F("contact_email"),
+                output_field=EmailField(),
+            ),
             contact_phone=Case(
                 When(contact_phone="", then=Value(str(cleaned_data.get("contact_phone", "")))),
+                default=F("contact_phone"),
+                output_field=PhoneNumberField(),
             ),
             employees_insertion_count=Case(
                 When(
                     employees_insertion_count__isnull=True,
                     then=Value(cleaned_data.get("employees_insertion_count", None)),
-                )
+                ),
+                default=F("employees_insertion_count"),
+                output_field=PositiveIntegerField(),
             ),
             # If we update employees_insertion_count, then we also
             # need to update employees_insertion_count_last_updated date
             employees_insertion_count_last_updated=Case(
-                When(employees_insertion_count__isnull=True, then=Value(timezone.now()))
+                When(employees_insertion_count__isnull=True, then=Value(timezone.now())),
+                default=F("employees_insertion_count_last_updated"),
             ),
         )
