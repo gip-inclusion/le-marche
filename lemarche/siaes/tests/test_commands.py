11ab7744ef03c2cedc8b72dfb720aeee7c1e6341
Guilouf <gui.lebreton@gmail.com>
2025-08-21 16:03:49 +0200

    Add to hosmoz network

--- lemarche/siaes/tests/test_commands.py
+++ lemarche/siaes/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.models import signals
 from django.test import TestCase, TransactionTestCase
 
+from lemarche.networks.factories import NetworkFactory
 from lemarche.siaes import constants as siae_constants
 from lemarche.siaes.factories import SiaeActivityFactory, SiaeFactory
 from lemarche.siaes.models import Siae
@@ -578,6 +579,9 @@ class SiaeUpdateApiEntrepriseFieldsCommandTest(TestCase):
 
 class HosmoZCommandTest(TestCase):
 
+    def setUp(self):
+        self.hosmoz_network = NetworkFactory(slug="hosmoz")
+
     def test_update_empty_siae(self):
         siae = SiaeFactory(
             siret="41155513900012",
@@ -586,6 +590,7 @@ class HosmoZCommandTest(TestCase):
             employees_insertion_count=None,
             employees_insertion_count_last_updated=None,
         )
+        self.assertEqual(siae.networks.all().count(), 0)
 
         call_command("update_hosmoz", csv_file="lemarche/fixtures/tests/hosmoz_import.csv", stdout=StringIO())
 
@@ -595,6 +600,7 @@ class HosmoZCommandTest(TestCase):
         self.assertEqual(siae.contact_phone, "01 02 03 04 05")
         self.assertEqual(siae.employees_insertion_count, 22)
         self.assertIsNotNone(siae.employees_insertion_count_last_updated)
+        self.assertEqual(siae.networks.all().count(), 0)
 
     def test_update_full_siae(self):
         siae = SiaeFactory(
@@ -603,7 +609,9 @@ class HosmoZCommandTest(TestCase):
             contact_phone="++33 1 02 06 06 06",
             employees_insertion_count=10,
             employees_insertion_count_last_updated=None,
+            networks=[self.hosmoz_network],
         )
+        self.assertEqual(siae.networks.all().count(), 1)
 
         call_command("update_hosmoz", csv_file="lemarche/fixtures/tests/hosmoz_import.csv", stdout=StringIO())
 
@@ -613,3 +621,4 @@ class HosmoZCommandTest(TestCase):
         self.assertEqual(siae.contact_phone, "++33 1 02 06 06 06")
         self.assertEqual(siae.employees_insertion_count, 10)
         self.assertIsNone(siae.employees_insertion_count_last_updated)
+        self.assertEqual(siae.networks.all().count(), 1)
