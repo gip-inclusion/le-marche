384ed8a5484d5f828db05fb7a5d5745f9e27d049
Guilouf <Guilouf@users.noreply.github.com>
2025-08-28 16:31:07 +0200

    filter siae king

--- lemarche/www/dashboard/filters.py
+++ lemarche/www/dashboard/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from django import forms
 
 from lemarche.purchases.models import Purchase
+from lemarche.siaes.constants import KIND_CHOICES_WITH_EXTRA, KIND_HANDICAP_LIST, KIND_INSERTION_LIST
 
 
 def get_purchase_category_choices():
@@ -15,8 +16,21 @@ def get_buying_entities_choices():
 
 
 class PurchaseFilterSet(django_filters.FilterSet):
-    inclusive_sector_type = ...
-    siae_type = ...
+    INCLUSIVE_SECTOR_TYPE_CHOCIES = ((KIND_INSERTION_LIST, "Insertion"), (KIND_HANDICAP_LIST, "Handicap"))
+    #
+    # inclusive_sector_type = django_filters.MultipleChoiceFilter(
+    #     label="Type de secteur inclusif",
+    #     widget=forms.CheckboxSelectMultiple,
+    #     field_name="siae__kind",
+    #     lookup_expr="in",
+    #     choices=INCLUSIVE_SECTOR_TYPE_CHOCIES,
+    # )
+    siae_type = django_filters.MultipleChoiceFilter(
+        label="Type de structure",
+        widget=forms.CheckboxSelectMultiple,
+        method="filter_siae_type",
+        choices=KIND_CHOICES_WITH_EXTRA,
+    )
     purchase_category = django_filters.MultipleChoiceFilter(
         choices=get_purchase_category_choices(), widget=forms.CheckboxSelectMultiple
     )
@@ -26,4 +40,8 @@ class PurchaseFilterSet(django_filters.FilterSet):
 
     class Meta:
         model = Purchase
-        fields = ["purchase_category", "buying_entity"]
+        fields = ["siae_type", "purchase_category", "buying_entity"]
+
+    def filter_siae_type(self, queryset, name, value):
+        """Return departement by provided code or name"""
+        return queryset.filter(siae__kind__in=value)
