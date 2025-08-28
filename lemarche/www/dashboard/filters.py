bb88be0e9d005e9e5bfa412a565599933b42f8e8
Guilouf <Guilouf@users.noreply.github.com>
2025-08-28 16:04:20 +0200

    filter with displayed choices

--- lemarche/www/dashboard/filters.py
+++ lemarche/www/dashboard/filters.py
@@ -1,11 +1,28 @@
 import django_filters
+from django import forms
 
 from lemarche.purchases.models import Purchase
+
+
+def get_purchase_category_choices():
+    purchase_categories = Purchase.objects.values_list("purchase_category", flat=True).distinct()
+    return [(purchase_category, purchase_category) for purchase_category in purchase_categories]
+
+
+def get_buying_entities_choices():
+    buying_entities = Purchase.objects.values_list("buying_entity", flat=True).distinct()
+    return [(buying_entity, buying_entity) for buying_entity in buying_entities]
 
 
 class PurchaseFilterSet(django_filters.FilterSet):
     inclusive_sector_type = ...
     siae_type = ...
+    purchase_category = django_filters.MultipleChoiceFilter(
+        choices=get_purchase_category_choices(), widget=forms.CheckboxSelectMultiple
+    )
+    buying_entity = django_filters.MultipleChoiceFilter(
+        choices=get_buying_entities_choices(), widget=forms.CheckboxSelectMultiple
+    )
 
     class Meta:
         model = Purchase
