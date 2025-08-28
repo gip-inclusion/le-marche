a5f2ae003e776fbce4bd29a27418b4b537343b6f
Guilouf <Guilouf@users.noreply.github.com>
2025-08-28 15:31:58 +0200

    working filter

--- lemarche/www/dashboard/filters.py
+++ lemarche/www/dashboard/filters.py
@@ -1,8 +1,12 @@
 import django_filters
 
+from lemarche.purchases.models import Purchase
 
-class PurchaseFilter(django_filters.FilterSet):
+
+class PurchaseFilterSet(django_filters.FilterSet):
     inclusive_sector_type = ...
     siae_type = ...
-    purchase_category = ...
-    buying_entity = ...
+
+    class Meta:
+        model = Purchase
+        fields = ["purchase_category", "buying_entity"]
