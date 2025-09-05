94a8fa4b5450cd07870c5f0864e69865575470c5
Guilouf <Guilouf@users.noreply.github.com>
2025-09-05 10:32:58 +0200

    fix flake8

--- lemarche/api/inclusive_potential/utils.py
+++ lemarche/api/inclusive_potential/utils.py
@@ -22,6 +22,38 @@ class PotentialData:
     employees_permanent_average: int
 
 
+def set_analysis_data(siaes, siaes_count, budget, analysis_data):
+    # Get all valid CA values from siaes and calculate the average manually to avoid to many fat queries
+    ca_values = [siae.ca or siae.api_entreprise_ca for siae in siaes if siae.ca or siae.api_entreprise_ca]
+    if ca_values:
+        analysis_data["ca_average"] = round(sum(ca_values) / len(ca_values))
+        analysis_data["eco_dependency"] = round(budget / analysis_data["ca_average"] * 100)
+
+    if siaes_count > LIMIT_FOR_RESERVATION:
+        if "eco_dependency" in analysis_data and analysis_data["eco_dependency"] < LIMIT_FOR_ECO_DEPENDENCY:
+            recommendation = RECOMMENDATIONS["reservation"].copy()
+        else:
+            recommendation = RECOMMENDATIONS["lot"].copy()
+    elif siaes_count > LIMIT_FOR_LOT:
+        recommendation = RECOMMENDATIONS["lot"].copy()
+    elif siaes_count > LIMIT_FOR_CLAUSE:
+        recommendation = RECOMMENDATIONS["clause"].copy()
+    else:
+        recommendation = RECOMMENDATIONS["aucun"].copy()
+
+    # check if eco_dependency and ca_average exist as they are needed to format the explanation
+    recommendation["explanation"] = (
+        recommendation["explanation"].format(
+            siaes_count=siaes_count,
+            eco_dependency=analysis_data["eco_dependency"],
+            ca_average=analysis_data["ca_average"],
+        )
+        if "eco_dependency" in analysis_data and "ca_average" in analysis_data
+        else None
+    )
+    analysis_data["recommendation"] = recommendation
+
+
 def get_inclusive_potential_data(sector: str, perimeter: str, budget: int) -> tuple[PotentialData, dict]:
     """
     Get the potential data for a given sector and perimeter.
@@ -58,35 +90,7 @@ def get_inclusive_potential_data(sector: str, perimeter: str, budget: int) -> tu
 
     analysis_data = {}
     if budget:
-        # Get all valid CA values from siaes and calculate the average manually to avoid to many fat queries
-        ca_values = [siae.ca or siae.api_entreprise_ca for siae in siaes if siae.ca or siae.api_entreprise_ca]
-        if ca_values:
-            analysis_data["ca_average"] = round(sum(ca_values) / len(ca_values))
-            analysis_data["eco_dependency"] = round(budget / analysis_data["ca_average"] * 100)
-
-        if siaes_count > LIMIT_FOR_RESERVATION:
-            if "eco_dependency" in analysis_data and analysis_data["eco_dependency"] < LIMIT_FOR_ECO_DEPENDENCY:
-                recommendation = RECOMMENDATIONS["reservation"].copy()
-            else:
-                recommendation = RECOMMENDATIONS["lot"].copy()
-        elif siaes_count > LIMIT_FOR_LOT:
-            recommendation = RECOMMENDATIONS["lot"].copy()
-        elif siaes_count > LIMIT_FOR_CLAUSE:
-            recommendation = RECOMMENDATIONS["clause"].copy()
-        else:
-            recommendation = RECOMMENDATIONS["aucun"].copy()
-
-        # check if eco_dependency and ca_average exist as they are needed to format the explanation
-        recommendation["explanation"] = (
-            recommendation["explanation"].format(
-                siaes_count=siaes_count,
-                eco_dependency=analysis_data["eco_dependency"],
-                ca_average=analysis_data["ca_average"],
-            )
-            if "eco_dependency" in analysis_data and "ca_average" in analysis_data
-            else None
-        )
-        analysis_data["recommendation"] = recommendation
+        set_analysis_data(siaes, siaes_count, budget, analysis_data)
 
     return (
         PotentialData(
