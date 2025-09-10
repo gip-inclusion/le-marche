fcc565b2ef8a7197e07ce188c1ae04a6bf5b62bd
SebastienReuiller <sebastien@reuiller.email>
2025-09-10 11:00:58 +0200

    fix(admin): Suppression de la possibilité d'ajouter dans l'admin des correspondances entre les références clients et les entreprises (#1782)

--- lemarche/companies/admin.py
+++ lemarche/companies/admin.py
@@ -196,6 +196,9 @@ class CompanySiaeClientReferenceMatchAdmin(admin.ModelAdmin):
 
     actions = ["approve_matches", "reject_matches"]
 
+    def has_add_permission(self, request):
+        return False
+
     def get_queryset(self, request):
         return super().get_queryset(request).select_related("company", "siae_client_reference", "moderated_by")
 
