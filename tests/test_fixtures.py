a88abac22d9e1c0e6d093cfda0b5ba9eb9991b66
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-10-06 23:13:51 +0200

    tests: Clean fixture test
    We only need to check that the fixtures can load, no need to check that
    there are some elements in the fixtures files.

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,12 +1,5 @@
 from django.test import TestCase
 
-from lemarche.companies.models import Company
-from lemarche.networks.models import Network
-from lemarche.sectors.models import Sector, SectorGroup
-from lemarche.siaes.models import Siae, SiaeClientReference, SiaeGroup, SiaeLabelOld, SiaeOffer, SiaeUser
-from lemarche.tenders.models import Tender, TenderQuestion, TenderSiae
-from lemarche.users.models import User
-
 
 class FixturesTest(TestCase):
     fixtures = [
@@ -36,21 +29,5 @@ class FixturesTest(TestCase):
     ]
 
     def test_flat_fixtures_load_successfully(self):
-        # alternative?
-        # management.call_command("loaddata", "lemarche/fixtures/django/01_siaes.json")
-        # why not self.assertTrue(Siae.objects.all().count()) ?
-        # returns an error: django.db.utils.InternalError: variable not found in subplan target list
-        self.assertTrue(len(Network.objects.all()) > 0)
-        self.assertTrue(len(SectorGroup.objects.all()) > 0)
-        self.assertTrue(len(Sector.objects.all()) > 0)
-        self.assertTrue(len(Company.objects.all()) > 0)
-        self.assertTrue(len(SiaeGroup.objects.all()) > 0)
-        self.assertTrue(len(Siae.objects.all()) > 0)
-        self.assertTrue(len(User.objects.all()) > 0)
-        self.assertTrue(len(SiaeUser.objects.all()) > 0)
-        self.assertTrue(len(SiaeClientReference.objects.all()) > 0)
-        self.assertTrue(len(SiaeLabelOld.objects.all()) > 0)
-        self.assertTrue(len(SiaeOffer.objects.all()) > 0)
-        self.assertTrue(len(Tender.objects.all()) > 0)
-        self.assertTrue(len(TenderQuestion.objects.all()) > 0)
-        self.assertTrue(len(TenderSiae.objects.all()) > 0)
+        # if this test passes, the fixtures have loaded successfully
+        pass
