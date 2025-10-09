f73ebd1a4fec5cd59d40d5b69df65b32e617be06
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-10-09 12:27:51 +0200

    chore: Squash networks migrations

--- lemarche/users/migrations/0001_initial.py
+++ lemarche/users/migrations/0001_initial.py
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("auth", "0012_alter_user_first_name_max_length"),
         ("companies", "0001_initial"),
-        ("networks", "0004_alter_network_slug"),
+        ("networks", "0001_initial"),
         ("sectors", "0001_initial"),
     ]
 
