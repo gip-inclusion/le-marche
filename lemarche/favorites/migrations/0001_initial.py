bca5f3d9fa3142ce91493d8f7e24671adbbfbb62
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-10-09 13:25:47 +0200

    chore: Squash siaes migrations

--- lemarche/favorites/migrations/0001_initial.py
+++ lemarche/favorites/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("siaes", "0091_siaeesus"),
+        ("siaes", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
