90ac51a7a5fd80fae51254d929af7e306db9b1cf
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-01-23 13:38:46 +0100

    Rename master branch into main

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -8,27 +8,27 @@ if ! git diff-index --quiet HEAD; then
   exit
 fi
 
-# Rebase master into master_clever, then push master_clever
+# Rebase main into main_clever, then push main_clever
 # Deployment to Clever Cloud is actually triggered via a hook
 # on a push on this branch
-git checkout master
-git pull origin master
-git checkout master_clever
-git pull origin master_clever
+git checkout main
+git pull origin main
+git checkout main_clever
+git pull origin main_clever
 echo "====="
-git_diff_between_master_and_master_clever=`git log master_clever..master --oneline`
-echo "$git_diff_between_master_and_master_clever"
+git_diff_between_main_and_main_clever=`git log main_clever..main --oneline`
+echo "$git_diff_between_main_and_main_clever"
 echo "====="
-git rebase master
-git push origin master_clever
+git rebase main
+git push origin main_clever
 
 # When we are done, we want to restore the initial state
-# (in order to avoid writing things directly on master_clever by accident)
+# (in order to avoid writing things directly on main_clever by accident)
 if [ -z $initial_branch ]; then
-  # The initial_branch is empty when user is in detached state, so we simply go back to master
-  git checkout master
+  # The initial_branch is empty when user is in detached state, so we simply go back to main
+  git checkout main
   echo
-  echo "You were on detached state before deploying, you are back to master"
+  echo "You were on detached state before deploying, you are back to main"
 else
   git checkout $initial_branch
   echo
