bf7c42bd289794c96e794ccf4b247c8d642798be
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-02-09 11:36:10 +0100

    nexus: bump itoutils for better sync logging

--- lemarche/nexus/sync.py
+++ lemarche/nexus/sync.py
@@ -1,7 +1,12 @@
+import logging
+
 from django.conf import settings
-from itoutils.django.nexus.api import NexusAPIClient
+from itoutils.django.nexus.api import NexusAPIClient, NexusAPIException
 
 from lemarche.utils.urls import get_object_share_url
+
+
+logger = logging.getLogger(__name__)
 
 
 USER_TRACKED_FIELDS = [
@@ -32,16 +37,20 @@ def sync_users(users):
     if settings.NEXUS_API_BASE_URL:
         try:
             NexusAPIClient().send_users([serialize_user(user) for user in users])
+        except NexusAPIException:
+            pass  # The client already logged the error, we don't want to crash if we can't connect to Nexus
         except Exception:
-            pass
+            logger.exception("Nexus: failed to sync users")
 
 
 def delete_users(user_pks):
     if settings.NEXUS_API_BASE_URL:
         try:
             NexusAPIClient().delete_users(user_pks)
+        except NexusAPIException:
+            pass  # The client already logged the error, we don't want to crash if we can't connect to Nexus
         except Exception:
-            pass
+            logger.exception("Nexus: failed to delete users")
 
 
 SIAE_TRACKED_FIELDS = [
@@ -87,16 +96,20 @@ def sync_siaes(siaes):
     if settings.NEXUS_API_BASE_URL:
         try:
             NexusAPIClient().send_structures([serialize_siae(siae) for siae in siaes])
+        except NexusAPIException:
+            pass  # The client already logged the error, we don't want to crash if we can't connect to Nexus
         except Exception:
-            pass
+            logger.exception("Nexus: failed to sync siaes")
 
 
 def delete_siaes(siae_pks):
     if settings.NEXUS_API_BASE_URL:
         try:
             NexusAPIClient().delete_structures(siae_pks)
+        except NexusAPIException:
+            pass  # The client already logged the error, we don't want to crash if we can't connect to Nexus
         except Exception:
-            pass
+            logger.exception("Nexus: failed to delete siaes")
 
 
 SIAEUSER_TRACKED_FIELDS = [
@@ -118,13 +131,17 @@ def sync_siaeusers(siaeusers):
     if settings.NEXUS_API_BASE_URL:
         try:
             NexusAPIClient().send_memberships([serialize_membership(siaeuser) for siaeuser in siaeusers])
+        except NexusAPIException:
+            pass  # The client already logged the error, we don't want to crash if we can't connect to Nexus
         except Exception:
-            pass
+            logger.exception("Nexus: failed to sync siaeusers")
 
 
 def delete_siaeusers(siaeuser_pks):
     if settings.NEXUS_API_BASE_URL:
         try:
             NexusAPIClient().delete_memberships(siaeuser_pks)
+        except NexusAPIException:
+            pass  # The client already logged the error, we don't want to crash if we can't connect to Nexus
         except Exception:
-            pass
+            logger.exception("Nexus: failed to delete siaeusers")
